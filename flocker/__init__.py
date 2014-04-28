b086670d6a0f2a30b562fe4142e0cb82aec0d68e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-04-28 14:47:44 -0400

    It is great to have a Python package and to have a test package for it.

---
+++ flocker/__init__.py
@@ -0,0 +1,7 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+# -*- test-case-name: flocker.test -*-
+
+"""
+Flocker is a hypervisor that provides ZFS-based replication and fail-over
+functionality to a Linux-based user-space operating system.
+"""
---
+++ flocker/test/__init__.py
@@ -0,0 +1,5 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+This package contains all of Flocker's unit tests.
+"""
