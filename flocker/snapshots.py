bdb35051df5ab0b0e7fc09c269f765d89771b489
Itamar Turner-Trauring <itamar@itamarst.org>
2014-05-06 15:12:06 -0400

    Move filesystem interfaces into their own module.

--- flocker/snapshots.py
+++ flocker/snapshots.py
@@ -4,35 +4,6 @@ Drive the snapshotting of a filesystem, based on change events from elsewhere.
 
 from __future__ import absolute_import
 
-from zope.interface import Interface
-
-
-class IFilesystemSnapshots(Interface):
-    """
-    Support creating and listing snapshots of a specific filesystem.
-    """
-    def create(name):
-        """
-        Create a snapshot of the filesystem.
-
-        :param name: The name of the snapshot.
-        :type name: :py:class:`bytes`
-
-        :return: Deferred that fires on snapshot creation, or errbacks if
-            snapshotting failed. The Deferred should support cancellation
-            if at all possible.
-        """
-
-
-    def list():
-        """
-        Return all the filesystem's snapshots.
-
-        :return: Deferred that fires with a ``list`` of ``bytes`` (snapshot
-            names). This will likely be improved in later iterations.
-        """
-
-
 
 class ChangeSnapshotter(object):
     """
