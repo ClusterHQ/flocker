02bea41d4d2689fe26eb96ec28f90b2a00e24f67
Itamar Turner-Trauring <itamar@itamarst.org>
2014-05-22 13:21:11 -0400

    Volume creation works.

--- prototype/flocker.py
+++ prototype/flocker.py
@@ -5,11 +5,77 @@ Each flocker instance is configured with a specific pool, which is assumed
 to be mounted at /<poolname>.
 
 For simplicity's sake not bothering with UUIDs, you need to make sure names
-don't conflict.
-
+don't conflict. Volume/branch/tag names should not contain "." because of
+hacky implementation details.
 """
 
-from twisted.python.usage import Options
+import subprocess
+from collections import namedtuple
+
+from twisted.python.usage import Options, UsageError
+from twisted.python.filepath import FilePath
+
+
+def zfs(*arguments):
+    """
+    Run a 'zfs' command with given arguments, raise on non-0 exit code.
+    """
+    subprocess.check_call(["zfs"] + list(arguments))
+
+
+
+class FlockerBranch(namedtuple("FlockerBranch", "flockerName volume branch")):
+    @property
+    def datasetName(self):
+        """
+        The name of the ZFS dataset for the branch.
+        """
+        return b".".join([self.flockerName, self.volume, self.branch])
+
+
+    @classmethod
+    def fromDatasetName(cls, datasetName):
+        """
+        Convert ZFS dataset name to FlockerBranch instance.
+        """
+        return cls(*datasetName.split(b"."))
+
+
+
+class Flocker(object):
+    """
+    Flocker volume manager.
+    """
+    def __init__(self, poolName):
+        self.poolName = poolName
+        self.mountRoot = FilePath(b"/").child(poolName)
+        # Re-use pool name as name for this Flocker instance:
+        self.flockerName = poolName
+
+
+    def createVolume(self, volumeName):
+        """
+        Create a new volume with given name.
+        """
+        trunk = FlockerBranch(self.flockerName, volumeName, b"trunk").datasetName
+        zfs(b"create", b"%s/%s" % (self.poolName, trunk))
+
+
+    def listVolumes(self):
+        """
+        Return list of all volumes.
+        """
+        result = []
+        for branchName in self.mountRoot.listdir():
+            if b"." not in branchName:
+                # Some junk, not something we're managing:
+                continue
+            branch = FlockerBranch.fromDatasetName(branchName)
+            if branch.flockerName == self.flockerName:
+                result.append(b"%s/%s" % (branch.volume, branch.branch))
+            else:
+                result.append(b"/".join(branch))
+        return result
 
 
 
@@ -21,8 +87,8 @@ class CreateOptions(Options):
         self.name = name
 
 
-    def postOptions(self):
-        pass
+    def run(self, flocker):
+        flocker.createVolume(self.name)
 
 
 
@@ -30,8 +96,9 @@ class ListVolumesOptions(Options):
     """
     List volumes.
     """
-    def postOptions(self):
-        pass
+    def run(self, flocker):
+        for name in flocker.listVolumes():
+            print name
 
 
 
@@ -43,7 +110,7 @@ class BranchOptions(Options):
         self.name = name
 
 
-    def postOptions(self):
+    def run(self, flocker):
         pass
 
 
@@ -56,7 +123,7 @@ class ListBranchesOptions(Options):
         self.name = name
 
 
-    def postOptions(self):
+    def run(self, flocker):
         pass
 
 
@@ -69,7 +136,7 @@ class TagOptions(Options):
         self.name = name
 
 
-    def postOptions(self):
+    def run(self, flocker):
         pass
 
 
@@ -81,7 +148,8 @@ class ListTagsOptions(Options):
     def parseArgs(self, name):
         self.name = name
 
-    def postOptions(self):
+
+    def run(self, flocker):
         pass
 
 
@@ -90,6 +158,10 @@ class FlockerOptions(Options):
     """
     Flocker volume manager.
     """
+    optParameters = [
+        ["pool", "p", None, "The name of the pool to use"],
+        ]
+
     subCommands = [
         ["volume", None, CreateOptions, "Create a volume and its default trunk branch"],
         ["list-volumes", None, ListVolumesOptions, "List volumes"],
@@ -106,6 +178,9 @@ class FlockerOptions(Options):
     def postOptions(self):
         if self.subCommand is None:
             return self.opt_help()
+        if self["pool"] is None:
+            raise UsageError("pool is required")
+        self.subOptions.run(Flocker(self["pool"]))
 
 
 
