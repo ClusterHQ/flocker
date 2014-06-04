87eb44bef89c12d7b04da4dab5b295ffaaccf841
Itamar Turner-Trauring <itamar@itamarst.org>
2014-06-04 12:48:39 -0400

    Command line parsing for the configuration file.

--- flocker/volume/script.py
+++ flocker/volume/script.py
@@ -1,7 +1,16 @@
 """The command-line ``flocker-volume`` tool."""
 
 from twisted.python.usage import Options
+from twisted.python.filepath import FilePath
 
 
 class FlockerVolumeOptions(Options):
     """flocker-volume - volume management."""
+
+    optParameters = [
+        ["config", None, b"/etc/flocker/volume.json",
+         "The path to the config file."],
+    ]
+
+    def postOptions(self):
+        self["config"] = FilePath(self["config"])
