fd9a097997183434ebd145ca586dc0c105a8eb37
Itamar Turner-Trauring <itamar@itamarst.org>
2014-06-05 13:59:48 -0400

    More PEP8ification.

--- flocker/volume/functional/test_filesystems_zfs.py
+++ flocker/volume/functional/test_filesystems_zfs.py
@@ -1,7 +1,6 @@
 # Copyright Hybrid Logic Ltd.  See LICENSE file for details.
 
-"""
-Functional tests for ZFS filesystem implementation.
+"""Functional tests for ZFS filesystem implementation.
 
 Further coverage is provided in
 :module:`flocker.volume.test.test_filesystems_zfs`.
@@ -19,29 +18,28 @@ from ..test.filesystemtests import makeIFilesystemSnapshotsTests
 from ..filesystems.zfs import ZFSSnapshots, Filesystem
 
 
-def createZFSPool(testCase):
-    """
-    Create a new ZFS pool, then delete it after the test is over.
+def create_zfs_pool(test_case):
+    """Create a new ZFS pool, then delete it after the test is over.
 
-    :param testCase: A ``unittest.TestCase``.
+    :param test_case: A ``unittest.TestCase``.
 
     :return: A :class:`Filesystem` instance.
     """
     if os.getuid() != 0:
         raise SkipTest("Functional tests must run as root.")
 
-    poolName = "testpool_%s" % (uuid.uuid4(),)
-    poolPath = tempfile.mktemp()
-    mountPath = tempfile.mktemp()
-    subprocess.check_call([b"dd", b"if=/dev/zero", b"of=%s" % (poolPath),
+    pool_name = "testpool_%s" % (uuid.uuid4(),)
+    pool_path = tempfile.mktemp()
+    mount_path = tempfile.mktemp()
+    subprocess.check_call([b"dd", b"if=/dev/zero", b"of=%s" % (pool_path),
                            b"count=200000"])
-    testCase.addCleanup(os.remove, poolPath)
-    subprocess.check_call([b"zpool", b"create", b"-m", mountPath, poolName,
-                           poolPath])
-    testCase.addCleanup(subprocess.check_call,
-                        [b"zpool", b"destroy", poolName])
-    return Filesystem(poolName)
+    test_case.addCleanup(os.remove, pool_path)
+    subprocess.check_call([b"zpool", b"create", b"-m", mount_path, pool_name,
+                           pool_path])
+    test_case.addCleanup(subprocess.check_call,
+                        [b"zpool", b"destroy", pool_name])
+    return Filesystem(pool_name)
 
 
 IFilesystemSnapshotsTests = makeIFilesystemSnapshotsTests(
-    lambda testCase: ZFSSnapshots(reactor, createZFSPool(testCase)))
+    lambda test_case: ZFSSnapshots(reactor, create_zfs_pool(test_case)))
