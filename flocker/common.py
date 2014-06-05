d56782e9eadfb01ca09d2ed804db098b4d8abfc7
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-06-05 09:17:37 -0400

    Import UnCooperator.

--- flocker/common.py
+++ flocker/common.py
@@ -4,9 +4,9 @@
 Generally helpful tools.
 """
 
-__all__ = ["EventChannel"]
+__all__ = ["EventChannel", "UnCooperator"]
 
-from twisted.internet.defer import Deferred
+from twisted.internet.defer import Deferred, succeed
 
 
 class EventChannel(object):
@@ -64,3 +64,100 @@ class EventChannel(object):
         d = Deferred(canceller=self._subscriptions.remove)
         self._subscriptions.append(d)
         return d
+
+
+
+class _MinimallyCooperativeTask(object):
+    """
+    @ivar _done: C{False} until iteration over the iterator has gone past the
+        last element.  C{True} afterwards.
+    @type _done: L{bool}
+
+    @ivar _result: C{None} until C{_done} is C{True}.  Afterwards, the last
+        element of the iterator (or if the last element was a L{Deferred}, the
+        result of that L{Deferred}).
+
+    @ivar _channel: Used to give out results from C{whenDone}.  Fires with just
+        one event (when the iterator is exhausted).
+    @type _channel: L{EventChannel}
+    """
+    def __init__(self, iterator):
+        """
+        @param iterator: The iterator to exhaust.
+        """
+        self._done = False
+        self._result = None
+        self._channel = EventChannel()
+        self._channel.subscribe().addBoth(self._setResult)
+        self._sequentially(iterator).addCallbacks(
+            self._channel.callback, self._channel.errback)
+
+
+    def _setResult(self, result):
+        """
+        Mark the task as complete and record its result.
+        """
+        self._done = True
+        self._result = result
+
+
+    def whenDone(self):
+        """
+        Get a L{Deferred} that fires with the result of this task.
+        """
+        if self._done:
+            return succeed(self._result)
+        else:
+            return self._channel.subscribe()
+
+
+    @classmethod
+    def _sequentially(cls, iterator):
+        """
+        Begin iterating over C{iterator}.
+
+        Iteration is done as quickly as possible.  It is only interrupted when
+        a L{Deferred} without a result is encountered.  Then iteration is
+        suspended until that L{Deferred} has a result.
+        """
+        finished = Deferred()
+        cls._iteration(finished, iterator)
+        return finished
+
+
+    @classmethod
+    def _iteration(cls, finished, iterator, result=None):
+        """
+        Iterate over C{iterator} until a L{Deferred} is encountered.
+        """
+        while True:
+            try:
+                waiting = next(iterator)
+            except StopIteration:
+                finished.callback(result)
+                break
+            except:
+                finished.errback()
+                break
+            else:
+                if isinstance(waiting, Deferred):
+                    waiting.addCallbacks(
+                        lambda result:
+                            cls._iteration(finished, iterator, result),
+                        finished.errback)
+                    break
+                result = waiting
+
+
+
+class UnCooperator(object):
+    """
+    An L{UnCooperator} looks like a L{twisted.internet.task.Cooperator} but it
+    doesn't try to be cooperative.  It iterates over iterators as fast as it
+    can.
+    """
+    def cooperate(self, iterator):
+        """
+        Create and start an uncooperative task for the given iterator.
+        """
+        return _MinimallyCooperativeTask(iterator)
--- flocker/test/test_common.py
+++ flocker/test/test_common.py
@@ -5,10 +5,11 @@ Tests for :py:mod:`flocker.common`.
 """
 
 from twisted.python.failure import Failure
-from twisted.internet.defer import CancelledError
+from twisted.internet.defer import CancelledError, Deferred, succeed, fail
 from twisted.trial.unittest import TestCase
 
-from ..common import EventChannel
+from .common import ArbitraryException
+from ..common import EventChannel, UnCooperator
 
 
 class EventChannelTests(TestCase):
@@ -63,3 +64,101 @@ class EventChannelTests(TestCase):
         d = channel.subscribe()
         d.cancel()
         return self.assertFailure(d, CancelledError)
+
+
+
+class UnCooperatorTests(TestCase):
+    """
+    Tests for L{UnCooperator}.
+    """
+    def setUp(self):
+        self.cooperator = UnCooperator()
+        self.cooperate = lambda seq: self.cooperator.cooperate(iter(seq))
+
+
+    def test_empty(self):
+        """
+        The task returned by L{UnCooperator.cooperate} when called with an
+        empty iterator is already done.
+        """
+        cooperating = self.cooperate([]).whenDone()
+        self.assertIs(None, self.successResultOf(cooperating))
+
+
+    def test_alreadySucceededElements(self):
+        """
+        The task returned by L{UnCooperator.cooperate} when called with an
+        iterator of L{Deferred} instances that already have results is already
+        done.
+        """
+        cooperating = self.cooperate([succeed(None)]).whenDone()
+        self.assertIs(None, self.successResultOf(cooperating))
+
+
+    def test_nonDeferredElements(self):
+        """
+        The task returned by L{UnCooperator.cooperate} when called with an
+        iterator of objects that are not L{Deferred} instances is already done.
+        """
+        cooperating = self.cooperate([None]).whenDone()
+        self.assertIs(None, self.successResultOf(cooperating))
+
+
+    def test_resultIsLastValue(self):
+        """
+        The result of the L{Deferred} returned by the C{whenDone} method of the
+        task returned by L{UnCooperator.cooperate} is the value of the last
+        element of the iterator.
+        """
+        result = object()
+        cooperating = self.cooperate([None, result]).whenDone()
+        self.assertIs(result, self.successResultOf(cooperating))
+
+
+    def test_waitForDeferreds(self):
+        """
+        When the iterator passed to L{UnCooperator.cooperate} includes a
+        L{Deferred} instance that does not yet have a result, the task returned
+        also does not have a result.
+        """
+        cooperating = self.cooperate([Deferred()]).whenDone()
+        self.assertNoResult(cooperating)
+
+
+    def test_doneWhenElementDeferredDone(self):
+        """
+        When the not yet complete L{Deferred} in the iterator passed to
+        L{UnCooperator.cooperate} does complete, the task returned completes.
+        """
+        result = object()
+        waiting = Deferred()
+        cooperating = self.cooperate([waiting]).whenDone()
+        waiting.callback(result)
+        self.assertIs(result, self.successResultOf(cooperating))
+
+
+    def test_errorOnException(self):
+        """
+        When an exception (except StopIteration) is raised trying to get the
+        next element of the iterator the L{Deferred} returned by the
+        C{whenDone} method of the task returned by L{UnCooperator.cooperate}
+        fires with a L{Failure}.
+        """
+        def broken():
+            raise ArbitraryException()
+            yield
+
+        cooperating = self.cooperate(broken()).whenDone()
+        self.failureResultOf(cooperating, ArbitraryException)
+
+
+    def test_errorOnFailure(self):
+        """
+        Like L{test_errorOnException} but for the case where an element of the
+        iterator is a L{Deferred} that fires with a L{Failure}.
+        """
+        def broken():
+            yield fail(ArbitraryException())
+
+        cooperating = self.cooperate(broken()).whenDone()
+        self.failureResultOf(cooperating, ArbitraryException)
