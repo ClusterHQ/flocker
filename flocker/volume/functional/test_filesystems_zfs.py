2cb0c8c34be7f9208fb3a89ce10647949725329b
Itamar Turner-Trauring <itamar@itamarst.org>
2014-06-06 10:05:29 -0400

    Address review comment: Better test setup and more PEP 8.

--- flocker/volume/functional/test_filesystems_zfs.py
+++ flocker/volume/functional/test_filesystems_zfs.py
@@ -7,14 +7,14 @@ Further coverage is provided in
 """
 
 import os
-import tempfile
 import subprocess
 import uuid
 
 from twisted.internet import reactor
 from twisted.trial.unittest import SkipTest
+from twisted.python.filepath import FilePath
 
-from ..test.filesystemtests import makeIFilesystemSnapshotsTests
+from ..test.filesystemtests import make_ifilesystemsnapshots_tests
 from ..filesystems.zfs import ZFSSnapshots, Filesystem
 
 
@@ -29,17 +29,20 @@ def create_zfs_pool(test_case):
         raise SkipTest("Functional tests must run as root.")
 
     pool_name = "testpool_%s" % (uuid.uuid4(),)
-    pool_path = tempfile.mktemp()
-    mount_path = tempfile.mktemp()
-    subprocess.check_call([b"dd", b"if=/dev/zero", b"of=%s" % (pool_path),
+    pool_path = FilePath(test_case.mktemp())
+    mount_path = FilePath(test_case.mktemp())
+    with pool_path.open("wb") as f:
+        f.truncate(100 * 1024 * 1024)
+    subprocess.check_call([b"dd", b"if=/dev/zero", b"of=%s" % (pool_path.path),
                            b"count=200000"])
-    test_case.addCleanup(os.remove, pool_path)
-    subprocess.check_call([b"zpool", b"create", b"-m", mount_path, pool_name,
-                           pool_path])
+    test_case.addCleanup(pool_path.remove)
+    subprocess.check_call([b"zpool", b"create", b"-m", mount_path.path,
+                           pool_name, pool_path.path])
     test_case.addCleanup(subprocess.check_call,
                         [b"zpool", b"destroy", pool_name])
     return Filesystem(pool_name)
 
 
-IFilesystemSnapshotsTests = makeIFilesystemSnapshotsTests(
-    lambda test_case: ZFSSnapshots(reactor, create_zfs_pool(test_case)))
+class IFilesystemSnapshotsTests(make_ifilesystemsnapshots_tests(
+        lambda test_case: ZFSSnapshots(reactor, create_zfs_pool(test_case)))):
+    """``IFilesystemSnapshots`` tests for ZFS."""
