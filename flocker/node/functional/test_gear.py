8a64487a415373b808f42ef6a6123bfb76f331dc
Itamar Turner-Trauring <itamar@itamarst.org>
2014-06-11 13:15:37 -0400

    Test for correct image being used, which requires gear to be local.

--- flocker/node/functional/test_gear.py
+++ flocker/node/functional/test_gear.py
@@ -4,9 +4,11 @@
 
 import os
 import json
+import subprocess
 from unittest import skipIf
 
 from twisted.trial.unittest import TestCase
+from twisted.python.procutils import which
 
 from treq import request, content
 
@@ -14,19 +16,13 @@ from ...testtools import loop_until
 from ..test.test_gear import make_igearclient_tests, random_name
 from ..gear import GearClient
 
-
-# Allow setting gear server location:
-GEAR_HOST = os.getenv("GEAR_HOST", None)
-
-
-_if_gear_configured = skipIf(
-    GEAR_HOST is None,
-    "Must set GEAR_HOST env variable to run functional gear tests.")
+_if_gear_configured = skipIf(which("gear") == [],
+                             "Must run on machine with gear running.")
 _if_root = skipIf(os.getuid() != 0, "Must run as root.")
 
 
 class IGearClientTests(make_igearclient_tests(
-        lambda test_case: GearClient(GEAR_HOST))):
+        lambda test_case: GearClient("127.0.0.1"))):
     """``IGearClient`` tests for ``FakeGearClient``."""
 
     @_if_gear_configured
@@ -41,10 +37,14 @@ class GearClientTests(TestCase):
     def setUp(self):
         pass
 
-    def test_add_starts_container(self):
-        """``GearClient.add`` starts the container."""
-        client = GearClient(GEAR_HOST)
-        name = random_name()
+    def start_container(self, name):
+        """Start a unit and wait until it's up and running.
+
+        :param name: The name of the unit.
+
+        :return: Deferred that fires when the unit is running.
+        """
+        client = GearClient("127.0.0.1")
         d = client.add(name, u"openshift/busybox-http-app")
 
         def is_started(data):
@@ -56,7 +56,7 @@ class GearClientTests(TestCase):
             # Replace with ``GearClient.list`` as part of
             # https://github.com/hybridlogic/flocker/issues/32
             responded = request(
-                b"GET", b"http://%s:43273/containers" % (GEAR_HOST,),
+                b"GET", b"http://%s:43273/containers" % ("127.0.0.1",),
                 persistent=False)
             responded.addCallback(content)
             responded.addCallback(json.loads)
@@ -69,9 +69,23 @@ class GearClientTests(TestCase):
         d.addCallback(added)
         return d
 
+    def test_add_starts_container(self):
+        """``GearClient.add`` starts the container."""
+        name = random_name()
+        return self.start_container(name)
+
     @_if_root
     def test_correct_image_used(self):
         """``GearClient.add`` creates a container with the specified image."""
+        name = random_name()
+        d = self.start_container(name)
+
+        def started(_):
+            data = subprocess.check_output(
+                [b"docker", b"inspect", name.encode("ascii")])
+            self.assertEqual(json.loads(data)[u"Config"][u"Image"], u"busybox")
+        d.addCallback(started)
+        return d
 
     def test_exists_error(self):
         """``GearClient.exists`` returns ``Deferred`` that errbacks with
@@ -90,4 +104,4 @@ class GearClientTests(TestCase):
 
 
 # XXX still need to write documentation.
-
+# XXX add flocker- prefix to container names?
