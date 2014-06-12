ee1f3937a1628121069fe0e41fd987d0aef56f2e
rwall <richard@the-moon.net>
2014-06-12 10:01:01 +0100

    A test mixin for the standard options on every flocker command

--- flocker/script.py
+++ flocker/script.py
@@ -47,3 +47,41 @@ def volume(config):
     """
     """
     click.echo()
+
+
+
+from twisted.python.usage import Options
+from twisted.python.filepath import FilePath
+from . import __version__
+
+def flocker_standard_options(cls):
+    def opt_version(self):
+        """
+        Print the program's version and exit.
+        """
+        print(__version__)
+        raise SystemExit(0)
+    cls.opt_version = opt_version
+    cls.opt_v = opt_version
+
+    return cls
+
+
+
+@flocker_standard_options
+class FlockerVolumeOptions(Options):
+    """Command line options for ``flocker-volume`` volume management tool."""
+
+    longdesc = """flocker-volume allows you to manage volumes, filesystems
+    that can be attached to Docker containers.
+
+    At the moment no functionality has been implemented.
+    """
+
+    optParameters = [
+        ["config", None, b"/etc/flocker/volume.json",
+         "The path to the config file."],
+    ]
+
+    def postOptions(self):
+        self["config"] = FilePath(self["config"])
--- flocker/test/test_script.py
+++ flocker/test/test_script.py
@@ -6,6 +6,9 @@ Tests for :module:`flocker.script`.
 
 from __future__ import absolute_import
 
+import sys
+import io
+
 from twisted.trial.unittest import SynchronousTestCase
 from twisted.application.service import IServiceMaker, IServiceCollection
 from twisted.plugin import getPlugins
@@ -13,7 +16,7 @@ from zope.interface.verify import verifyObject
 
 from click.testing import CliRunner
 
-from ..script import flocker, volume
+from ..script import flocker, volume, FlockerVolumeOptions
 from .. import __version__
 
 
@@ -112,12 +115,34 @@ class FlockerVolumeTests(CommonArgumentsTestsMixin, SynchronousTestCase):
 
 
 
-# class FilePathTypeTests(SynchronousTestCase):
-#     """
-#     Tests for L{FilePath} click type.
-#     """
-#     def test_foo(self):
-#         """
+class StandardOptionsTestsMixin(object):
+    """
+    Tests for the standard options that should be available on every flocker
+    command.
+    """
+    options = None
 
-#         """
-#         self.assertEqual('', FilePath())
+    def test_version(self):
+        """
+        Flocker commands have a I{--version} option which prints the current
+        version string to stdout and causes the command to exit with status 0.
+        """
+        output = io.BytesIO()
+        self.patch(sys, 'stdout', output)
+        error = self.assertRaises(
+            SystemExit,
+            self.options().parseOptions,
+            ['--version']
+        )
+        self.assertEqual(
+            (__version__ + '\n', 0),
+            (output.getvalue(), error.code)
+        )
+
+
+
+class FlockerVolumeOptionsTests(StandardOptionsTestsMixin, SynchronousTestCase):
+    """
+    Tests for L{FlockerVolumeOptions}.
+    """
+    options = FlockerVolumeOptions
