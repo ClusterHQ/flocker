26d80706b990d0411a11cb0b4200b40d3b51fe85
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-06-12 09:31:05 -0400

    Try to overcome the limitations of iptables-save and iptables-restore.

--- flocker/route/functional/iptables.py
+++ flocker/route/functional/iptables.py
@@ -14,6 +14,16 @@ def preserve_iptables():
     """
     rules = check_output([b"iptables-save"])
 
+    # Naive use of iptables-save | iptables-restore isn't actually a safe way
+    # to restore original configuration.
+    # https://bugzilla.netfilter.org/show_bug.cgi?id=960
+    tables = [b"filter", b"nat", b"mangle", b"raw", b"security"]
+
+    # Add material to flush every table first.  This overcomes the lack of
+    # information about "empty" tables in the bare iptables-save output.
+    rules = "".join(
+        "*{table}\nCOMMIT\n".format(table=table) for table in tables) + rules
+
     def restore():
         process = Popen([b"iptables-restore"], stdin=PIPE)
         process.stdin.write(rules)
