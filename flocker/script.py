0ad83e92b8bc5495fbfec4eb014281ef30e863b7
rwall <richard@the-moon.net>
2014-06-12 10:03:34 +0100

    Remove all the old click based stuff

--- flocker/script.py
+++ flocker/script.py
@@ -2,57 +2,12 @@
 """
 A top level flocker command.
 """
-
-import click
-
-from twisted.python import filepath
-
 from . import __version__
-
-
-class FilePath(click.ParamType):
-    """
-    A wrapper around L{click.Path} which returns a
-    L{twisted.python.filepath.FilePath}.
-    """
-    name = 'filepath'
-
-    def __init__(self, *args, **kwargs):
-        self.path = click.Path(*args, **kwargs)
-
-
-    def convert(self, value, param, ctx):
-        value = self.path.convert(value, param, ctx)
-        return filepath.FilePath(value)
-
-
-
-@click.group()
-@click.help_option()
-@click.version_option(version=__version__)
-def flocker():
-    pass
-
-
-
-@flocker.command()
-@click.help_option()
-@click.option(
-    '--config',
-    type=FilePath(),
-    default=b"/etc/flocker/volume.json",
-    help='The path to the config file.'
-)
-def volume(config):
-    """
-    """
-    click.echo()
-
 
 
 from twisted.python.usage import Options
 from twisted.python.filepath import FilePath
-from . import __version__
+
 
 def flocker_standard_options(cls):
     def opt_version(self):
--- flocker/test/test_script.py
+++ flocker/test/test_script.py
@@ -6,113 +6,12 @@ Tests for :module:`flocker.script`.
 
 from __future__ import absolute_import
 
-import sys
-import io
+import io, sys
 
 from twisted.trial.unittest import SynchronousTestCase
-from twisted.application.service import IServiceMaker, IServiceCollection
-from twisted.plugin import getPlugins
-from zope.interface.verify import verifyObject
 
-from click.testing import CliRunner
-
-from ..script import flocker, volume, FlockerVolumeOptions
+from ..script import FlockerVolumeOptions
 from .. import __version__
-
-
-def assertClickHelp(testCase, command, arguments):
-    """
-    Assert that help text is printed.
-    """
-    runner = CliRunner()
-    result = runner.invoke(command, arguments)
-
-    testCase.assertEqual(
-        (None, 0, u'Usage'),
-        (result.exception, result.exit_code, result.output[:len(u'Usage')])
-    )
-
-
-
-def assertClickOutput(testCase, result, expectedOutput, expectedStatus=0):
-    """
-    Assert that the expected output is printed and that no unexpected
-    exceptions were raised.
-
-    XXX: This is required because of the way click.testing.CliRunner
-    captures exceptions. See https://github.com/mitsuhiko/click/issues/136
-    """
-    testCase.assertEqual(
-        (None, expectedOutput, expectedStatus),
-        (result.exception, result.output, result.exit_code)
-    )
-
-
-
-class CommonArgumentsTestsMixin(object):
-    def test_help(self):
-        """
-        When run with a help argument, the flocker prints help text and exits
-        with status 0.
-        """
-        assertClickHelp(self, flocker, ['--help'])
-
-
-    def test_version(self):
-        """
-        When run with a version argument, the command prints the flocker version
-        and exits with status 0
-        """
-        runner = CliRunner()
-        result = runner.invoke(flocker, ['--version'])
-        assertClickOutput(
-            self, result,
-            u'flocker, version {version}\n'.format(version=__version__),
-            0
-        )
-
-
-
-class FlockerTests(CommonArgumentsTestsMixin, SynchronousTestCase):
-    """
-    """
-    def test_noArguments(self):
-        """
-        When run without any arguments flocker prints help text.
-        """
-        assertClickHelp(self, flocker, [])
-
-
-
-class FlockerVolumeTests(CommonArgumentsTestsMixin, SynchronousTestCase):
-    """
-    """
-    def test_subcommand(self):
-        """
-        L{volume} is registered as a subcommand of L{flocker}.
-        """
-        runner = CliRunner()
-        result = runner.invoke(flocker, ['volume'])
-        assertClickOutput(self, result, u'\n', 0)
-
-
-    def test_noArguments(self):
-        """
-        L{volume} without arguments prints a blank line and exits with status 0.
-        """
-        runner = CliRunner()
-        result = runner.invoke(volume, [])
-        assertClickOutput(self, result, u'\n', 0)
-
-
-    def test_configDefault(self):
-        """
-        L{volume} without a config argument passes the default value.
-        """
-        runner = CliRunner()
-        result = runner.invoke(volume)
-        assertClickOutput(self, result, b'\n', 0)
-
 
 
 class StandardOptionsTestsMixin(object):
