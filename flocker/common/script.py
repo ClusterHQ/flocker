86558d545d18b66018ac75b0b4076cee36de5e50
Richard Wall <richard@the-moon.net>
2014-06-18 11:47:30 +0100

    move common infrastructure, tests and helpers to a new common package.

---
+++ flocker/common/script.py
@@ -0,0 +1,88 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""Helpers for flocker shell commands."""
+
+import sys
+
+from twisted.internet.task import react
+from twisted.python import usage
+
+from .. import __version__
+
+
+def flocker_standard_options(cls):
+    """
+    Add various standard command line options to flocker commands and
+    subcommands.
+    """
+    original_init = cls.__init__
+    def __init__(self, *args, **kwargs):
+        self._sys_module = kwargs.pop('sys_module', sys)
+        self['verbosity'] = 0
+        original_init(self, *args, **kwargs)
+    cls.__init__ = __init__
+
+
+    def opt_version(self):
+        """
+        Print the program's version and exit.
+        """
+        self._sys_module.stdout.write(__version__.encode('utf-8') + b'\n')
+        raise SystemExit(0)
+    cls.opt_version = opt_version
+
+
+    def opt_verbose(self):
+        """
+        Increase the verbosity.
+        """
+        self['verbosity'] += 1
+    cls.opt_verbose = opt_verbose
+    cls.opt_v = opt_verbose
+
+    return cls
+
+
+
+class FlockerScriptRunner(object):
+    """
+    An API for running standard flocker scripts.
+    """
+    _react = staticmethod(react)
+
+    def __init__(self, script, options, sys_module=None):
+        """
+        """
+        self.script = script
+        self.options = options
+
+        if sys_module is None:
+            sys_module = sys
+        self.sys_module = sys_module
+
+
+    def _parseOptions(self, arguments):
+        """
+        Parse the options defined in the script's options class.
+
+        L{UsageErrors} are caught and printed to I{stderr} and the script then
+        exits.
+
+        @param arguments: The command line arguments to be parsed.
+        @rtype: L{Options}
+        """
+        try:
+            self.options.parseOptions(arguments)
+        except usage.UsageError as e:
+            self.sys_module.stderr.write(unicode(self.options).encode('utf-8'))
+            self.sys_module.stderr.write(b'ERROR: ' + e.message.encode('utf-8') + b'\n')
+            raise SystemExit(1)
+        return self.options
+
+
+    def main(self):
+        """
+        Parse arguments and run the script's main function via L{react}.
+        """
+        options = self._parseOptions(self.sys_module.argv[1:])
+        return self._react(self.script.main, (options,))
--- flocker/volume/script.py
+++ flocker/volume/script.py
@@ -2,49 +2,12 @@
 
 """The command-line ``flocker-volume`` tool."""
 
-import sys
-
-from twisted.python.usage import Options, UsageError
+from twisted.python.usage import Options
 from twisted.python.filepath import FilePath
-from twisted.internet.task import react
 from twisted.internet.defer import succeed
 
 from .service import VolumeService, CreateConfigurationError
-from .. import __version__
-
-
-def flocker_standard_options(cls):
-    """
-    Add various standard command line options to flocker commands and
-    subcommands.
-    """
-    original_init = cls.__init__
-    def __init__(self, *args, **kwargs):
-        self._sys_module = kwargs.pop('sys_module', sys)
-        self['verbosity'] = 0
-        original_init(self, *args, **kwargs)
-    cls.__init__ = __init__
-
-
-    def opt_version(self):
-        """
-        Print the program's version and exit.
-        """
-        self._sys_module.stdout.write(__version__.encode('utf-8') + b'\n')
-        raise SystemExit(0)
-    cls.opt_version = opt_version
-
-
-    def opt_verbose(self):
-        """
-        Increase the verbosity.
-        """
-        self['verbosity'] += 1
-    cls.opt_verbose = opt_verbose
-    cls.opt_v = opt_verbose
-
-    return cls
-
+from flocker.common.script import flocker_standard_options, FlockerScriptRunner
 
 
 @flocker_standard_options
@@ -65,52 +28,6 @@ class VolumeOptions(Options):
 
     def postOptions(self):
         self["config"] = FilePath(self["config"])
-
-
-
-class FlockerScriptRunner(object):
-    """
-    An API for running standard flocker scripts.
-    """
-    _react = staticmethod(react)
-
-    def __init__(self, script, options, sys_module=None):
-        """
-        """
-        self.script = script
-        self.options = options
-
-        if sys_module is None:
-            sys_module = sys
-        self.sys_module = sys_module
-
-
-    def _parseOptions(self, arguments):
-        """
-        Parse the options defined in the script's options class.
-
-        L{UsageErrors} are caught and printed to I{stderr} and the script then
-        exits.
-
-        @param arguments: The command line arguments to be parsed.
-        @rtype: L{Options}
-        """
-        try:
-            self.options.parseOptions(arguments)
-        except UsageError as e:
-            self.sys_module.stderr.write(unicode(self.options).encode('utf-8'))
-            self.sys_module.stderr.write(b'ERROR: ' + e.message.encode('utf-8') + b'\n')
-            raise SystemExit(1)
-        return self.options
-
-
-    def main(self):
-        """
-        Parse arguments and run the script's main function via L{react}.
-        """
-        options = self._parseOptions(self.sys_module.argv[1:])
-        args = (options,)
-        return self._react(self.script.main, args)
 
 
 
@@ -138,3 +55,11 @@ flocker_volume_main = FlockerScriptRunner(
     script=VolumeScript(),
     options=VolumeOptions()
 ).main
+
+
+
+__all__ = [
+    'flocker_volume_main',
+    'VolumeOptions',
+    'VolumeScript',
+]
