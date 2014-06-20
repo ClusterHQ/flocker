f1541c54329e9fc4c03cdbc1ccce6435a3f698db
Tom Prince <tom.prince@ualberta.net>
2014-06-20 00:39:22 -0600

    Match version tested against to available twisted version in fedora 21.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
         # Pinning this isn't great in general, but we're only using UTC so meh:
         "pytz == 2014.2",
         "characteristic == 0.1.0",
-        "Twisted == 13.2.0",
+        "Twisted == 14.0.0",
 
         "netifaces == 0.8",
         "ipaddr == 2.1.10",
