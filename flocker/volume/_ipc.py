13ad033451c3b65559bf6579eb21e950e754ab56
Itamar Turner-Trauring <itamar@itamarst.org>
2014-06-26 13:43:31 -0400

    Address review comment: JP hates commas.

--- flocker/volume/_ipc.py
+++ flocker/volume/_ipc.py
@@ -3,7 +3,7 @@
 """Inter-process communication for the volume manager.
 
 Specific volume managers ("nodes") may wish to push data to other
-nodes. In the current iteration this is done over SSH, using a blocking
+nodes. In the current iteration this is done over SSH using a blocking
 API. In some future iteration this will be replaced with an actual
 well-specified communication protocol between daemon processes using
 Twisted's event loop (https://github.com/ClusterHQ/flocker/issues/154).
