5adb8f26ffd426073ba41162ca66ce0fb7a0f556
Adam Dangoor <adamdangoor@gmail.com>
2014-06-26 14:56:55 +0100

    Start of tests for DeployOptions

--- flocker/cli/test/test_deploy_script.py
+++ flocker/cli/test/test_deploy_script.py
@@ -4,9 +4,9 @@
 Unit tests for the implementation ``flocker-deploy``.
 """
 
-from twisted.trial.unittest import TestCase
+from twisted.trial.unittest import TestCase, SynchronousTestCase
 
-from ...testtools import FlockerScriptTestsMixin
+from ...testtools import FlockerScriptTestsMixin, StandardOptionsTestsMixin
 from ..script import DeployScript, DeployOptions
 
 
@@ -15,3 +15,22 @@ class FlockerDeployTests(FlockerScriptTestsMixin, TestCase):
     script = DeployScript
     options = DeployOptions
     command_name = u'flocker-deploy'
+
+
+class DeployOptionsTests(StandardOptionsTestsMixin, SynchronousTestCase):
+    """Tests for :class:`FlockerVolumeOptions`."""
+    options = DeployOptions
+
+    # def test_default_config(self):
+    #     """By default the config file is ``b'/etc/flocker/volume.json'``."""
+    #     options = self.options()
+    #     options.parseOptions([])
+    #     self.assertEqual(options["config"],
+    #                      FilePath(b"/etc/flocker/volume.json"))
+    #
+    # def test_custom_config(self):
+    #     """A custom config file can be specified with ``--config``."""
+    #     options = self.options()
+    #     options.parseOptions([b"--config", b"/path/somefile.json"])
+    #     self.assertEqual(options["config"],
+    #                      FilePath(b"/path/somefile.json"))
