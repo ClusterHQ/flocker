e9835db54cfccdb13d8f37481f5d427afdf955a1
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-06-26 15:36:21 -0400

    Some more of the test suite.

--- flocker/test_sshconfig.py
+++ flocker/test_sshconfig.py
@@ -44,7 +44,7 @@ class ConfigureSSHTests(SynchronousTestCase):
 
     def test_key_generated(self):
         """
-        ``configure_ssh`` generates a new key pair and writes it to
+        ``configure_ssh`` generates a new key pair and writes it locally to
         ``id_rsa_flocker`` and ``id_rsa_flocker.pub``.
         """
         try:
@@ -84,3 +84,45 @@ class ConfigureSSHTests(SynchronousTestCase):
         id_rsa_pub = self.ssh_config.child(b"id_rsa_flocker.pub")
         keys = self.server.home.descendant([b".ssh", b"authorized_keys"])
         self.assertEqual(id_rsa_pub.getContent(), keys.getContent())
+
+    def test_authorized_keys_already_in_place(self):
+        """
+        When the SSH connection is established, if the
+        ``~/.ssh/authorized_keys`` file already has the public part of the key
+        pair then it is not appended again.
+        """
+        self.configure_ssh(self.server.host, self.server.port)
+        self.configure_ssh(self.server.host, self.server.port)
+        id_rsa_pub = self.ssh_config.child(b"id_rsa_flocker.pub")
+        keys = self.server.home.descendant([b".ssh", b"authorized_keys"])
+        self.assertEqual(id_rsa_pub.getContent(), keys.getContent())
+
+    def test_existing_authorized_keys_preserved(self):
+        """
+        Any unrelated content in the ``~/.ssh/authorized_keys`` file is left in
+        place by ``configure_ssh``.
+        """
+        existing_keys = (
+            b"ssh-dss AAAAB3Nz1234567890 comment\n"
+            b"ssh-dss AAAAB3Nz0987654321 comment\n"
+        )
+        authorized_keys = self.server.home.descendant([b".ssh", b"authorized_keys"])
+        authorized_keys.setContent(existing_keys)
+        self.configure_ssh(self.server.host, self.server.port)
+        self.assertIn(existing_keys, authorized_keys.getContent())
+
+    def test_flocker_keypair_written(self):
+        """
+        ``configure_ssh`` writes the keypair to ``id_rsa_flocker`` and
+        ``id_rsa_flocker.pub`` remotely.
+        """
+        self.configure_ssh(self.server.host, self.server.port)
+        expected = (
+            self.ssh_config.child(b"id_rsa_flocker").getContent(),
+            self.ssh_config.child(b"id_rsa_flocker.pub").getContent()
+        )
+        actual = (
+            self.flocker_config.child(b"id_rsa_flocker").getContent(),
+            self.flocker_config.child(b"id_rsa_flocker.pub").getContent()
+        )
+        self.assertEqual(expected, actual)
