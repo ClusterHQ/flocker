73d941029352d4699990dc55e3a1e5cc5254c3b2
Tom Prince <tom.prince@ualberta.net>
2014-06-29 23:29:50 -0600

    Fix create test to snapshot subvolume.

--- flocker/volume/test/test_filesystems_zfs.py
+++ flocker/volume/test/test_filesystems_zfs.py
@@ -108,15 +108,15 @@ class ZFSSnapshotsTests(SynchronousTestCase):
     def test_create(self):
         """
         ``ZFSSnapshots.create()`` calls the ``zfs snapshot`` command with the
-        pool and snapshot name.
+        filesystem and snapshot name.
         """
         reactor = FakeProcessReactor()
-        snapshots = ZFSSnapshots(reactor, Filesystem(b"mypool", None))
+        snapshots = ZFSSnapshots(reactor, Filesystem(b"mypool", "fs"))
         name = SnapshotName(datetime.now(UTC), b"node")
         snapshots.create(name)
         arguments = reactor.processes[0]
         self.assertEqual(arguments.args, [b"zfs", b"snapshot",
-                                          b"mypool@%s" % (name.to_bytes(),)])
+                                          b"mypool/fs@%s" % (name.to_bytes(),)])
 
     def test_create_no_result_yet(self):
         """
