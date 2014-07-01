5d6100f2a3067d77162880e308494b9b879e54de
Richard Wall <richard@the-moon.net>
2014-07-01 18:07:34 +0100

    Basic initialiser tests

--- flocker/node/test/test_model.py
+++ flocker/node/test/test_model.py
@@ -3,3 +3,24 @@
 """
 Tests for ``flocker.node._model``.
 """
+
+
+from ...testtools import make_with_init_tests
+from .._model import Application, DockerImage
+
+class DockerImageInitTests(make_with_init_tests(
+    record_type=DockerImage,
+    kwargs=dict(repository=u'clusterhq/flocker', tag=u'release-14.0')
+)):
+    """
+    Tests for ``DockerImage.__init__``.
+    """
+
+
+class ApplicationInitTests(make_with_init_tests(
+    record_type=Application,
+    kwargs=dict(name=u'site-example.com', image=object())
+)):
+    """
+    Tests for ``Application.__init__``.
+    """
