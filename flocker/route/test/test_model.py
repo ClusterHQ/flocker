b82aadd14e27b2111b03196a67dfbcdc72c27e9a
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-07-04 11:17:43 -0400

    Add some model tests for Proxy.

---
+++ flocker/route/test/test_model.py
@@ -0,0 +1,18 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Tests for ``flocker.route._model``.
+"""
+
+from ipaddr import IPAddress
+
+from .._model import Proxy
+
+from ...testtools import make_with_init_tests
+
+class ProxyInitTests(make_with_init_tests(
+        record_type=Proxy,
+        kwargs=dict(ip=IPAddress("10.0.1.2"), port=12345))):
+    """
+    Tests for ``Proxy.__init__``.
+    """
