4fefa059238f8b14cede628e691dd615e9d6e3d2
Tom Prince <tom.prince@ualberta.net>
2014-07-08 07:08:11 -0600

    Shorter line.

--- vagrant/base/cache.sh
+++ vagrant/base/cache.sh
@@ -16,4 +16,5 @@ docker pull openshift/busybox-http-app
 
 # Download python wheels
 mkdir /var/cache/wheelhouse
-wget --no-directories --directory-prefix /var/cache/wheelhouse --input-file https://s3-us-west-2.amazonaws.com/clusterhq-wheelhouse/fedora20-x86_64/index
+wget --no-directories --directory-prefix /var/cache/wheelhouse \
+     --input-file https://s3-us-west-2.amazonaws.com/clusterhq-wheelhouse/fedora20-x86_64/index
