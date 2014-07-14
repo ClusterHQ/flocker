985051a8a6bf62478fdf6eea8743e8e3ba265e70
Richard Wall <richard@the-moon.net>
2014-07-14 13:04:59 +0100

    Fix some existing tests by supplying the new positional argument

--- flocker/node/test/test_script.py
+++ flocker/node/test/test_script.py
@@ -55,7 +55,9 @@ class ChangeStateOptionsTests(StandardOptionsTestsMixin, SynchronousTestCase):
                                 "appspam": "appeggs",
                                 "appnumber": 17}
         options.parseOptions(
-            [safe_dump(expected_deployment), safe_dump(expected_application)])
+            [safe_dump(expected_deployment),
+             safe_dump(expected_application),
+             b'node1.example.com'])
 
         self.assertDictContainsSubset(
             {'deployment_config': expected_deployment,
@@ -71,7 +73,8 @@ class ChangeStateOptionsTests(StandardOptionsTestsMixin, SynchronousTestCase):
         options = self.options()
         deployment_bad_yaml = "{'foo':'bar', 'x':y, '':'"
         e = self.assertRaises(
-            UsageError, options.parseOptions, [deployment_bad_yaml, b''])
+            UsageError, options.parseOptions,
+            [deployment_bad_yaml, b'', b'node1.example.com'])
 
         self.assertTrue(
             str(e).startswith('Deployment config could not be parsed as YAML')
@@ -85,7 +88,8 @@ class ChangeStateOptionsTests(StandardOptionsTestsMixin, SynchronousTestCase):
         options = self.options()
         application_bad_yaml = "{'foo':'bar', 'x':y, '':'"
         e = self.assertRaises(
-            UsageError, options.parseOptions, [b'', application_bad_yaml])
+            UsageError, options.parseOptions,
+            [b'', application_bad_yaml, b'node1.example.com'])
 
         self.assertTrue(
             str(e).startswith('Application config could not be parsed as YAML')
