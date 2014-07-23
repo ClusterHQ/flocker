9db957faf0f2d1af54414a5686815a1c2f0aa5d9
Itamar Turner-Trauring <itamar@itamarst.org>
2014-07-23 14:27:27 -0400

    Hook up current state to the deployer.

--- flocker/node/_deploy.py
+++ flocker/node/_deploy.py
@@ -99,16 +99,18 @@ class Deployer(object):
         d.addCallback(applications_from_units)
         return d
 
-    def calculate_necessary_state_changes(self, desired_state,# current_state,
-                                          hostname):
+    def calculate_necessary_state_changes(self, desired_state,
+                                          current_cluster_state, hostname):
         """
         Work out which changes need to happen to the local state to match
         the given desired state.
 
         :param Deployment desired_state: The intended configuration of all
             nodes.
-        :param Deployment current_state: The current configuration of all
-            nodes.
+        :param Deployment current_cluster_state: The current configuration
+            of all nodes. While technically this also includes the current
+            node's state, this information may be out of date so we check
+            again to ensure we have absolute latest information.
         :param unicode hostname: The hostname of the node that this is running
             on.
 
@@ -160,21 +162,21 @@ class Deployer(object):
         return d
 
     def change_node_state(self, desired_state,
-                          #current_state,
+                          current_cluster_state,
                           hostname):
         """
         Change the local state to match the given desired state.
 
         :param Deployment desired_state: The intended configuration of all
             nodes.
-        :param Deployment current_state: The current configuration of all
-            nodes.
+        :param Deployment current_cluster_state: The current configuration
+            of all nodes.
         :param unicode hostname: The hostname of the node that this is running
             on.
         """
         d = self.calculate_necessary_state_changes(
             desired_state=desired_state,
-            #current_state=current_state,
+            current_cluster_state=current_cluster_state,
             hostname=hostname)
         d.addCallback(self._apply_changes)
         return d
