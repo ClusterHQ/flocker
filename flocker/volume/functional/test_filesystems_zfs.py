951e2a5e3b785513cca38646d6765613f6ccac2f
Itamar Turner-Trauring <itamar@itamarst.org>
2014-07-31 11:52:22 -0400

    Move create_zfs_pool to common test utilities module.

--- flocker/volume/functional/test_filesystems_zfs.py
+++ flocker/volume/functional/test_filesystems_zfs.py
@@ -21,29 +21,7 @@ from ..filesystems.zfs import (
     ZFSSnapshots, Filesystem, StoragePool, volume_to_dataset,
     )
 from ..service import Volume
-
-
-def create_zfs_pool(test_case):
-    """Create a new ZFS pool, then delete it after the test is over.
-
-    :param test_case: A ``unittest.TestCase``.
-
-    :return: The pool's name as ``bytes``.
-    """
-    if os.getuid() != 0:
-        raise SkipTest("Functional tests must run as root.")
-
-    pool_name = b"testpool_%s" % (uuid.uuid4(),)
-    pool_path = FilePath(test_case.mktemp())
-    mount_path = FilePath(test_case.mktemp())
-    with pool_path.open("wb") as f:
-        f.truncate(100 * 1024 * 1024)
-    test_case.addCleanup(pool_path.remove)
-    subprocess.check_call([b"zpool", b"create", b"-m", mount_path.path,
-                           pool_name, pool_path.path])
-    test_case.addCleanup(subprocess.check_call,
-                         [b"zpool", b"destroy", pool_name])
-    return pool_name
+from ..testtools import create_zfs_pool
 
 
 class IFilesystemSnapshotsTests(make_ifilesystemsnapshots_tests(
--- flocker/volume/testtools.py
+++ flocker/volume/testtools.py
@@ -4,6 +4,11 @@
 Testing utilities provided by ``flocker.volume``.
 """
 
+import os
+import uuid
+import subprocess
+from unittest import SkipTest
+
 from twisted.python.filepath import FilePath
 from twisted.internet.task import Clock
 
@@ -26,3 +31,26 @@ def create_volume_service(test):
     service.startService()
     test.addCleanup(service.stopService)
     return service
+
+
+def create_zfs_pool(test_case):
+    """Create a new ZFS pool, then delete it after the test is over.
+
+    :param test_case: A ``unittest.TestCase``.
+
+    :return: The pool's name as ``bytes``.
+    """
+    if os.getuid() != 0:
+        raise SkipTest("Functional tests must run as root.")
+
+    pool_name = b"testpool_%s" % (uuid.uuid4(),)
+    pool_path = FilePath(test_case.mktemp())
+    mount_path = FilePath(test_case.mktemp())
+    with pool_path.open("wb") as f:
+        f.truncate(100 * 1024 * 1024)
+    test_case.addCleanup(pool_path.remove)
+    subprocess.check_call([b"zpool", b"create", b"-m", mount_path.path,
+                           pool_name, pool_path.path])
+    test_case.addCleanup(subprocess.check_call,
+                         [b"zpool", b"destroy", pool_name])
+    return pool_name
