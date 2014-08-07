d43afc0da40cd5df9b8bb1c88d9265edd52ac78d
Adam Dangoor <adamdangoor@gmail.com>
2014-08-07 17:18:43 +0100

    Revert "Move the virtualenv install stage to the docs"
    This reverts commit a8d8d6ad224a325c26f6175dd124a3cdd50e70c7.

--- docs/gettingstarted/osx-install.sh
+++ docs/gettingstarted/osx-install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-# Create a virtualenv, an isolated Python environment, in a new directory called
-# "flocker-tutorial":
-virtualenv --python=/usr/local/bin/python2.7 flocker-tutorial
+# Get a working version of virtualenv and virtualenvwrapper
+# XXX change to a better URL
+# XXX get a license to use this
+curl -O https://glyph.im/pip/bootstrap.sh
+chmod u+x ./bootstrap.sh
+./bootstrap.sh
+
+# Create a virtualenv, an isolated Python environment, in a new directory
+# called "flocker-tutorial":
+mkvirtualenv flocker
 
 # Upgrade the pip Python package manager to its latest version inside the
 # virtualenv. Some older versions of pip have issues installing Python wheel
