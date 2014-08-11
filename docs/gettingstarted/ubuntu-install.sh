4ba8ae26d7cd78a154b498156c21258577ba8e82
Adam Dangoor <adamdangoor@gmail.com>
2014-08-11 09:17:48 -0700

    Updated versions in installers and vagrantfiles

--- docs/gettingstarted/ubuntu-install.sh
+++ docs/gettingstarted/ubuntu-install.sh
@@ -13,4 +13,4 @@ virtualenv --python=/usr/bin/python2.7 flocker-tutorial
 flocker-tutorial/bin/pip install --upgrade pip
 
 # Install flocker-cli and dependencies inside the virtualenv:
-flocker-tutorial/bin/pip install https://storage.googleapis.com/archive.clusterhq.com/downloads/flocker/Flocker-0.0.6-py2-none-any.whl
+flocker-tutorial/bin/pip install https://storage.googleapis.com/archive.clusterhq.com/downloads/flocker/Flocker-0.0.7-py2-none-any.whl
