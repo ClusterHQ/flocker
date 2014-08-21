58bc129bc363c46fbec877c99c48d52822ed885f
Richard Wall <richard.wall@clusterhq.com>
2014-08-21 12:42:13 +0100

    Tests and implementation

--- flocker/common/_defer.py
+++ flocker/common/_defer.py
@@ -4,25 +4,40 @@
 from twisted.internet.defer import gatherResults
 from twisted.python import log
 
-def _log_and_return_failure(self, failure):
+class GatherDeferredsAPI(object):
     """
-    Log and return the supplied failure.
-
-    :param Failure failure: The ``Failure`` to be logged.
-    :returns: The supplied ``Failure``.
+    An API for gather_deferreds which allows logging to be disabled for certain
+    unit tests.
     """
-    # log.err(failure)
-    # return failure
+    def __init__(self, log_errors=True):
+        """
+        :param bool log_errors: A flag which controls whether error logging is
+            enabled.
+        """
+        self.log_errors = log_errors
+
+    def _log_and_return_failure(self, failure):
+        """
+        Log and return the supplied failure.
+
+        :param Failure failure: The ``Failure`` to be logged.
+        :returns: The supplied ``Failure``.
+        """
+        log.err(failure)
+        return failure
+
+    def gather_deferreds(self, deferreds):
+        """
+        Return a deferred which fires when all of the supplied deferreds have
+        themselves fired.
+
+        Any errback in the supplied deferreds will be handled and logged with a call
+        to ``twisted.python.log.err``.
+        """
+        if self.log_errors:
+            for deferred in deferreds:
+                deferred.addErrback(self._log_and_return_failure)
+        return gatherResults(deferreds, consumeErrors=True)
 
 
-def gather_deferreds(deferreds):
-    """
-    Return a deferred which fires when all of the supplied deferreds have
-    themselves fired.
-
-    Any errback in the supplied deferreds will be handled and logged with a call
-    to ``twisted.python.log.err``.
-    """
-    # for deferred in deferreds:
-    #     deferred.addErrback(_log_and_return_failure)
-    # return gatherResults(deferreds)
+gather_deferreds = GatherDeferredsAPI().gather_deferreds
--- flocker/common/test/test_defer.py
+++ flocker/common/test/test_defer.py
@@ -1,41 +1,71 @@
+from .._defer import GatherDeferredsAPI, gather_deferreds
+
+from twisted.internet.defer import fail, FirstError, succeed
+from twisted.python import log
+from twisted.python.failure import Failure
 from twisted.trial.unittest import TestCase
-
-class LogAndReturnFailureTests(TestCase):
-    """
-    Tests for ``_log_and_return_failure``.
-    """
-    def test_failure_logged(self):
-        """
-        The supplied failure is logged.
-        """
-
-    def test_default_logger(self):
-        """
-        The default logger is twisted.python.log.err
-        """
-
-    def test_failure_returned(self):
-        """
-        The supplied failure is returned.
-        """
 
 
 class GatherDeferredsTests(TestCase):
     """
     Tests for ``gather_deferreds``.
     """
+    def test_gather_deferreds_api(self):
+        """
+        ``gather_deferreds`` is bound to an instance of ``GatherDeferredsAPI``.
+        """
+        self.assertIs(GatherDeferredsAPI, gather_deferreds.im_class)
+
+    def test_success(self):
+        """
+        The successful results of the supplied ``Deferred``s are returned.
+        """
+        expected_result1 = object()
+        expected_result2 = object()
+
+        d = GatherDeferredsAPI(log_errors=False).gather_deferreds(
+            [succeed(expected_result1), succeed(expected_result2)])
+
+        results = self.successResultOf(d)
+        self.assertEqual([expected_result1, expected_result2], results)
+
     def test_consume_errors_true(self):
         """
         The unhandled errbacks in the supplied ``Deferred``s are handled.
         """
+        d = GatherDeferredsAPI(log_errors=False).gather_deferreds(
+            [fail(ZeroDivisionError('test_consume_errors'))])
+
+        self.failureResultOf(d, FirstError)
+        self.assertEqual([], self.flushLoggedErrors(ZeroDivisionError))
 
     def test_fire_on_first_failure(self):
         """
         The first errback in the supplied list of deferreds causes the returned
         deferred to errback with that failure.
         """
+        expected_error = ZeroDivisionError('test_fire_on_first_failure')
+        d = GatherDeferredsAPI(log_errors=False).gather_deferreds(
+            [fail(expected_error)])
+
+        failure = self.failureResultOf(d, FirstError)
+        self.assertEqual(expected_error, failure.value.subFailure.value)
 
     def test_logging(self):
         """
-        Failures in the supplied deferreds are logged.
+        Failures in the supplied deferreds are all logged.
         """
+        messages = []
+        log.addObserver(messages.append)
+        self.addCleanup(log.removeObserver, messages.append)
+        expected_failure1 = Failure(ZeroDivisionError('test_logging1'))
+        expected_failure2 = Failure(ZeroDivisionError('test_logging2'))
+
+        d = GatherDeferredsAPI(log_errors=True).gather_deferreds(
+            [fail(expected_failure1), fail(expected_failure2)])
+
+        self.failureResultOf(d, FirstError)
+        self.assertEqual(
+            [expected_failure1, expected_failure2],
+            self.flushLoggedErrors(ZeroDivisionError)
+        )
