25450eaf812d42f16ee013f5713f9133a1bdd999
Adam Dangoor <adamdangoor@gmail.com>
2014-09-09 08:57:18 -0700

    Check links without anchors

--- docs/conf.py
+++ docs/conf.py
@@ -277,3 +277,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# Don't check anchors because many websites use #! for AJAX magic
+linkcheck_anchors = False
