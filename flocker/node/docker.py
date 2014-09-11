bb21c76380c617d90743ae0556ebef531a7b68ea
Itamar Turner-Trauring <itamar@itamarst.org>
2014-09-11 14:06:51 -0400

    Namespacing, all IGearClient tests now pass.

--- flocker/node/docker.py
+++ flocker/node/docker.py
@@ -16,6 +16,11 @@ from twisted.internet.threads import deferToThread
 from .gear import IGearClient, AlreadyExists, Unit
 
 
+# We namespace containers we manage so we don't clobber containers managed
+# by others:
+CONTAINER_NAMESPACE = u"flocker--"
+
+
 @implementer(IGearClient)
 class DockerClient(object):
     """
@@ -27,11 +32,23 @@ class DockerClient(object):
     def __init__(self):
         self._client = Client(version="1.12")
 
+    def _to_container_name(self, unit_name):
+        """
+        Add the namespace to the container name.
+
+        :param unicode unit_name: The unit's name.
+
+        :return unicode: The container's name.
+        """
+        return CONTAINER_NAMESPACE + unit_name
+
     def add(self, unit_name, image_name, ports=None, links=None,
             environment=None):
+        container_name = self._to_container_name(unit_name)
         def _add():
-            self._client.create_container(image_name, name=unit_name)
-            self._client.start(unit_name)
+            self._client.create_container(image_name,
+                                          name=container_name)
+            self._client.start(container_name)
         d = deferToThread(_add)
 
         def _extract_error(failure):
@@ -53,10 +70,11 @@ class DockerClient(object):
         return d
 
     def remove(self, unit_name):
+        container_name = self._to_container_name(unit_name)
         def _remove():
             try:
-                self._client.stop(unit_name)
-                self._client.remove_container(unit_name)
+                self._client.stop(container_name)
+                self._client.remove_container(container_name)
             except APIError as e:
                 if e.response.status_code == 404:
                     return
@@ -72,9 +90,12 @@ class DockerClient(object):
             for i in ids:
                 data = self._client.inspect_container(i)
                 state = u"active" if data[u"State"][u"Running"] else u"inactive"
-                # XXX get the impression Docker has container name
-                # namespacing now, should look into that...
-                result.add(Unit(name=data[u"Name"][1:],
+                name = data[u"Name"]
+                if not name.startswith(u"/" + CONTAINER_NAMESPACE):
+                    continue
+                else:
+                    name = name[1 + len(CONTAINER_NAMESPACE):]
+                result.add(Unit(name=name,
                                 activation_state=state,
                                 sub_state=u"",
                                 # We'll add this and the other available
