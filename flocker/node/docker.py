1bcc6081e602a8f9e960df416f3a82e5dbc17f48
Itamar Turner-Trauring <itamar@itamarst.org>
2014-09-11 13:34:24 -0400

    Basic tests pass; need extended add() arguments and probably some form of namespacing.

--- flocker/node/docker.py
+++ flocker/node/docker.py
@@ -4,162 +4,83 @@
 Docker API client.
 """
 
-from characteristic import attrs
+from __future__ import absolute_import
 
-from zope.interface import Interface, implementer
+from zope.interface import implementer
 
-from docker import Client, APIError
+from docker import Client
+from docker.errors import APIError
 
-from twisted.internet.defer import maybeDeferred
+from twisted.internet.threads import deferToThread
 
-
-@attrs(["path", "mountpoint", "readonly"])
-class VolumeMount(object):
-    """
-    Description of a volume mount within a container.
-
-    :ivar FilePath path: Path on host filesystem.
-    :ivar FilePath mountpoint: Path within container.
-    :ivar bool readonly: Whether or not mount is read-only.
-    """
+from .gear import IGearClient, AlreadyExists, Unit
 
 
-class NotFound(Exception):
-    """
-    The referenced object was not found.
-    """
-
-
-class ServerError(Exception):
-    """
-    Internal error in the server.
-    """
-
-
-class IDockerClient(Interface):
-    """
-    A client that talks to the local Docker server.
-
-    In general operations are asynchronous; results indicate that the
-    docker daemon responded and started the operation, not that the action
-    has finished.
-
-    Results may errback with ``NotFound`` or ``ServerError`` as relevant.
-    """
-
-    def create_container(self, image, command=None, name=None, volumes=None):
-        """
-        Create a new container that can be started later.
-
-        :param unicode image: The image to use.
-        :param command: A ``list`` of bytes (the command to run) or
-            ``None`` to use the default.
-        :param name: If ``None`` a name is randomly generated, otherwise
-            ``unicode`` should be given.
-        :param volumes: ``None``, or a list of strings indicating mount
-            points for volumes.
-
-        :return: ``Deferred`` firing with a ``unicode`` container
-            identifier when the operation is started by the Docker daemon.
-        """
-
-    def start_container(self, container, binds=None):
-        """
-        Start the given container.
-
-        :param unicode container: The name or id of the container.
-        :param binds: ``None``, or a list of ``VolumeMount`` instances
-            matching the volumes defined when the container was created.
-
-        :return: ``Deferred`` firing when the operation is started by the
-            Docker daemon.
-        """
-
-    def remove_container(self, container):
-        """
-        Remove the given container.
-
-        :param unicode container: The name or id of the container.
-
-        :return: ``Deferred`` firing when the operation is started by the
-            Docker daemon.
-        """
-
-    def inspect_container(self, container):
-        """
-        Return information about the given container.
-
-        :param unicode container: The name or id of the container.
-
-        :return: ``Deferred`` firing with a dictionary (decoded JSON
-            result).
-        """
-
-
-@implementer(IDockerClient)
+@implementer(IGearClient)
 class DockerClient(object):
     """
-    Talk to the real Docker server.
+    Talk to the real Docker server directly.
 
-    For now we don't bother with any sort of asyncness since we're talking
-    to localhost and expect answers back quickly. Longer term we may wish
-    to adds threading, or perhaps write our own client.
+    Some operations can take a while (e.g. stopping a container), so we
+    use a thread pool.
     """
     def __init__(self):
-        self._client = Client(version="1.10")
+        self._client = Client(version="1.12")
 
-    def _extract_error(self, failure):
-        failure.trap(APIError)
-        code = failure.value.response.status_code
-        if code == 404:
-            raise NotFound()
-        if code == 500:
-            raise ServerError(failure.value.explanation)
-        return failure
+    def add(self, unit_name, image_name, ports=None, links=None,
+            environment=None):
+        def _add():
+            self._client.create_container(image_name, name=unit_name)
+            self._client.start(unit_name)
+        d = deferToThread(_add)
 
-    def create_container(self, image, command=None, name=None, volumes=None):
-        d = maybeDeferred(self._client.create_container, image, command=command,
-                          name=name, volumes=volumes)
-        d.addCallback(lambda result: result[u"Id"])
-        d.addErrback(self._extract_error)
+        def _extract_error(failure):
+            failure.trap(APIError)
+            code = failure.value.response.status_code
+            if code == 409:
+                raise AlreadyExists(unit_name)
+            return failure
+        d.addErrback(_extract_error)
         return d
 
-    def start_container(self, container, binds=None):
-        if binds is not None:
-            binds = {volume.path.path: {u"bind": volume.mountpoint.path,
-                                        u"ro": volume.readonly}
-                     for volume in binds}
-        d = maybeDeferred(self._client.start, container, binds=binds)
-        d.addErrback(self._extract_error)
+    def exists(self, unit_name):
+        # XXX inefficient!
+        d = self.list()
+
+        def got_units(units):
+            return unit_name in [unit.name for unit in units]
+        d.addCallback(got_units)
         return d
 
-    def remove_container(self, container):
-        d = maybeDeferred(self._client.remove_container, container)
-        d.addErrback(self._extract_error)
+    def remove(self, unit_name):
+        def _remove():
+            try:
+                self._client.stop(unit_name)
+                self._client.remove_container(unit_name)
+            except APIError as e:
+                if e.response.status_code == 404:
+                    return
+                raise
+        d = deferToThread(_remove)
         return d
 
-    def inspect_container(self, container):
-        d = maybeDeferred(self._client.inspect_container, container)
-        d.addErrback(self._extract_error)
-        return d
-
-
-@implementer(IDockerClient)
-class FakeDockerClient(object):
-    """
-    Fake in-memory client.
-    """
-    def __init__(self):
-        self._containers = {} # map id to some information
-
-    def create_container(self, image, command=None, name=None, volumes=None):
-        pass
-
-    def start_container(self, container, binds=None):
-        pass
-
-    def remove_container(self, container):
-        pass
-
-    def inspect_container(self, container):
-        pass
+    def list(self):
+        def _list():
+            result = set()
+            ids = [d[u"Id"] for d in
+                   self._client.containers(quiet=True, all=True)]
+            for i in ids:
+                data = self._client.inspect_container(i)
+                state = u"active" if data[u"State"][u"Running"] else u"inactive"
+                # XXX get the impression Docker has container name
+                # namespacing now, should look into that...
+                result.add(Unit(name=data[u"Name"][1:],
+                                activation_state=state,
+                                sub_state=u"",
+                                # We'll add this and the other available
+                                # info later, for now we're just aiming at
+                                # GearClient compatibility.
+                                container_image=None,
+                            ))
+            return result
+        return deferToThread(_list)
