8f0c398f7ee9908cb58bb7de17720dfd5748f47f
Richard Wall <richard.wall@clusterhq.com>
2014-09-12 18:42:36 +0100

    Add some comments...in lieu of tests

--- setup.py
+++ setup.py
@@ -20,25 +20,46 @@ def rpm_version(flocker_version):
     version and a release version.
     See: http://fedoraproject.org/wiki/Packaging:NamingGuidelines#Pre-Release_packages
     """
+    # E.g. 0.1.2-69-gd2ff20c-dirty
+    # tag+distance+shortid+dirty
     parts = flocker_version.split('-')
-    version, since_tag = parts[0], parts[1:]
+    tag, remainder = parts[0], parts[1:]
     for suffix in ('pre', 'dev'):
-        parts = version.rsplit(suffix, 1)
+        parts = tag.rsplit(suffix, 1)
         if len(parts) == 2:
+            # A pre or dev suffix was present. ``version`` is the part before
+            # the pre and ``suffix_number`` is the part after the pre, but
+            # before the first dash.
             version = parts.pop(0)
-            release = parts[0]
-            if release.isdigit():
-                release = '.'.join(['0', release, suffix] + since_tag)
+            suffix_number = parts[0]
+            if suffix_number.isdigit():
+                # Given pre or dev number X create a 0 prefixed, `.` separated
+                # string of version labels. E.g.
+                # 0.1.2pre2  becomes
+                # 0.1.2-0.2.pre
+                release = ['0', suffix_number, suffix]
+                if remainder:
+                    # The version may also contain a distance, shortid which
+                    # means that there have been changes since the last
+                    # tag. Additionally there may be a ``dirty`` suffix which
+                    # indicates that there are uncommitted changes in the
+                    # working directory.  We probably don't want to release
+                    # untagged RPM versions, and this branch should probably
+                    # trigger and error or a warning. But for now we'll add
+                    # that extra information to the end of release number.
+                    release.extend(remainder)
             else:
+                # Non-integer pre or dev number found.
                 raise Exception(
                     'Non-integer value "{}" for "{}". '
                     'Supplied version {}'.format(
                         release, suffix, flocker_version))
             break
     else:
+        # Neither of the expected suffixes was found, the tag can be used as
+        # the RPM version
+        version = tag
         release = '1'
-
-    version = version.replace('-', '_')
 
     return version, release
 
