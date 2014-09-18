7a03aebdaf99912eddc9b95230a8a214294e890c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-09-18 15:01:31 -0400

    expose the exception type in the public api

--- flocker/libzfs_core/__init__.py
+++ flocker/libzfs_core/__init__.py
@@ -7,9 +7,12 @@ A library exposing libzfs_core, the ZFS C API, to Python using CFFI.
 from __future__ import absolute_import
 
 from ._binding import LibZFSCore
+from ._error import ZFSError
 
 (ffi, lib) = LibZFSCore.build()
 
 __all__ = [
+    "ZFSError",
+
     "ffi", "lib",
     ]
