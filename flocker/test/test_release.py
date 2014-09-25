7ad422e501badf7f3f1106f855ea9c513619b15d
Richard Wall <richard.wall@clusterhq.com>
2014-09-25 17:08:26 +0100

    Oops. Forgot to add the new modules and tests.

---
+++ flocker/test/test_release.py
@@ -0,0 +1,53 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Tests for ``flocker.common._release``.
+"""
+
+from unittest import TestCase
+
+from flocker._release import rpm_version, make_rpm_version
+
+class MakeRpmVersionTests(TestCase):
+    """
+    Tests for ``make_rpm_version``.
+    """
+    def test_good(self):
+        """
+        ``make_rpm_version`` gives the expected ``rpm_version`` instances when
+        supplied with valid ``flocker_version_number``s.
+        """
+        expected = {
+            '0.1.0': rpm_version('0.1.0', '1'),
+            '0.1.0-99-abc': rpm_version('0.1.0', '1.99.abc'),
+            '0.1.1pre1': rpm_version('0.1.1', '0.1.pre'),
+            '0.1.1': rpm_version('0.1.1', '1'),
+            '0.2.0dev1': rpm_version('0.2.0', '0.1.dev'),
+            '0.2.0dev2-99-xyz': rpm_version('0.2.0', '0.2.dev.99.xyz'),
+            '0.2.0dev3-100-abc-dirty': rpm_version('0.2.0', '0.3.dev.100.abc.dirty'),
+        }
+        unexpected_results = []
+        for supplied_version, expected_rpm_version in expected.items():
+            actual_rpm_version = make_rpm_version(supplied_version)
+            if actual_rpm_version != expected_rpm_version:
+                unexpected_results.append((
+                    supplied_version,
+                    actual_rpm_version,
+                    expected_rpm_version
+                ))
+
+        if unexpected_results:
+            self.fail(unexpected_results)
+
+    def test_non_integer_suffix(self):
+        """
+        ``make_rpm_version`` raises ``Exception`` when supplied with a version
+        with a non-integer pre or dev suffix number.
+        """
+        with self.assertRaises(Exception) as exception:
+            make_rpm_version('0.1.2preX')
+
+        self.assertEqual(
+            u'Non-integer value "X" for "pre". Supplied version 0.1.2preX',
+            unicode(exception.exception)
+        )
