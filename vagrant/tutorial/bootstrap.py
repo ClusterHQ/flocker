edc825f4a590a70b3b2d2899145811221407a4ba
Richard Wall <wallrj@users.noreply.github.com>
2014-09-29 15:03:37 +0100

    Merge pull request #817 from ClusterHQ/noop-scheduler-235
    Force the use of noop ioscheduler in tutorial vagrant vms
    
    Authors: @wallrj, @ryao
    Reviewer: @exarkun
    
    Fixes #235

--- vagrant/tutorial/bootstrap.py
+++ vagrant/tutorial/bootstrap.py
@@ -2,7 +2,8 @@
 
 # This script builds the base flocker-dev box.
 
-import sys, os
+import sys
+import os
 from subprocess import check_call, check_output
 from textwrap import dedent
 
@@ -17,8 +18,17 @@ if len(sys.argv) > 2:
 
 # Make it possible to install flocker-node
 rpm_dist = check_output(['rpm', '-E', '%dist']).strip()
-check_call(['yum', 'install', '-y', 'https://s3.amazonaws.com/archive.zfsonlinux.org/fedora/zfs-release%s.noarch.rpm' % (rpm_dist,)])
-check_call(['yum', 'install', '-y', 'https://storage.googleapis.com/archive.clusterhq.com/fedora/clusterhq-release%s.noarch.rpm' % (rpm_dist,)])
+zfs_repo_url = (
+    'https://s3.amazonaws.com/'
+    'archive.zfsonlinux.org/'
+    'fedora/zfs-release%s.noarch.rpm') % (rpm_dist,)
+check_call(['yum', 'install', '-y',  zfs_repo_url])
+
+clusterhq_repo_url = (
+    'https://storage.googleapis.com/'
+    'archive.clusterhq.com/'
+    'fedora/clusterhq-release%s.noarch.rpm') % (rpm_dist,)
+check_call(['yum', 'install', '-y', clusterhq_repo_url])
 
 if branch:
     with open('/etc/yum.repos.d/clusterhq-build.repo', 'w') as repo:
@@ -43,8 +53,15 @@ check_call(['systemctl', 'enable', 'geard'])
 
 # Make it easy to authenticate as root
 check_call(['mkdir', '-p', '/root/.ssh'])
-check_call(['cp', os.path.expanduser('~vagrant/.ssh/authorized_keys'), '/root/.ssh'])
+check_call(
+    ['cp', os.path.expanduser('~vagrant/.ssh/authorized_keys'), '/root/.ssh'])
 
+# Configure GRUB2 to boot kernel with elevator=noop to workaround
+# https://github.com/ClusterHQ/flocker/issues/235
+with open('/etc/default/grub', 'a') as f:
+    f.write('GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX} elevator=noop"\n')
+
+check_call(['grub2-mkconfig', '-o', '/boot/grub2/grub.cfg'])
 # Create a ZFS storage pool backed by a normal filesystem file.  This
 # is a bad way to configure ZFS for production use but it is
 # convenient for a demo in a VM.
