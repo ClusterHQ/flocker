e466feb3066f5bddac9c817ff34e064f5f90e48c
Adam Dangoor <adamdangoor@gmail.com>
2014-10-09 17:45:53 +0100

    Notes about current state and plans

--- flocker/acceptance/test_deployment.py
+++ flocker/acceptance/test_deployment.py
@@ -23,13 +23,15 @@ class DeploymentTests(TestCase):
     """
     Tests for deploying applications.
 
-    Similar to http://doc-dev.clusterhq.com/gettingstarted/tutorial/moving-applications.html#starting-an-application
+    Similar to http://doc-dev.clusterhq.com/gettingstarted/tutorial/
+    moving-applications.html#starting-an-application
     """
     @_require_installed
     def setUp(self):
         """
         This is an alternative to
-        http://doc-dev.clusterhq.com/gettingstarted/tutorial/vagrant-setup.html#creating-vagrant-vms-needed-for-flocker
+        http://doc-dev.clusterhq.com/gettingstarted/tutorial/
+        vagrant-setup.html#creating-vagrant-vms-needed-for-flocker
         """
         namespace = u"acceptance-tests"
         self.client = NamespacedDockerClient(namespace)
@@ -47,5 +49,15 @@ class DeploymentTests(TestCase):
         Call a 'deploy' utility function with an application and deployment
         config and watch docker ps output.
         """
-        result = check_output([b"flocker-deploy"] + [b"utils.py"] + [b"utils2.py"])
-        self.assertEqual(result, b"%s\n" % (5,))
+        # TODO Write out YAML files or use ready-made ones?
+        # minimal-application.yml as made for the documentation is suitable
+        # but minimal-deployment.yml needs to know node IP addresses
+        # Is it possible to set the docker container's IP addresses? If not,
+        # is it possible to find them?
+        # How do we specify that the containers should be priviledged (so as
+        # to be able to be run inside )
+        # TODO no need to check output, just run the command
+        result = check_output([b"flocker-deploy"] + [b"application.yml"] +
+                              [b"deployment.yml"])
+        # TODO use self.client.list() to check that the application is
+        # deployed onto the right node
