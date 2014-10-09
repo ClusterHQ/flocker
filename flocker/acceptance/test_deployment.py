fbcdada8dbf21c51ac60c0fca4e026c317f60dfb
Adam Dangoor <adamdangoor@gmail.com>
2014-10-09 17:30:06 +0100

    Require flocker-deploy to be installed

--- flocker/acceptance/test_deployment.py
+++ flocker/acceptance/test_deployment.py
@@ -14,14 +14,18 @@ from flocker.node._docker import NamespacedDockerClient
 from flocker.node.testtools import wait_for_unit_state
 from flocker.testtools import random_name
 
+from subprocess import check_output
+
+_require_installed = skipUnless(which("flocker-deploy"),
+                                "flocker-deploy not installed")
+
 class DeploymentTests(TestCase):
     """
     Tests for deploying applications.
 
     Similar to http://doc-dev.clusterhq.com/gettingstarted/tutorial/moving-applications.html#starting-an-application
     """
-    # TODO Skip if docker is not running
-    # see _require_installed in test_deploy_script
+    @_require_installed
     def setUp(self):
         """
         This is an alternative to
@@ -43,6 +47,5 @@ class DeploymentTests(TestCase):
         Call a 'deploy' utility function with an application and deployment
         config and watch docker ps output.
         """
-        from subprocess import check_output
         result = check_output([b"flocker-deploy"] + [b"utils.py"] + [b"utils2.py"])
         self.assertEqual(result, b"%s\n" % (5,))
