de679d01d0ba86a60a46e53433754bb2f1d2f949
Adam Dangoor <adamdangoor@gmail.com>
2014-10-10 09:50:56 +0100

    Get IP addresses for both nodes which are created

--- flocker/acceptance/test_deployment.py
+++ flocker/acceptance/test_deployment.py
@@ -11,6 +11,8 @@ Run with:
 from subprocess import check_output
 from unittest import skipUnless
 
+from docker import Client
+
 from twisted.python.procutils import which
 from twisted.trial.unittest import TestCase
 
@@ -35,22 +37,30 @@ class DeploymentTests(TestCase):
         This is an alternative to
         http://doc-dev.clusterhq.com/gettingstarted/tutorial/
         vagrant-setup.html#creating-vagrant-vms-needed-for-flocker
+
+        This will probably be a utility function
         """
         namespace = u"acceptance-tests"
         self.client = NamespacedDockerClient(namespace)
-        self.node_1_name = random_name()
-        self.node_2_name = random_name()
+        node_1_name = random_name()
+        node_2_name = random_name()
         image = u"openshift/busybox-http-app"
-        d = self.client.add(self.node_1_name, image)
-        d.addCallback(lambda _: self.client.add(self.node_2_name, image))
+        d = self.client.add(node_1_name, image)
+
+        d.addCallback(lambda _: self.client.add(node_2_name, image))
         d.addCallback(lambda _: self.client.list())
-        # wait_for_unit_state?
+        # TODO wait_for_unit_state? Why (not)?
         # add cleanup
-        from docker import Client
-        def get_ips(containers):
+
+        def get_ips(units):
             docker = Client()
-            docker.inspect_container(u'flocker--acceptance-tests--104591187757')['NetworkSettings']['IPAddress']
-            import pdb; pdb.set_trace()
+            prefix = u'flocker--' + namespace + u'--'
+
+            node_1 = docker.inspect_container(prefix + node_1_name)
+            node_2 = docker.inspect_container(prefix + node_2_name)
+
+            self.node_1_ip = node_1['NetworkSettings']['IPAddress']
+            self.node_2_ip = node_2['NetworkSettings']['IPAddress']
 
         d.addCallback(get_ips)
         return d
@@ -63,11 +73,12 @@ class DeploymentTests(TestCase):
         # TODO Write out YAML files or use ready-made ones?
         # minimal-application.yml as made for the documentation is suitable
         # but minimal-deployment.yml needs to know node IP addresses
-        # Is it possible to set the docker container's IP addresses? If not,
-        # is it possible to find them?
+        # Is it possible to set the docker container's IP addresses? Instead
+        # of just finding them
         # How do we specify that the containers should be priviledged (so as
-        # to be able to be run inside )
+        # to be able to be run inside)
         # TODO no need to check output, just run the command
+        import pdb; pdb.set_trace()
         check_output([b"flocker-deploy"] + [b"application.yml"] +
                      [b"deployment.yml"])
         # TODO use self.client.list() to check that the application is
