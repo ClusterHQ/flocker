4c3b92bbc2606d7d0e5c254fea297ad0b66446f7
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-10-21 11:10:09 -0400

    Factor out the duplication in the node functional script tests.

--- flocker/node/functional/test_script.py
+++ flocker/node/functional/test_script.py
@@ -13,29 +13,33 @@ from twisted.trial.unittest import TestCase
 from ... import __version__
 
 
-_require_installed = skipUnless(which("flocker-changestate"),
-                                "flocker-changestate not installed")
+def make_script_tests(executable):
+    """
+    Generate a test suite which applies to any Flocker-installed node script.
+
+    :param bytes executable: The basename of the script to be tested.
+
+    :return: A ``TestCase`` subclass which defines some tests applied to the
+        given executable.
+    """
+    class ScriptTests(TestCase):
+        @skipUnless(which(executable), executable + " not installed")
+        def test_version(self):
+            """
+            The script is a command available on the system path.
+            """
+            result = check_output([executable] + [b"--version"])
+            self.assertEqual(result, b"%s\n" % (__version__,))
+    return ScriptTests
 
 
-class FlockerChangeStateTests(TestCase):
-    """Tests for ``flocker-changestate``."""
-
-    @_require_installed
-    def test_version(self):
-        """
-        ``flocker-changestate`` is a command available on the system path
-        """
-        result = check_output([b"flocker-changestate"] + [b"--version"])
-        self.assertEqual(result, b"%s\n" % (__version__,))
+class FlockerChangeStateTests(make_script_tests(b"flocker-changestate")):
+    """
+    Tests for ``flocker-changestate``.
+    """
 
 
-class FlockerReportStateTests(TestCase):
-    """Tests for ``flocker-reportstate``."""
-
-    @_require_installed
-    def test_version(self):
-        """
-        ``flocker-reportstate`` is a command available on the system path
-        """
-        result = check_output([b"flocker-reportstate"] + [b"--version"])
-        self.assertEqual(result, b"%s\n" % (__version__,))
+class FlockerReportStateTests(make_script_tests(b"flocker-reportstate")):
+    """
+    Tests for ``flocker-reportstate``.
+    """
