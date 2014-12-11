96b4dd1ac25d8a8575d87db2bb8ade2facc66a85
David Gebler <davidgebler@gmail.com>
2014-12-11 15:43:58 +0000

    add additional test for unchanged volume quotas when moving applications

--- flocker/acceptance/test_deployment.py
+++ flocker/acceptance/test_deployment.py
@@ -20,7 +20,7 @@ class DeploymentTests(TestCase):
     moving-applications.html#starting-an-application
     """
     @require_flocker_cli
-    def test_application_volume_quotas(self):
+    def test_application_volume_quotas_changed(self):
         """
         Deploying an application to one node without a defined maximum size
         on its volume and then moving that application to another node with a
@@ -88,6 +88,62 @@ class DeploymentTests(TestCase):
             # now we verify that the second deployment has moved the app and
             # flocker-reportstate on the new host gives the expected maximum
             # size for the deployed app's volume
+            self.assertEqual(state[MONGO_APPLICATION], application)
+
+        nodes.addCallback(deploy_with_quotas)
+        return nodes
+
+    @require_flocker_cli
+    def test_application_volume_quotas(self):
+        """
+        Deploying an application to one node with a defined maximum size
+        on its volume and then moving that application to another node with a
+        deployment configuration that also specifies the same maximum size
+        results in the volume being moved and the configured quota size being
+        applied on the target node after the volume is successfully received.
+        In other words, the defined volume quota size is preserved from one
+        node to the next.
+        """
+        SIZE_100_MB = u"104857600"
+        nodes = get_nodes(self, num_nodes=2)
+
+        def deploy_with_quotas(nodes):
+            node_1, node_2 = nodes
+            application = create_application(
+                MONGO_APPLICATION, MONGO_IMAGE,
+                volume=create_attached_volume(
+                    name=MONGO_APPLICATION,
+                    mountpoint=b'/data/db',
+                    maximum_size=int(SIZE_100_MB)
+                )
+            )
+            config_deployment = {
+                u"version": 1,
+                u"nodes": {
+                    node_1: [MONGO_APPLICATION],
+                    node_2: [],
+                }
+            }
+            config_application = {
+                u"version": 1,
+                u"applications": {
+                    MONGO_APPLICATION: {
+                        u"image": MONGO_IMAGE,
+                        u"volume": {
+                            u"mountpoint": b"/data/db",
+                            u"maximum_size": SIZE_100_MB
+                        }
+                    }
+                }
+            }
+
+            flocker_deploy(self, config_deployment, config_application)
+            state = get_node_state(node_1)
+            self.assertEqual(state[MONGO_APPLICATION], application)
+            config_deployment[u"nodes"][node_2] = [MONGO_APPLICATION]
+            config_deployment[u"nodes"][node_1] = []
+            flocker_deploy(self, config_deployment, config_application)
+            state = get_node_state(node_2)
             self.assertEqual(state[MONGO_APPLICATION], application)
 
         nodes.addCallback(deploy_with_quotas)
