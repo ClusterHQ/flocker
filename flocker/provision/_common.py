f2eed4fad353bece1d796378a2dd13bf0bce8634
Tom Prince <tom.prince@clusterhq.com>
2014-12-16 18:05:51 -0700

    Add missing file.

---
+++ flocker/provision/_common.py
@@ -0,0 +1,21 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+from characteristic import attributes, Attribute
+
+
+@attributes([
+    Attribute('version', default_value=None),
+    Attribute('branch', default_value=None),
+    Attribute('build_server', default_value="http://build.clusterhq.com/"),
+])
+class PackageSource(object):
+    """
+    Source for the installation of a flocker package.
+
+    :ivar version: The version of flocker to install.
+        If not specified, install the most recent version.
+    :ivar branch: The branch from which to install flocker.
+        If not specified, install from the release repository.
+    :ivar build_server: The builderver to install from.
+        Only meaningful if a branch is specified.
+    """
