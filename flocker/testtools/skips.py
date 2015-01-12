8f1cbbb1de4ebbb20dbda58f615515201b465f78
Adam Dangoor <adamdangoor@gmail.com>
2015-01-12 13:15:34 +0000

    Start of change to make tests fail instead of run anyways

--- flocker/testtools/skips.py
+++ flocker/testtools/skips.py
@@ -15,14 +15,16 @@ def ignore_skip(test_object):
     :return: True if ``test_object`` is covered by environment variable
         FLOCKER_TEST_NO_SKIPS, else False.
     """
+    return True
 
 # The following functions need suitable names.
 # Ideally those names would read well, like how:
 # skipUnless(SELENIUM_INSTALLED) reads like "skip unless selenium is installed".
 # One option is to have them called skipIf and skipUnless, like the unittest
 # methods, and then only import lines must be changed.
+# another option is "skipOrFailUnless" and "skipOrFailIf"
 
-def skipUnlessForcedOr(condition, reason):
+def skipUnless2(condition, reason):
     """
     Ignore skipUnless if an environment variable has been set instructing to do
     so.
@@ -31,13 +33,17 @@ def skipUnlessForcedOr(condition, reason):
     """
     def skip_unless_condition_or_forced(function):
         if ignore_skip(test_object=function):
-            return function
+            # instead make the test fail
+            import pdb; pdb.set_trace()
+            return function.fail("HELLO")
+
+            # return function
         else:
             return skipUnless(condition, reason)
 
     return skip_unless_condition_or_forced
 
-def skipUnlessForcedIf(condition, reason):
+def skipIf2(condition, reason):
     """
     Ignore skipIf if an environment variable has been set instructing to do
     so.
@@ -55,8 +61,4 @@ def skipUnlessForcedIf(condition, reason):
 # also handle all raise SkipTests
 # also handle .skip
 
-# follow up issues for skipping setUp when skipping tests
 # add to tox / buildbot reconfiguration
-# check that this will work with tox
-# document this in CONTRIBUTING.rst
-# tests for this
