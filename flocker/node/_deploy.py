a790b5b562a996d02304055d40dbd238a01e6fb1
Itamar Turner-Trauring <itamar@itamarst.org>
2015-01-12 11:13:22 -0500

    Delete no longer relevant comment.

--- flocker/node/_deploy.py
+++ flocker/node/_deploy.py
@@ -651,11 +651,6 @@ def find_volume_changes(hostname, current_state, desired_state):
     :param Deployment desired_state: The new state of the cluster towards which
         the changes are working.
     """
-    # XXX would be updated to deal with datasets/manifestations in
-    # calculation.  Among other issues, this or some previous function
-    # need to deal with the fact that the desired state may well lack
-    # UUIDs for many of the datasets, and they will need to be looked
-    # up/generated as necessary.
     desired_volumes = {node.hostname: set(application.volume for application
                                           in node.applications
                                           if application.volume)
