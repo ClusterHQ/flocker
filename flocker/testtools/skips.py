3b63526f0d87a0f2a6e69c8ee4bd9909a6a5ab6e
Adam Dangoor <adamdangoor@gmail.com>
2015-01-12 14:15:28 +0000

    Document new approach, with fail instead of running test

--- flocker/testtools/skips.py
+++ flocker/testtools/skips.py
@@ -4,61 +4,73 @@
 Utilities to help with skipping tests.
 """
 
-from unittest import skipIf, skipUnless
+from unittest import SkipTest, skip, skipUnless
 
-def ignore_skip(test_object):
+
+def fail_if_skipped(test_object):
     """
-    Check whether to ignore any skip decorators on a test object.
+    Check whether to fail the given test object if it will be skipped.
 
     :param test_object: test method or class with a skip decorator.
 
     :return: True if ``test_object`` is covered by environment variable
         FLOCKER_TEST_NO_SKIPS, else False.
     """
-    return True
+    return False
 
 # The following functions need suitable names.
 # Ideally those names would read well, like how:
-# skipUnless(SELENIUM_INSTALLED) reads like "skip unless selenium is installed".
+# skipUnless(SELENIUM_INSTALLED) reads like "skip unless selenium is
+# installed".
 # One option is to have them called skipIf and skipUnless, like the unittest
 # methods, and then only import lines must be changed.
 # another option is "skipOrFailUnless" and "skipOrFailIf"
 
+
 def skipUnless2(condition, reason):
     """
-    Ignore skipUnless if an environment variable has been set instructing to do
-    so.
+    A test object decorator to skip the test unless ``condition`` evaluates to
+    ``True``. Fail the test if an environment variable says to fail the
+    decorated test if it is going to be skipped.
 
     See unittest.skipUnless for parameter documentation.
     """
-    def skip_unless_condition_or_forced(function):
-        if ignore_skip(test_object=function):
-            # instead make the test fail
-            import pdb; pdb.set_trace()
-            return function.fail("HELLO")
-
-            # return function
+    def fail_or_skip(function):
+        if not condition:
+            if fail_if_skipped(test_object=function):
+                return lambda function: function.fail(reason)
+            else:
+                return skipUnless(condition, reason)
+                # raise SkipTest("HELLO")
+                # return skipUnless(condition, reason)
         else:
-            return skipUnless(condition, reason)
+            return function
 
-    return skip_unless_condition_or_forced
+    return fail_or_skip
 
 def skipIf2(condition, reason):
     """
-    Ignore skipIf if an environment variable has been set instructing to do
-    so.
+    A test object decorator to skip the test if ``condition`` evaluates to
+    ``True``. Fail the test if an environment variable says to fail the
+    decorated test if it is going to be skipped.
 
     See unittest.skipIf for parameter documentation.
     """
-    def skip_if_condition_or_forced(function):
-        if ignore_skip(test_object=function):
-            return function
+    def fail_or_skip(function):
+        if condition:
+            if fail_if_skipped(test_object=function):
+                return lambda function: function.fail(reason)
+            else:
+                raise SkipTest(reason)
         else:
-            return skipIf(condition, reason)
+            return function
 
-    return skip_if_condition_or_forced
+    return fail_or_skip
 
 # also handle all raise SkipTests
 # also handle .skip
 
-# add to tox / buildbot reconfiguration
+# This requires follow-up issues to modify tox configurations and buildbot
+# builders
+
+# all skips will have to be changed
