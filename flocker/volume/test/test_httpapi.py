0214f18c490d4cddbb89e15a2873bcfe37716136
Richard Wall <richard.wall@clusterhq.com>
2015-01-13 10:18:57 +0000

    remove noop tests

--- flocker/volume/test/test_httpapi.py
+++ flocker/volume/test/test_httpapi.py
@@ -22,17 +22,6 @@ class APITestsMixin(object):
     """
     Integration tests for the Dataset Manager API.
     """
-    def test_noop(self):
-        """
-        The ``/noop`` commands return JSON-encoded ``null``.
-        """
-        requesting = self.agent.request(b"GET", b"/noop")
-        requesting.addCallback(readBody)
-        requesting.addCallback(lambda body: self.assertEqual(
-            goodResult(None), loads(body)))
-        return requesting
-
-
     def test_version(self):
         """
         The ``/version`` command returns JSON-encoded ``__version__``.
