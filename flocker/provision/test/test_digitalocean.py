d86b25f0f4769574f9bf469249d5e26d736c1463
Richard Wall <richard.wall@clusterhq.com>
2015-01-14 13:29:46 +0000

    Make it so I can test errors too

--- flocker/provision/test/test_digitalocean.py
+++ flocker/provision/test/test_digitalocean.py
@@ -3,10 +3,13 @@
 """
 Tests for ``flocker.provision._digitalocean``.
 """
-
+import httplib
 import os
+import json
 
 from twisted.trial.unittest import SynchronousTestCase
+
+from libcloud.common.base import JsonResponse
 
 from flocker.provision._digitalocean import DigitalOceanNodeDriverV2
 
@@ -21,14 +24,50 @@ class CannedResponse(object):
         self.object = expected_response
 
 
+class CannedError(CannedResponse, Exception):
+    """
+    """
+
+
+class FakeHTTPLibResponse(object):
+    reason = ''
+
+    def __init__(self, body, status):
+        self.body = body
+        self.status = status
+
+    def getheaders(self):
+        return []
+
+    def read(self):
+        return self.body
+
+
+def canned_json_response(response_object, response_status=httplib.OK):
+    """
+    """
+    def response():
+        response_content = json.dumps(response_object)
+        return JsonResponse(
+            response=FakeHTTPLibResponse(body=response_content, status=response_status),
+            connection=object()
+        )
+    return response
+
+
+def canned_json_error(response_object, response_status=httplib.NOT_FOUND):
+    return canned_json_response(response_object, response_status)
+
+
 class CannedResponseConnection(object):
     """
     """
-    def __init__(self, expected_response):
-        self._response = expected_response
+    def __init__(self, expected_responses):
+        self._responses = expected_responses
 
-    def request(self, action, method='GET'):
-        return CannedResponse(self._response)
+    def request(self, action):
+        response = self._responses[action]
+        return response()
 
 
 class ListKernelsTestsMixin(object):
@@ -44,6 +83,18 @@ class ListKernelsTestsMixin(object):
         expected_kernels = []
         self.assertEqual(expected_kernels, actual_kernels)
 
+    def test_unknown_droplet_id(self):
+        """
+        ``DigitalOceanNodeDriverV2.list_kernels``
+        ``DigitalOceanKernel`` instances for the supplied ``droplet_id``.
+        """
+        exception = self.assertRaises(
+            Exception,
+            self.driver.list_kernels,
+            droplet_id=''
+        )
+        self.assertEqual('', exception)
+
 
 def make_tests(driver, tests_mixin):
     class Tests(tests_mixin, SynchronousTestCase):
@@ -53,23 +104,30 @@ def make_tests(driver, tests_mixin):
 
 
 canned_connection = CannedResponseConnection(
-    expected_response = {
-        "kernels": [
-          {
-            "id": 231,
-            "name": "DO-recovery-static-fsck",
-            "version": "3.8.0-25-generic"
-          }
-        ],
-        "links": {
-          "pages": {
-            "last": "https://api.digitalocean.com/v2/droplets/3164494/kernels?page=124&per_page=1",
-            "next": "https://api.digitalocean.com/v2/droplets/3164494/kernels?page=2&per_page=1"
-          }
-        },
-        "meta": {
-          "total": 124
-        }
+    expected_responses = {
+        '/droplets/2800208/kernels': canned_json_response({
+            "kernels": [
+              {
+                "id": 231,
+                "name": "DO-recovery-static-fsck",
+                "version": "3.8.0-25-generic"
+              }
+            ],
+            "links": {
+              "pages": {
+                "last": "https://api.digitalocean.com/v2/droplets/3164494/kernels?page=124&per_page=1",
+                "next": "https://api.digitalocean.com/v2/droplets/3164494/kernels?page=2&per_page=1"
+              }
+            },
+            "meta": {
+              "total": 124
+            }
+        }),
+
+        '/droplets//kernels': canned_json_error({
+            "id": "not_found",
+            "message": "The resource you were accessing could not be found."
+        })
     }
 )
 
