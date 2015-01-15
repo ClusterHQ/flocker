bb0844152c272f47f789d6a3124e3f351bef056e
Richard Wall <richard.wall@clusterhq.com>
2015-01-15 10:33:15 +0000

    tidy up

--- flocker/volume/test/test_schemas.py
+++ flocker/volume/test/test_schemas.py
@@ -1,7 +1,7 @@
 # Copyright Hybrid Logic Ltd.  See LICENSE file for details.
 
 """
-Tests for hybridcluster/publicapi/schemas.
+Tests for flocker/volume/schemas.
 """
 
 from twisted.trial.unittest import SynchronousTestCase
@@ -9,31 +9,6 @@ from jsonschema.exceptions import ValidationError
 
 from flocker.restapi._schema import getValidator
 from flocker.volume.httpapi import SCHEMAS
-
-def withoutItem(mapping, key):
-    """
-    Return a shallow copy of C{mapping} without C{key}.
-
-    @param mapping: A copyable mapping.
-    @param key: The key to exclude from the copy.
-    """
-    without = mapping.copy()
-    del without[key]
-    return without
-
-
-
-def withItems(mapping, newValues):
-    """
-    Return a shallow copy of C{mapping} with new values replacing old ones.
-
-    @param mapping: A copyable mapping.
-    @param newValues: A mapping to overwrite existing values.
-    """
-    withAdded = mapping.copy()
-    withAdded.update(newValues)
-    return withAdded
-
 
 
 def buildSchemaTest(name, schema, failingInstances, passingInstances):
@@ -61,7 +36,7 @@ def buildSchemaTest(name, schema, failingInstances, passingInstances):
     for i, inst in enumerate(failingInstances):
         def test(self, inst=inst):
             self.assertRaises(ValidationError,
-                    self.validator.validate, inst)
+                              self.validator.validate, inst)
         test.__name__ = 'test_failsValidation_%d' % (i,)
         body[test.__name__] = test
 
@@ -72,24 +47,6 @@ def buildSchemaTest(name, schema, failingInstances, passingInstances):
         body[test.__name__] = test
 
     return type(name, (SynchronousTestCase, object), body)
-
-
-_FAILING_NODENAMES = [
-    # Not a string.
-    10, {}, [], False,
-
-    # Strings that are not IPv4 addresses.
-    "hello", "1.2.3.4.5", "256.1.2.3", "-1.2.3.4", "1.2.3.4.", ".1.2.3.4",
-
-    # Strings that are not dotted-quad formatted IPv4 addresses.
-    # These are erroneously accepted.
-    # https://www.pivotaltracker.com/story/show/66089428
-    # "127.1", "0x7f000001", "017700000001", "2130706433",
-    ]
-
-_PASSING_NODENAMES = [
-    "1.2.3.4", "255.255.255.255", "1.0.0.1",
-    ]
 
 
 VersionsTests = buildSchemaTest(
