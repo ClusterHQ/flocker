8b21dadaab65b9c2c0d009fed0b46e9b4e69ccd8
Itamar Turner-Trauring <itamar@itamarst.org>
2015-01-15 14:15:35 -0500

    Implement most of persistence functionality.

--- flocker/control/_persistence.py
+++ flocker/control/_persistence.py
@@ -1,13 +1,18 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
 """
 Persistence of cluster configuration.
 """
 
-from twisted.application.service import Service
-from twisted.python.filepath import FilePath
+from json import dumps, loads
 
-from .._config import (
+from twisted.application.service import Service
+
+from ._config import (
     marshal_to_application_config_format, marshal_to_deployment_config_format,
+    deployment_from_configuration_files
     )
+from ._model import Deployment
 
 
 class ConfigurationPersistenceService(Service):
@@ -16,16 +21,24 @@ class ConfigurationPersistenceService(Service):
 
     :ivar Deployment _deployment: The current desired deployment configuration.
     """
-    def __init__(self, path):
+    def __init__(self, reactor, path):
         """
+        :param reactor: Reactor to use for thread pool.
         :param FilePath path: Directory where desired deployment will be
             persisted.
         """
+        self._path = path
 
     def startService(self):
-        # Load application and deployment YAML files from disk, set to
-        # self._deployment.
-        pass
+        if not self._path.exists():
+            self._path.makedirs()
+        self._config_path = self._path.child(b"current_configuration.json")
+        if self._config_path.exists():
+            data = loads(self._config_path.getContent())
+            self._deployment = deployment_from_configuration_files(
+                data[u"applications"], data[u"deployment"])
+        else:
+            self._deployment = Deployment(nodes=frozenset())
 
     def save(self, deployment):
         """
@@ -33,8 +46,18 @@ class ConfigurationPersistenceService(Service):
 
         :return Deferred: Fires when write is finished.
         """
+        data = {
+            u"applications": marshal_to_application_config_format(deployment),
+            u"deployment": marshal_to_deployment_config_format(deployment),
+            }
+        self._config_path.setContent(dumps(data))
+        self._deployment = deployment
+        from twisted.internet.defer import succeed
+        return succeed(None)
 
     def get(self):
         """
         :return Deployment: The current desired configuration.
         """
+        return self._deployment
+
