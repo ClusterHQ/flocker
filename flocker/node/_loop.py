beb6b9cb6216e4cebbd33d0c93c44b0fe8f7520c
Itamar Turner-Trauring <itamar@itamarst.org>
2015-01-23 16:30:27 -0500

    Initial (actually second attempt) sketch of state machine for convergence agent.

---
+++ flocker/node/_loop.py
@@ -0,0 +1,63 @@
+"""
+Convergence loop for the agent.
+
+The convergence agent runs a loop that attempts to converge the node's
+state with the desired configuration as transmitted by the control
+service. This involves two state machines: ClusterStatus and AgentOperation.
+The ClusterStatus state machine receives inputs from the connection to the
+control service, and sends outputs to the AgentOperation state machine.
+
+ClusterStatus has the following states:
+
+STOPPED:
+
+When started, attempt to connect to server, switch to DISCONNECTED:
+
+DISCONNECTED:
+
+The node is not connected to the control service.
+If connected switch to IGNORANT.
+If connection failed try to connect again.
+
+IGNORANT:
+
+The status of the cluster is unknown.
+If desired configuration and cluster state are received send a GO input
+symbol to the AgentOperation state machine. switch to KNOWN.
+If disconnected the switch to DISCONNECTED.
+
+KNOWLEDGEABLE:
+
+If disconnected send a STOP input to AgentOperation and switch to DISCONNECTED.
+
+
+AgentOperation has the following states:
+
+STOPPED:
+
+Nothing going on.
+When GO is received start discovery and switch to DISCOVERING.
+
+DISCOVERING:
+
+Discovery is ongoing.
+When discovery result is received send it (asynchronously) to control
+service, start changing node appropriately, switch to CHANGING.
+If STOP is received switch to DISCOVERING_STOPPED.
+
+DISCOVERING_STOPPED:
+
+If discovery result is received switch to STOPPED.
+If GO is received switch to DISCOVERING.
+
+CHANGING:
+
+Change is ongoing.
+If changes finish start discovery and switch to DISCOVERING.
+If STOP is received switch to CHANGING_STOPPED.
+
+CHANGING_STOPPED:
+
+If changes finish switch to STOPPED.
+If GO is received switch to CHANGING.
+"""
