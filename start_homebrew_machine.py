f0f0740c7bc56602480f9282159c11abf867824e
Adam Dangoor <adamdangoor@gmail.com>
2015-02-25 17:20:57 +0000

    Use upstream-ed vmrun patch

--- start_homebrew_machine.py
+++ start_homebrew_machine.py
@@ -1,20 +1,18 @@
-# Revert the Yosemite virtualmachine to a state where homebrew has
-# just been installed, start the machine and ssh into it.
+"""
+Revert the Yosemite virtualmachine to a state where homebrew has just been
+installed, start the machine and ssh into it. Then start a VM and test a
+Homebrew script in that VM.
 
-# PATH="${PATH}:/Applications/VMware Fusion.app/Contents/Library"
-# YOSEMITE_VMX_PATH="${HOME}/Desktop/Virtual Machines.localized/OS X 10.10.vmwarevm/OS X 10.10.vmx"
-#
-# vmrun revertToSnapshot "${YOSEMITE_VMX_PATH}" homebrew-clean
-# # vmrun start "${YOSEMITE_VMX_PATH}" nogui
-# vmrun start "${YOSEMITE_VMX_PATH}"
-# sleep 100
-#
-#
-# # ssh -o ConnectTimeout=500 ClusterHQVM@172.18.140.54
-# fab --timeout=200 -H ClusterHQVM@172.18.140.54 brew
-#
-# vmrun stop "${YOSEMITE_VMX_PATH}" hard
+The machine must have:
+    * VMWare Fusion installed,
+    * A VMWare OS X VM available at a particular location
 
+The VM must have:
+    * A snapshot with a particular name,
+    * Homebrew installed and available
+"""
+
+# TODO add vmfusion to setup.py and google doc
 from vmfusion import vmrun
 import os
 from flocker.provision._install import run, Run
@@ -23,13 +21,18 @@ from flocker.provision._install import run, Run
 YOSEMITE_VMX_PATH = "{HOME}/Desktop/Virtual Machines.localized/OS X 10.10.vmwarevm/OS X 10.10.vmx".format(HOME=os.environ['HOME'])
 VM_ADDRESS = "172.18.140.54"
 
+# XXX Requires https://github.com/msteinhoff/vmfusion-python/pull/4 to be
+# merged
+vmrun.revertToSnapshot(YOSEMITE_VMX_PATH, 'homebrew-clean')
 vmrun.start(YOSEMITE_VMX_PATH, gui=True)
-# from time import sleep
-# sleep(50)
 
+version = "0.3.3dev6"
+url = "https://raw.githubusercontent.com/ClusterHQ/homebrew-tap/release/flocker-{version}/flocker-{version}.rb".format(version=version)
 update = "brew update"
-install = "brew install https://raw.githubusercontent.com/ClusterHQ/homebrew-tap/release/flocker-0.3.3dev6/flocker-0.3.3dev6.rb"
-test = "brew test flocker-0.3.3dev6"
+install = "brew install " + url
+test = "brew test flocker-{version}".format(version=version)
 
 for command in [update, install, test]:
     run(username="ClusterHQVM", address=VM_ADDRESS, commands=[Run(command=command)])
+
+vmrun.stop(YOSEMITE_VMX_PATH, soft=False)
