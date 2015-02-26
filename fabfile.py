a06d5d2c2b303eef284b596c47cecdefff7894e2
Adam Dangoor <adamdangoor@gmail.com>
2015-02-26 14:21:20 +0000

    Remove untouched files

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-from fabric.api import task, run, local
-
-@task
-def brew():
-    run("brew update")
-    run("brew install https://raw.githubusercontent.com/ClusterHQ/homebrew-tap/release/flocker-0.3.3dev6/flocker-0.3.3dev6.rb")
-    run("brew test flocker-0.3.3dev6")
-
