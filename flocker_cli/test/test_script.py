25eebf642fca719defaba42ca0f75a77b54123bd
David Gebler <davidgebler@gmail.com>
2015-02-26 10:47:33 +0000

    addtional test stubs

--- flocker_cli/test/test_script.py
+++ flocker_cli/test/test_script.py
@@ -1,13 +1,38 @@
 # -*- coding: utf-8 -*-
 # Copyright Hybrid Logic Ltd.  See LICENSE file for details.
 
-from twisted.trial.unittest import TestCase
-from ..testtools import FlockerScriptTestsMixin
+from twisted.trial.unittest import TestCase, SynchronousTestCase
+from ..testtools import FlockerScriptTestsMixin, StandardOptionsTestsMixin
 from ..script import CLIScript, CLIOptions
 
 
-class FlockerDeployTests(FlockerScriptTestsMixin, TestCase):
-    """Tests for ``flocker-deploy``."""
+class FlockerCLITests(FlockerScriptTestsMixin, TestCase):
+    """Tests for ``flocker`` CLI."""
     script = CLIScript
     options = CLIOptions
     command_name = u'flocker'
+
+
+class CLIOptionsTests(StandardOptionsTestsMixin, SynchronousTestCase):
+    """Tests for :class:`CLIOptions`."""
+    options = CLIOptions
+
+
+class FlockerCLIMainTests(TestCase):
+    """
+    Tests for ``CLIScript.main``.
+    """
+    def test_deferred_result(self):
+        """
+        ``CLIScript.main`` returns a ``Deferred`` on success.
+        """
+        options = CLIOptions()
+        options.parseOptions([])
+
+        script = CLIScript()
+        dummy_reactor = object()
+
+        self.assertEqual(
+            None,
+            self.successResultOf(script.main(dummy_reactor, options))
+        )
