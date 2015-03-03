8992d1429f99232525105ca103c434fedf73b277
Itamar Turner-Trauring <itamar@itamarst.org>
2015-03-03 11:41:11 -0500

    Try to be lenient to incoming bytes because YAML library stupidly spits out bytes.

--- flocker/control/_model.py
+++ flocker/control/_model.py
@@ -188,7 +188,7 @@ class Dataset(PRecord):
     :ivar int maximum_size: The maximum size in bytes of this dataset, or
         ``None`` if there is no specified limit.
     """
-    dataset_id = field(mandatory=True, type=unicode)
+    dataset_id = field(mandatory=True, type=unicode, factory=unicode)
     deleted = field(mandatory=True, initial=False, type=bool)
     maximum_size = field(mandatory=True, initial=None)
     metadata = field(mandatory=True, type=PMap, factory=pmap, initial=pmap())
