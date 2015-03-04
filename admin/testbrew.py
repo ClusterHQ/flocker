2671fddce8cffa208a5ab08d0412ba643f09e55c
David Gebler <davidgebler@gmail.com>
2015-03-04 14:49:27 +0000

    use Twisted Options for command line parsing

--- admin/testbrew.py
+++ admin/testbrew.py
@@ -22,6 +22,8 @@ import os
 import sys
 import urllib2
 
+from twisted.python.usage import Options, UsageError
+
 from subprocess import check_output, CalledProcessError
 
 from flocker.provision._install import run_with_fabric, Run
@@ -41,12 +43,47 @@ VM_USERNAME = "ClusterHQVM"
 VM_HOST = "10.0.126.88"
 
 
+class TestBrewOptions(Options):
+    description = ("Start a OS X Virtual Machine and test a brew recipe "
+                   "installatiion.")
+
+    optParameters = [
+        ['vmhost', 'h', VM_HOST,
+         'IP address or hostname of the Virtual Machine'],
+        ['vmuser', 'u', VM_USERNAME,
+         'Username for the Virtual Machine'],
+        ['vmpath', 'p', YOSEMITE_VMX_PATH,
+         'Full path to the Virtual Machine image'],
+        ['vmsnapshot', 's', YOSEMITE_SNAPSHOT,
+         'Snapshot identifier for the Virtual Machine'],
+    ]
+
+    synopsis = ('Usage: testbrew.py <recipe URL> [--vmhost <host>] '
+                '[--vmuser <username>] '
+                '[--vmpath <path>] '
+                '[--vmsnapshot <name>]')
+
+    def parseArgs(self, *args):
+        if len(args) < 1:
+            raise UsageError((
+                "URL of homebrew recipe not specified. "
+                "Run with --help for usage"
+            ))
+        else:
+            self['recipe_url'] = args[0]
+
+
 def main():
     try:
+        # The following line is present just to prevent flake8 warnings.
         BASEPATH, TOPLEVEL
-        if len(sys.argv) < 2:
-            raise Exception("URL of homebrew recipe not specified")
-        recipe_url = sys.argv[1]
+        options = TestBrewOptions()
+        try:
+            options.parseOptions()
+        except UsageError as e:
+            sys.stderr.write("Error: {error}.\n".format(error=str(e)))
+            sys.exit(1)
+        recipe_url = options['recipe_url']
         # Open the recipe URL just to validate and verify that it exists.
         # We do not need to read its content.
         urllib2.urlopen(recipe_url)
@@ -67,13 +104,14 @@ def main():
         print "Done."
         sys.exit(0)
     except CalledProcessError as e:
-        print (
-            "Error: Command {cmd} terminated with exit status {code}.").format(
-            cmd=" ".join(e.cmd), code=e.returncode
+        sys.stderr.write(
+            (
+                "Error: Command {cmd} terminated with exit status {code}.\n"
+            ).format(cmd=" ".join(e.cmd), code=e.returncode)
         )
         sys.exit(1)
     except Exception as e:
-        print "Error: {error}.".format(error=str(e))
+        sys.stderr.write("Error: {error}.\n".format(error=str(e)))
         sys.exit(1)
 
 if __name__ == "__main__":
