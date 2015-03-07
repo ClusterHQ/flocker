ebf544ae685ff34590ceae96956c0dca6efc0fb2
Adam Dangoor <adamdangoor@gmail.com>
2015-03-07 13:12:09 +0000

    Progress on fake Yum tools

--- admin/yum.py
+++ admin/yum.py
@@ -33,6 +33,9 @@ class DownloadPackagesFromRepository(object):
 
 @sync_performer
 def perform_download_packages_from_repository(dispatcher, intent):
+    """
+    See :class:`DownloadPackagesFromRepository`.
+    """
     yum_repo_config = intent.target_path.child(b'build.repo')
     yum_repo_config.setContent(dedent(b"""
          [flocker]
@@ -99,8 +102,9 @@ def perform_create_repository(dispatcher, intent):
         b'--quiet',
         intent.path.path])
 
-    return [os.path.basename(path.path) for path in intent.path.child('repodata').walk()
-            if path.isfile()]
+    # TODO separate this out
+    return [os.path.basename(path.path) for path in
+            intent.path.child('repodata').walk() if path.isfile()]
 
 yum_dispatcher = TypeDispatcher({
     DownloadPackagesFromRepository: perform_download_packages_from_repository,
@@ -113,7 +117,45 @@ class FakeYum(object):
     Enough of a fake implementation of yum utilities to test
     :func:`admin.release.upload_rpms`.
     """
-    # TODO fake sync performers
+    # TODO fill in these sync performers
+    @sync_performer
+    def _perform_download_packages_from_repository(self, dispatcher, intent):
+        """
+        See :class:`DownloadPackagesFromRepository`.
+        """
+        from admin.release import make_rpm_version
+        from admin.packaging import package_filename, PackageTypes
+
+        rpm_version = make_rpm_version(intent.version)
+        versioned_packages = [
+            package_filename(package_type=PackageTypes.RPM,
+                             package=package,
+                             architecture='all',
+                             rpm_version=rpm_version)
+            for package in intent.packages]
+
+        # TODO only return these if there have been changes
+        # TODO put files in target_path
+        for package in versioned_packages:
+            intent.target_path.child(package).setContent(package)
+
+        return versioned_packages
+        # :ivar bytes source_repo: Location of repoisitory.
+        # :ivar FilePath target_path: Directory to download packages to.
+        # :ivar list packages: List of bytes, package names to download.
+        # :ivar bytes version: Version number of Flocker to download packages for.
+
+    @sync_performer
+    def _perform_create_repository(self, dispatcher, intent):
+        """
+        See :class:`CreateRepo`.
+        """
+        xml_file = intent.path.child('repodata').child('repomd.xml')
+        xml_file.parent().makedirs()
+        xml_file.touch()
+
+        return [os.path.basename(path.path) for path in
+                intent.path.child('repodata').walk() if path.isfile()]
 
     def get_dispatcher(self):
         """
@@ -122,6 +164,6 @@ class FakeYum(object):
         """
         return TypeDispatcher({
             DownloadPackagesFromRepository:
-                perform_download_packages_from_repository,
-            CreateRepo: perform_create_repository,
+                self._perform_download_packages_from_repository,
+            CreateRepo: self._perform_create_repository,
         })
