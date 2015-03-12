635207a95c7dbf462f1b1dce8b460cdd1f1ca800
Richard Wall <richard.wall@clusterhq.com>
2015-03-12 12:55:56 +0000

    Import the IBlockDeviceAPI and Loopback implementation from loopback-create-FLOC-1502

---
+++ flocker/node/agents/blockdevice.py
@@ -0,0 +1,263 @@
+# -*- test-case-name: flocker.node.agents.test.test_blockdevice -*-
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+This module implements the parts of a block-device based dataset
+convergence agent that can be re-used against many different kinds of block
+devices.
+"""
+
+from uuid import uuid4
+from subprocess import check_output
+
+from zope.interface import implementer, Interface
+
+from characteristic import attributes
+from pyrsistent import PRecord, field
+
+from twisted.python.filepath import FilePath
+
+
+class VolumeException(Exception):
+    """
+    A base class for exceptions raised by  ``IBlockDeviceAPI`` operations.
+
+    :param unicode blockdevice_id: The unique identifier of the block device.
+    """
+    def __init__(self, blockdevice_id):
+        if not isinstance(blockdevice_id, unicode):
+            raise TypeError(
+                'Unexpected blockdevice_id type. '
+                'Expected unicode. '
+                'Got {!r}.'.format(blockdevice_id)
+            )
+        Exception.__init__(self, blockdevice_id)
+        self.blockdevice_id = blockdevice_id
+
+
+class UnknownVolume(VolumeException):
+    """
+    The block device could not be found.
+    """
+
+
+class AlreadyAttachedVolume(VolumeException):
+    """
+    A failed attempt to attach a block device that is already attached.
+    """
+
+
+class UnattachedVolume(VolumeException):
+    """
+    An attempt was made to operate on an unattached volume but the operation
+    requires the volume to be attached.
+    """
+
+
+class IBlockDeviceAPI(Interface):
+    """
+    Common operations provided by all block device backends.
+    """
+    def create_volume(size):
+        """
+        Create a new block device.
+
+        :param int size: The size of the new block device in bytes.
+        :returns: A ``BlockDeviceVolume``.
+        """
+
+    def attach_volume(blockdevice_id, host):
+        """
+        Attach ``blockdevice_id`` to ``host``.
+
+        :param unicode blockdevice_id: The unique identifier for the block
+            device being attached.
+        :raises UnknownVolume: If the supplied ``blockdevice_id`` does not
+            exist.
+        :raises AlreadyAttachedVolume: If the supplied ``blockdevice_id`` is
+            already attached.
+        :returns: A ``BlockDeviceVolume`` with a ``host`` attribute set to
+            ``host``.
+        """
+
+    def list_volumes():
+        """
+        List all the block devices available via the back end API.
+
+        :returns: A ``list`` of ``BlockDeviceVolume``s.
+        """
+
+    def get_device_path(blockdevice_id):
+        """
+        Calculate the path at which ``blockdevice_id`` will be mounted on the
+        host when attached.
+
+        :param unicode blockdevice_id: The unique identifier for the block
+            device.
+        :returns: A ``BlockDeviceVolume`` with a ``host`` attribute set to
+            ``host``.
+
+        """
+
+
+class BlockDeviceVolume(PRecord):
+    """
+    A block device that may be attached to a host.
+
+    :ivar unicode blockdevice_id: The unique identifier of the block device.
+    :ivar int size: The size of the block device.
+    :ivar bytes host: The IP address of the host to which the block device is
+        attached or ``None`` if it is currently unattached.
+    """
+    blockdevice_id = field(type=unicode, mandatory=True)
+    size = field(type=int, mandatory=True)
+    # XXX: Should be hostname, for consistency of is host a better name since
+    # we currently only expect IP addresses?
+    host = field(type=(bytes, type(None)), initial=None)
+
+
+def device_for_path(file_path):
+    """
+    :param FilePath file_path: A path which may be associated with a loopback
+        device.
+    :returns: A ``FilePath`` to the loopback device if one is found, or
+        ``None`` if no device exists.
+    """
+    device_path = check_output(
+        ["losetup", "--noheadings", "--output", "name", "--associated",
+         file_path.path]
+    ).strip()
+    if device_path != b"":
+        return FilePath(device_path.strip())
+    return None
+
+
+@implementer(IBlockDeviceAPI)
+@attributes(['root_path'])
+class LoopbackBlockDeviceAPI(object):
+    """
+    A simulated ``IBlockDeviceAPI`` which creates loopback devices backed by
+    files located beneath the supplied ``root_path``.
+    """
+    _attached_directory_name = 'attached'
+    _unattached_directory_name = 'unattached'
+
+    @classmethod
+    def from_path(cls, root_path):
+        """
+        :param bytes root_path: The path to a directory in which loop back
+            backing files will be created. The directory is created if it does
+            not already exist.
+        :returns: A ``LoopbackBlockDeviceAPI`` with the supplied ``root_path``.
+        """
+        api = cls(root_path=FilePath(root_path))
+        api._initialise_directories()
+        return api
+
+    def _initialise_directories(self):
+        """
+        Create the root and sub-directories in which loopback files will be
+        created.
+        """
+        self._unattached_directory = self.root_path.child(
+            self._unattached_directory_name)
+
+        try:
+            self._unattached_directory.makedirs()
+        except OSError:
+            pass
+
+        self._attached_directory = self.root_path.child(
+            self._attached_directory_name)
+
+        try:
+            self._attached_directory.makedirs()
+        except OSError:
+            pass
+
+    def get_device_path(self, blockdevice_id):
+        volume = self._get(blockdevice_id)
+
+        if volume.host is not None:
+            volume_path = self._attached_directory.descendant(
+                [volume.host, volume.blockdevice_id]
+            )
+            device_path = device_for_path(volume_path)
+            if device_path is None:
+                check_output(["losetup", "--find", volume_path.path])
+
+            return device_for_path(volume_path)
+
+        raise UnattachedVolume(blockdevice_id)
+
+    def create_volume(self, size):
+        """
+        Create a file of some size and put it in the ``unattached`` directory.
+
+        See ``IBlockDeviceAPI.create_volume`` for parameter and return type
+        documentation.
+        """
+        volume = BlockDeviceVolume(
+            blockdevice_id=unicode(uuid4()),
+            size=size,
+        )
+        self._unattached_directory.child(
+            volume.blockdevice_id.encode('ascii')
+        ).setContent(b'\0' * volume.size)
+        return volume
+
+    def _get(self, blockdevice_id):
+        for volume in self.list_volumes():
+            if volume.blockdevice_id == blockdevice_id:
+                return volume
+        raise UnknownVolume(blockdevice_id)
+
+    def attach_volume(self, blockdevice_id, host):
+        """
+        Move an existing ``unattached`` file into a per-host directory.
+
+        See ``IBlockDeviceAPI.attach_volume`` for parameter and return type
+        documentation.
+        """
+        volume = self._get(blockdevice_id)
+        if volume.host is None:
+            old_path = self._unattached_directory.child(blockdevice_id)
+            host_directory = self._attached_directory.child(host)
+            attached_volume = volume.set(host=host)
+            try:
+                host_directory.makedirs()
+            except OSError:
+                pass
+            old_path.moveTo(host_directory.child(blockdevice_id))
+            return attached_volume
+
+        raise AlreadyAttachedVolume(blockdevice_id)
+
+    def list_volumes(self):
+        """
+        Return ``BlockDeviceVolume`` instances for all the files in the
+        ``unattached`` directory and all per-host directories.
+
+        See ``IBlockDeviceAPI.list_volumes`` for parameter and return type
+        documentation.
+        """
+        volumes = []
+        for child in self.root_path.child('unattached').children():
+            volume = BlockDeviceVolume(
+                blockdevice_id=child.basename().decode('ascii'),
+                size=child.getsize(),
+            )
+            volumes.append(volume)
+
+        for host_directory in self.root_path.child('attached').children():
+            host_name = host_directory.basename().encode('ascii')
+            for child in host_directory.children():
+
+                volume = BlockDeviceVolume(
+                    blockdevice_id=child.basename().decode('ascii'),
+                    size=child.getsize(),
+                    host=host_name,
+                )
+                volumes.append(volume)
+
+        return volumes
---
+++ flocker/node/agents/test/test_blockdevice.py
@@ -0,0 +1,337 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Tests for ``flocker.node.agents.blockdevice``.
+"""
+
+from uuid import uuid4
+
+from zope.interface.verify import verifyObject
+
+from ..blockdevice import (
+    LoopbackBlockDeviceAPI, IBlockDeviceAPI,
+    BlockDeviceVolume, UnknownVolume, AlreadyAttachedVolume,
+    UnattachedVolume
+)
+
+from twisted.python.filepath import FilePath
+from twisted.trial.unittest import SynchronousTestCase
+
+
+class IBlockDeviceAPITestsMixin(object):
+    """
+    Tests to perform on ``IBlockDeviceAPI`` providers.
+    """
+    def test_interface(self):
+        """
+        ``api`` instances provide ``IBlockDeviceAPI``.
+        """
+        self.assertTrue(
+            verifyObject(IBlockDeviceAPI, self.api)
+        )
+
+    def test_list_volume_empty(self):
+        """
+        ``list_volumes`` returns an empty ``list`` if no block devices have
+        been created.
+        """
+        self.assertEqual([], self.api.list_volumes())
+
+    def test_created_is_listed(self):
+        """
+        ``create_volume`` returns a ``BlockVolume`` that is returned by
+        ``list_volumes``.
+        """
+        new_volume = self.api.create_volume(size=1000)
+        self.assertIn(new_volume, self.api.list_volumes())
+
+    def test_attach_unknown_volume(self):
+        """
+        An attempt to attach an unknown ``BlockDeviceVolume`` raises
+        ``UnknownVolume``.
+        """
+        self.assertRaises(
+            UnknownVolume,
+            self.api.attach_volume,
+            blockdevice_id=unicode(uuid4()),
+            host=b'192.0.2.123'
+        )
+
+    def test_attach_attached_volume(self):
+        """
+        An attempt to attach an already attached ``BlockDeviceVolume`` raises
+        ``AlreadyAttachedVolume``.
+        """
+        host = b'192.0.2.123'
+        new_volume = self.api.create_volume(size=1234)
+        attached_volume = self.api.attach_volume(
+            new_volume.blockdevice_id, host=host
+        )
+
+        self.assertRaises(
+            AlreadyAttachedVolume,
+            self.api.attach_volume,
+            blockdevice_id=attached_volume.blockdevice_id,
+            host=host
+        )
+
+    def test_attach_elsewhere_attached_volume(self):
+        """
+        An attempt to attach a ``BlockDeviceVolume`` already attached to
+        another host raises ``AlreadyAttachedVolume``.
+        """
+        new_volume = self.api.create_volume(size=1234)
+        attached_volume = self.api.attach_volume(
+            new_volume.blockdevice_id, host=b'192.0.2.123'
+        )
+
+        self.assertRaises(
+            AlreadyAttachedVolume,
+            self.api.attach_volume,
+            blockdevice_id=attached_volume.blockdevice_id,
+            host=b'192.0.2.124'
+        )
+
+    def test_attach_unattached_volume(self):
+        """
+        An unattached ``BlockDeviceVolume`` can be attached.
+        """
+        expected_host = b'192.0.2.123'
+        new_volume = self.api.create_volume(size=1000)
+        expected_volume = BlockDeviceVolume(
+            blockdevice_id=new_volume.blockdevice_id,
+            size=new_volume.size,
+            host=expected_host,
+        )
+        attached_volume = self.api.attach_volume(
+            blockdevice_id=new_volume.blockdevice_id,
+            host=expected_host
+        )
+        self.assertEqual(expected_volume, attached_volume)
+
+    def test_attached_volume_listed(self):
+        """
+        An attached ``BlockDeviceVolume`` is listed.
+        """
+        expected_host = b'192.0.2.123'
+        new_volume = self.api.create_volume(size=1000)
+        expected_volume = BlockDeviceVolume(
+            blockdevice_id=new_volume.blockdevice_id,
+            size=new_volume.size,
+            host=expected_host,
+        )
+        self.api.attach_volume(
+            blockdevice_id=new_volume.blockdevice_id,
+            host=expected_host
+        )
+        self.assertEqual([expected_volume], self.api.list_volumes())
+
+    def test_multiple_volumes_attached_to_host(self):
+        """
+        ``attach_volume`` can attach multiple block devices to a single host.
+        """
+        expected_host = b'192.0.2.123'
+        volume1 = self.api.create_volume(size=1234)
+        volume2 = self.api.create_volume(size=1234)
+        attached_volume1 = self.api.attach_volume(
+            volume1.blockdevice_id, host=expected_host
+        )
+        attached_volume2 = self.api.attach_volume(
+            volume2.blockdevice_id, host=expected_host
+        )
+
+        self.assertEqual(
+            [attached_volume1, attached_volume2],
+            self.api.list_volumes()
+        )
+
+    # def test_get_attached_volume_device(self):
+    #     1/0
+
+    # def test_get_unattached_volume_device(self):
+    #     1/0
+
+    def test_get_device_path_unknown_volume(self):
+        """
+        ``get_device_path`` raises ``UnknownVolume`` if the supplied
+        ``blockdevice_id`` has not been created.
+        """
+        unknown_blockdevice_id = unicode(uuid4())
+        exception = self.assertRaises(
+            UnknownVolume,
+            self.api.get_device_path,
+            unknown_blockdevice_id
+        )
+        self.assertEqual(unknown_blockdevice_id, exception.blockdevice_id)
+
+    def test_get_device_path_unattached_volume(self):
+        """
+        ``get_device_path`` raises ``UnattachedVolume`` if the supplied
+        ``blockdevice_id`` corresponds to an unattached volume.
+        """
+        new_volume = self.api.create_volume(size=1000)
+        exception = self.assertRaises(
+            UnattachedVolume,
+            self.api.get_device_path,
+            new_volume.blockdevice_id
+        )
+        self.assertEqual(new_volume.blockdevice_id, exception.blockdevice_id)
+
+    def test_get_device_path_device(self):
+        """
+        ``get_device_path`` returns a ``FilePath`` to the device representing
+        the attached volume.
+        """
+        new_volume = self.api.create_volume(size=1024 * 50)
+        attached_volume = self.api.attach_volume(
+            new_volume.blockdevice_id,
+            b'192.0.2.123'
+        )
+        device_path = self.api.get_device_path(attached_volume.blockdevice_id)
+        self.assertTrue(
+            device_path.isBlockDevice(),
+            u"Not a block device. Path: {!r}".format(device_path)
+        )
+
+    def test_get_device_path_device_exists(self):
+        """
+        ``get_device_path`` returns the same ``FilePath`` for the volume device
+        when called multiple times.
+        """
+        new_volume = self.api.create_volume(size=1024 * 50)
+        attached_volume = self.api.attach_volume(
+            new_volume.blockdevice_id,
+            b'192.0.2.123'
+        )
+
+        device_path1 = self.api.get_device_path(attached_volume.blockdevice_id)
+        device_path2 = self.api.get_device_path(attached_volume.blockdevice_id)
+
+        self.assertEqual(device_path1, device_path2)
+
+
+def make_iblockdeviceapi_tests(blockdevice_api_factory):
+    """
+    :returns: A ``TestCase`` for with tests that will be performed on the
+       supplied ``IBlockDeviceAPI`` provider.
+    """
+    class Tests(IBlockDeviceAPITestsMixin, SynchronousTestCase):
+        def setUp(self):
+            self.api = blockdevice_api_factory(test_case=self)
+
+    return Tests
+
+
+def loopbackblockdeviceapi_for_test(test_case):
+    """
+    :returns: A ``LoopbackBlockDeviceAPI`` with a temporary root directory
+    created for the supplied ``test_case``.
+    """
+    root_path = test_case.mktemp()
+    return LoopbackBlockDeviceAPI.from_path(root_path=root_path)
+
+
+class LoopbackBlockDeviceAPITests(
+        make_iblockdeviceapi_tests(
+            blockdevice_api_factory=loopbackblockdeviceapi_for_test
+        )
+):
+    """
+    Interface adherence Tests for ``LoopbackBlockDeviceAPI``.
+    """
+
+
+class LoopbackBlockDeviceAPIImplementationTests(SynchronousTestCase):
+    """
+    Implementation specific tests for ``LoopbackBlockDeviceAPI``.
+    """
+    def assertDirectoryStructure(self, directory):
+        """
+        Assert that the supplied ``directory`` has all the sub-directories
+        required by ``LoopbackBlockDeviceAPI``.
+        """
+        attached_directory = directory.child(
+            LoopbackBlockDeviceAPI._attached_directory_name
+        )
+        unattached_directory = directory.child(
+            LoopbackBlockDeviceAPI._unattached_directory_name
+        )
+
+        LoopbackBlockDeviceAPI.from_path(directory.path)
+
+        self.assertTrue(
+            (True, True),
+            (attached_directory.exists(), unattached_directory.exists())
+        )
+
+    def test_initialise_directories(self):
+        """
+        ``from_path`` creates a directory structure if it doesn't already
+        exist.
+        """
+        directory = FilePath(self.mktemp()).child('loopback')
+        self.assertDirectoryStructure(directory)
+
+    def test_initialise_directories_attached_exists(self):
+        """
+        ``from_path`` uses existing attached directory if present.
+        """
+        directory = FilePath(self.mktemp())
+        attached_directory = directory.child(
+            LoopbackBlockDeviceAPI._attached_directory_name
+        )
+        attached_directory.makedirs()
+        self.assertDirectoryStructure(directory)
+
+    def test_initialise_directories_unattached_exists(self):
+        """
+        ``from_path`` uses existing unattached directory if present.
+        """
+        directory = FilePath(self.mktemp())
+        unattached_directory = directory.child(
+            LoopbackBlockDeviceAPI._unattached_directory_name
+        )
+        unattached_directory.makedirs()
+        self.assertDirectoryStructure(directory)
+
+    def test_list_unattached_volumes(self):
+        """
+        ``list_volumes`` returns a ``BlockVolume`` for each unattached volume
+        file.
+        """
+        expected_size = 1234
+        api = loopbackblockdeviceapi_for_test(test_case=self)
+        blockdevice_volume = BlockDeviceVolume(
+            blockdevice_id=unicode(uuid4()),
+            size=expected_size,
+        )
+        (api
+         .root_path.child('unattached')
+         .child(blockdevice_volume.blockdevice_id)
+         .setContent(b'x' * expected_size))
+        self.assertEqual([blockdevice_volume], api.list_volumes())
+
+    def test_list_attached_volumes(self):
+        """
+        ``list_volumes`` returns a ``BlockVolume`` for each attached volume
+        file.
+        """
+        expected_size = 1234
+        expected_host = b'192.0.2.123'
+        api = loopbackblockdeviceapi_for_test(test_case=self)
+
+        blockdevice_id = unicode(uuid4())
+
+        host_dir = api.root_path.child('attached').child(expected_host)
+        host_dir.makedirs()
+        (host_dir
+         .child(blockdevice_id)
+         .setContent(b'x' * expected_size))
+
+        blockdevice_volume = BlockDeviceVolume(
+            blockdevice_id=blockdevice_id,
+            size=expected_size,
+            host=expected_host,
+        )
+
+        self.assertEqual([blockdevice_volume], api.list_volumes())
