61940887f8244f351c80a201aa32aa55617e3776
David Gebler <davidgebler@gmail.com>
2015-03-13 14:57:49 +0000

    updated restart API example

--- flocker/control/httpapi.py
+++ flocker/control/httpapi.py
@@ -432,7 +432,8 @@ class ConfigurationAPIUserV1(object):
             u"create container",
             u"create container with duplicate name",
             u"create container with ports",
-            u"create container with environment"
+            u"create container with environment",
+            u"create container with restart policy"
         ]
     )
     @structured(
