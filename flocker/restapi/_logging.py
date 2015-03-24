061e0e0702025d99956b7dc606ea0bb4fa5c84ea
Itamar Turner-Trauring <itamar@itamarst.org>
2015-03-24 12:04:11 -0400

    Address review comment: Better documentation.

--- flocker/restapi/_logging.py
+++ flocker/restapi/_logging.py
@@ -26,6 +26,8 @@ RESPONSE_CODE = Field.forTypes(
     u"The response code for the request.")
 
 
+# It would be nice if RESPONSE_CODE was in REQUEST instead of
+# JSON_REQUEST; see FLOC-1586.
 REQUEST = ActionType(
     LOG_SYSTEM + u":request",
     [REQUEST_PATH, METHOD],
@@ -35,4 +37,4 @@ JSON_REQUEST = ActionType(
     LOG_SYSTEM + u":json_request",
     [JSON],
     [RESPONSE_CODE, JSON],
-    u"A request containing JSON request and response.")
+    u"A request containing JSON request and response bodies.")
