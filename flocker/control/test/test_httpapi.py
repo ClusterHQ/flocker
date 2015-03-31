b2413488e10514bafab3ab78b881ec09368b2918
Itamar Turner-Trauring <itamar@itamarst.org>
2015-03-31 14:02:06 -0400

    Just enough endpoint.

--- flocker/control/test/test_httpapi.py
+++ flocker/control/test/test_httpapi.py
@@ -3082,3 +3082,37 @@ class ContainerStateTestsMixin(APITestsMixin):
 RealTestsContainerStateAPI, MemoryTestsContainerStateAPI = (
     buildIntegrationTests(ContainerStateTestsMixin, "ContainerStateAPI",
                           _build_app))
+
+
+class NodesStateTestsMixin(APITestsMixin):
+    """
+    Tests for the nodes state endpoint at ``/state/nodes``.
+    """
+    def test_empty(self):
+        """
+        When no nodes are known, the endpoint returns an empty list.
+        """
+        response = []
+        return self.assertResult(
+            b"GET", b"/state/nodes", None, OK, response
+        )
+
+    def test_nodes(self):
+        """
+        All nodes in the current cluster state are returned.
+        """
+        hostname1 = u"192.0.2.101"
+        hostname2 = u"192.0.2.102"
+        self.cluster_state_service.update_node_state(
+            NodeState(hostname=hostname1))
+        self.cluster_state_service.update_node_state(
+            NodeState(hostname=hostname2))
+        return self.assertResultItems(
+            b"GET", b"/state/nodes", None, OK,
+            [{u"hostname": hostname1}, {u"hostname": hostname2}],
+        )
+
+
+RealTestsNodesStateAPI, MemoryTestsNodesStateAPI = (
+    buildIntegrationTests(NodesStateTestsMixin, "NodesStateAPI",
+                          _build_app))
