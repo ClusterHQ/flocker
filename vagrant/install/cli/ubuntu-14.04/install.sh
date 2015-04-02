87a03411d9dae31d34b6b0b30d3a4959e3b6b537
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-04-02 11:25:15 +0100

    Add Vagrantfile's to run CLI installation from indepth/installation docs

---
+++ vagrant/install/cli/ubuntu-14.04/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e -x
+
+FLOCKER_VERSION=$1
+if [ -z "${FLOCKER_VERSION}" ]; then
+	echo "$0 <FLOCKER_VERSION>" >&2
+	exit 1
+fi
+
+VIRTUAL_ENV=flocker-cli-ubuntu
+
+sudo apt-get -y install gcc python2.7 python-virtualenv python2.7-dev
+
+virtualenv --python=/usr/bin/python2.7 ${VIRTUAL_ENV}
+
+${VIRTUAL_ENV}/bin/pip install --upgrade pip
+
+${VIRTUAL_ENV}/bin/pip install https://storage.googleapis.com/archive.clusterhq.com/downloads/flocker/Flocker-${FLOCKER_VERSION}-py2-none-any.whl
+
+version=`${VIRTUAL_ENV}/bin/flocker-deploy --version`
+if [ "${version}" != "${FLOCKER_VERSION}" ]; then
+	echo "Expected version ${FLOCKER_VERSION}, got version ${version}" >&2
+	exit 1
+fi
+
+echo "Flocker CLI ${version} installed."
+
+${VIRTUAL_ENV}/bin/trial flocker.cli
