ab867315423a80506d366d4b83256d75da048d52
Richard Wall <richard.wall@clusterhq.com>
2015-04-16 12:12:15 +0100

    Some ideas for cinder related modules.

---
+++ flocker/node/agents/functional/test_cinder.py
@@ -0,0 +1,6 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Functional tests for ``flocker.node.agents.cinder`` using a real OpenStack
+cluster.
+"""
---
+++ flocker/node/agents/test/test_cinder.py
@@ -0,0 +1,6 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Tests for ``flocker.node.agents.cinder`` against a fake, mimic-based cinder
+API.
+"""
