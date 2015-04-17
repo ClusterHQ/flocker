5766d4757774ff0308408295a4cc092edbacd7bc
David Gebler <davidgebler@gmail.com>
2015-04-17 15:11:01 +0100

    module exports for node certificates

--- flocker/ca/__init__.py
+++ flocker/ca/__init__.py
@@ -5,16 +5,17 @@ A minimal certificate authority.
 """
 
 __all__ = [
-    "CertificateAuthority", "ControlCertificate", "FlockerKeyPair",
-    "PathError", "CertificateAlreadyExistsError", "KeyAlreadyExistsError",
-    "EXPIRY_20_YEARS", "AUTHORITY_CERTIFICATE_FILENAME",
-    "AUTHORITY_KEY_FILENAME", "CONTROL_CERTIFICATE_FILENAME",
-    "CONTROL_KEY_FILENAME"
+    "CertificateAuthority", "ControlCertificate", "NodeCertificate",
+    "FlockerKeyPair", "PathError", "CertificateAlreadyExistsError",
+    "KeyAlreadyExistsError", "EXPIRY_20_YEARS",
+    "AUTHORITY_CERTIFICATE_FILENAME", "AUTHORITY_KEY_FILENAME",
+    "CONTROL_CERTIFICATE_FILENAME", "CONTROL_KEY_FILENAME"
 ]
 
 from ._ca import (
-    CertificateAuthority, ControlCertificate, FlockerKeyPair, PathError,
-    CertificateAlreadyExistsError, KeyAlreadyExistsError,
-    EXPIRY_20_YEARS, AUTHORITY_CERTIFICATE_FILENAME, AUTHORITY_KEY_FILENAME,
+    CertificateAuthority, ControlCertificate, NodeCertificate,
+    FlockerKeyPair, PathError, CertificateAlreadyExistsError,
+    KeyAlreadyExistsError, EXPIRY_20_YEARS,
+    AUTHORITY_CERTIFICATE_FILENAME, AUTHORITY_KEY_FILENAME,
     CONTROL_CERTIFICATE_FILENAME, CONTROL_KEY_FILENAME
 )
