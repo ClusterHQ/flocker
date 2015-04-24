3082fff8ad27dc79f28a32a55e35ac7e753497b5
Richard Wall <richard.wall@clusterhq.com>
2015-04-24 12:59:08 +0100

    Link to a mimic cinder plugin issue

--- flocker/node/agents/test/test_cinder.py
+++ flocker/node/agents/test/test_cinder.py
@@ -5,8 +5,12 @@ Tests for ``flocker.node.agents.cinder`` against a fake, mimic-based cinder
 API.
 """
 
-# Tests for _blockdevicevolume_from_pyrax_volume and any other  helper functions in flocker.node.agent.cinder
-# Maybe start a mimic server and use it to test the pyrax authentication step...although not sure it's worth doing that since we'll already be testing against rackspace.
+# Maybe start a mimic server and use it to test authentication
+# step...although not sure it's worth doing that since we'll already be testing
+# against rackspace.
 # * https://github.com/rackerlabs/mimic
-# Ideally, this unit test module would run all the make_iblockdeviceapi_tests against authenticated_cinder_api which interacting only with mimic, but
-# Mimic doesn't currently fake the cinder APIs (perhaps we could contribute that feature...or perhaps we haven't got time)
+# Ideally, this unit test module would run all the make_iblockdeviceapi_tests
+# against authenticated_cinder_api which interacting only with mimic, but
+# Mimic doesn't currently fake the cinder APIs
+# (perhaps we could contribute that feature...or perhaps we haven't got time)
+# See https://github.com/rackerlabs/mimic/issues/218
