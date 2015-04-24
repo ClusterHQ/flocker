f6cc90d7b0f8e1e70108bb900888050f4c9891e9
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-04-24 14:53:01 -0400

    Note the ticket for improving this behavior.

--- flocker/node/agents/test/test_blockdevice.py
+++ flocker/node/agents/test/test_blockdevice.py
@@ -476,7 +476,7 @@ class BlockDeviceDeployerAlreadyConvergedCalculateChangesTests(
         )
     test_deleted_ignored.skip = (
         "This will pass when the deployer is smart enough to know it should "
-        "not delete things that do not exist."
+        "not delete things that do not exist.  FLOC-1756."
     )
 
 
