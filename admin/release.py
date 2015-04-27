d6673a574717e0e422384754040618df1d3fc483
Adam Dangoor <adamdangoor@gmail.com>
2015-04-27 12:57:35 +0100

    Improve publish_homebrew_recipe docstring

--- admin/release.py
+++ admin/release.py
@@ -351,12 +351,9 @@ def publish_homebrew_recipe(homebrew_repo_url, version, scratch_directory):
     """
     Publish a Homebrew recipe to a Git repository.
 
-    :param git.Repo homebrew_repo: Homebrew tap Git repository.
+    :param git.Repo homebrew_repo: Homebrew tap Git repository. This should
+        be an SSH URL so as not to require a username and password.
     :param bytes version: Version of Flocker to publish a recipe for.
-    :param bytes content: The content of the desired Homebrew recipe.
-
-    :return git.remote.PushInfo: Information about the push to GitHub.
-    # TODO Docstring
     """
     sdist_url = 'https://s3.amazonaws.com/clusterhq-archive/python/Flocker-{}.tar.gz'.format(version)  # noqa
     content = make_recipe(
