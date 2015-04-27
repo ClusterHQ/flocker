bb4abae5b9ec2158c29ceb3932c5bdaa9706ace5
Madhuri Yechuri <madhuri.yechuri@clusterhq.com>
2015-04-27 16:26:21 -0400

    Checkpoint for {create_volume, list_volumes} support for EBS backend.

--- flocker/node/agents/ebs.py
+++ flocker/node/agents/ebs.py
@@ -4,6 +4,7 @@
 An EBS implementation of the ``IBlockDeviceAPI``.
 """
 
+import time
 from uuid import UUID
 from bitmath import Byte, GB
 from boto import ec2
@@ -14,11 +15,15 @@ from zope.interface import implementer
 
 from .blockdevice import IBlockDeviceAPI, BlockDeviceVolume
 
-def ec2_client(zone, access_key_id, secret_access_key):
-    ec2_zones = boto.ec2.get_all_zones([zone])
-    region = ec2_zones[0].region_name
+DATASET_ID_LABEL = u'flocker-dataset-id'
+METADATA_VERSION_LABEL = u'flocker-metadata-version'
+CLUSTER_ID_LABEL = u'flocker-cluster-id'
+
+
+def ec2_client(region, zone, access_key_id, secret_access_key):
     return _EC2(zone=zone,
-                connection=ec2.connect_to_region(region,
+                connection=ec2.connect_to_region(
+                    region,
                     aws_access_key_id=access_key_id,
                     aws_secret_access_key=secret_access_key))
 
@@ -41,8 +46,7 @@ class EBSBlockDeviceAPI(object):
         self.zone = ec2_client.zone
         self.cluster_id = cluster_id
 
-
-    def _blockdevicevolume_from_ebs_volume(ebs_volume):
+    def _blockdevicevolume_from_ebs_volume(self, ebs_volume):
         """
         """
         return BlockDeviceVolume(
@@ -52,26 +56,70 @@ class EBSBlockDeviceAPI(object):
             dataset_id=UUID(ebs_volume.tags[DATASET_ID_LABEL])
         )
 
+    def _wait_for_volume(self, expected_volume,
+                         expected_status=u'available',
+                         time_limit=60):
+        """
+        Wait for up to 60s for volume creation to complete.
+        """
+        start_time = time.time()
+        expected_volume.update()
+        while expected_volume.status != expected_status:
+            elapsed_time = time.time() - start_time
+            if elapsed_time < time_limit:
+                time.sleep(0.1)
+                expected_volume.update()
+            else:
+                raise Exception(
+                'Timed out while waiting for volume. '
+                'Expected Volume: {!r}, '
+                'Expected Status: {!r}, '
+                'Actual Status: {!r}, '
+                'Elapsed Time: {!r}, '
+                'Time Limit: {!r}.'.format(
+                    expected_volume, expected_status,
+                    expected_volume.status, elapsed_time,
+                    time_limit
+                )
+            )
+
+    def _is_cluster_volume(self, cluster_id, ebs_volume):
+        """
+        """
+        actual_cluster_id = ebs_volume.tags.get(CLUSTER_ID_LABEL)
+        if actual_cluster_id is not None:
+            actual_cluster_id = UUID(actual_cluster_id)
+            if actual_cluster_id == cluster_id:
+                return True
+        return False
 
     def create_volume(self, dataset_id, size):
         """
         """
         metadata = {
             METADATA_VERSION_LABEL: '1',
+            CLUSTER_ID_LABEL: unicode(self.cluster_id),
             DATASET_ID_LABEL: unicode(dataset_id),
         }
         requested_volume = self.connection.create_volume(
-            size=Byte(size).to_GB().value, self.zone)
+            size=int(Byte(size).to_GB().value), zone=self.zone)
         self.connection.create_tags([requested_volume.id],
                                     metadata)
-        requested_volume.update()
-        return _blockdevicevolume_from_ebs_volume(requested_volume)
 
+        created_volume = self._wait_for_volume(requested_volume)
+
+        return self._blockdevicevolume_from_ebs_volume(requested_volume)
 
     def list_volumes(self):
         """
         """
-        pass
+        volumes = []
+        for ebs_volume in self.connection.get_all_volumes():
+            if self._is_cluster_volume(self.cluster_id, ebs_volume):
+                volumes.append(
+                    self._blockdevicevolume_from_ebs_volume(ebs_volume)
+                )
+        return volumes
 
     def resize_volume(self, blockdevice_id, size):
         pass
