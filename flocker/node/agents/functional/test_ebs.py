0dc35744b443dabc54f79ff1d4b799d1c3d0b5b0
Madhuri Yechuri <madhuri.yechuri@clusterhq.com>
2015-04-28 22:02:42 -0400

    Add docstrings for test_ebs.py.

--- flocker/node/agents/functional/test_ebs.py
+++ flocker/node/agents/functional/test_ebs.py
@@ -17,14 +17,20 @@ from ..test.test_blockdevice import REALISTIC_BLOCKDEVICE_SIZE
 
 
 def ebsblockdeviceapi_for_test(test_case, cluster_id):
+    """
+    Create an ``EBSBlockDeviceAPI`` for use by tests.
+
+    :param string cluster_id: Flocker cluster id to be used
+        by the current set of tests.
+    """
     return EBSBlockDeviceAPI(
         ec2_client=ec2_client_from_environment(),
         cluster_id=cluster_id
     )
 
 
-# ``EBSBlockDeviceAPI`` only implements the ``create`` and ``list`` parts of
-# ``IBlockDeviceAPI``. Skip the rest of the tests for now.
+# ``EBSBlockDeviceAPI`` only implements the ``create``, ``list``,
+# and ``destroy`` parts of ``IBlockDeviceAPI``.
 @skip_except(
     supported_tests=[
         'test_interface',
@@ -54,6 +60,11 @@ class EBSBlockDeviceAPIInterfaceTests(
     Interface adherence Tests for ``EBSBlockDeviceAPI``.
     """
     def test_foreign_cluster_volume(self):
+        """
+        Test that volumes created for two Flocker clusters
+        using two ``EBSBlockDeviceAPI``s are listed
+        separate in ``list_volumes``.
+        """
         block_device_api2 = ebsblockdeviceapi_for_test(
             test_case=self,
             cluster_id=uuid4(),
@@ -63,7 +74,8 @@ class EBSBlockDeviceAPIInterfaceTests(
 
     def test_foreign_volume(self):
         """
-        Non-Flocker Volumes are not listed.
+        Test that ``list_volumes`` lists only those volumes
+        belonging to the current Flocker cluster.
         """
         ec2_client = ec2_client_from_environment()
         requested_volume = ec2_client.connection.create_volume(
