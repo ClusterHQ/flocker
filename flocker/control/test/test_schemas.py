7ae2ad02c50aada8d658aaedfd30cfa736aa4429
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-05-04 15:28:30 -0400

    Require size to be a multiple of 512 for ext4-related reasons.  Also fix a bug in the schema - divisibleBy has apparently been removed (draft 4).  multipleOf seems to work though.

--- flocker/control/test/test_schemas.py
+++ flocker/control/test/test_schemas.py
@@ -525,8 +525,15 @@ CONFIGURATION_DATASETS_FAILING_INSTANCES = [
     # wrong type for maximum size
     {u"primary": u"10.0.0.1", u"maximum_size": u"123"},
 
-    # too-small value for maximum size
-    {u"primary": u"10.0.0.1", u"maximum_size": 123},
+    # wrong numeric type for maximum size
+    {u"primary": u"10.0.0.1", u"maximum_size": 1024 * 1024 * 64 + 0.5},
+
+    # too-small (but multiple of 512) value for maximum size
+    {u"primary": u"10.0.0.1", u"maximum_size": 1024},
+
+    # Value for maximum_size that is not a multiple of 512 (but is larger than
+    # the minimum allowed)
+    {u"primary": u"10.0.0.1", u"maximum_size": 1024 * 1024 * 64 + 511},
 
     # wrong type for primary
     {u"primary": 10,
