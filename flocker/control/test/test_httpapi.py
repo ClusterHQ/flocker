cd3d3ee0aec386bff8b6c0db4b82784dbf9858bc
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-05-04 15:47:54 -0400

    Account for the schema change: make this smaller by one block instead of one byte.

--- flocker/control/test/test_httpapi.py
+++ flocker/control/test/test_httpapi.py
@@ -2362,13 +2362,13 @@ class UpdateSizeDatasetTestsMixin(APITestsMixin):
         """
         return self.assert_dataset_resize(
             original_size=67108864,
-            new_size=67108864 - 1,
+            new_size=67108864 - 512,
             expected_code=BAD_REQUEST,
             expected_result={
                 u'description':
                 u"The provided JSON doesn't match the required schema.",
                 u'errors':
-                [u'67108863 is less than the minimum of 67108864']
+                [u'67108352 is less than the minimum of 67108864']
             }
         )
 
