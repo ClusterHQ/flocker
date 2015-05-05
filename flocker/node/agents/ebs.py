254eca971d6d64655c9f9325be7ddaf97c24d2cc
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-05-05 15:52:49 -0400

    Also construct BlockDeviceVolume correctly (x2!).

--- flocker/node/agents/ebs.py
+++ flocker/node/agents/ebs.py
@@ -62,8 +62,7 @@ def _blockdevicevolume_from_ebs_volume(ebs_volume):
     return BlockDeviceVolume(
         blockdevice_id=unicode(ebs_volume.id),
         size=int(GB(ebs_volume.size).to_Byte().value),
-        # cloud_instance_id here too
-        host=None,
+        storage_backend_id=None,
         dataset_id=UUID(ebs_volume.tags[DATASET_ID_LABEL])
     )
 
