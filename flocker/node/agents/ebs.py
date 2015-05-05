7a67f18ff38b896513456c221c06001a6207d9c5
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-05-05 15:50:28 -0400

    AWS implementation, probably.

--- flocker/node/agents/ebs.py
+++ flocker/node/agents/ebs.py
@@ -12,6 +12,7 @@ from bitmath import Byte, GB
 from pyrsistent import PRecord, field
 from zope.interface import implementer
 from boto import ec2
+from boto.utils import get_instance_metadata
 
 from .blockdevice import IBlockDeviceAPI, BlockDeviceVolume, UnknownVolume
 
@@ -145,24 +146,11 @@ class EBSBlockDeviceAPI(object):
         self.zone = ec2_client.zone
         self.cluster_id = cluster_id
 
-    # This is now a free function....remove
-    def _blockdevicevolume_from_ebs_volume(self, ebs_volume):
+    def storage_backend_id(self):
         """
-        Helper function to convert Volume information from
-        EBS format to Flocker block device format.
+        Look up the EC2 instance ID for this node.
         """
-        return BlockDeviceVolume(
-            blockdevice_id=unicode(ebs_volume.id),
-            size=int(GB(ebs_volume.size).to_Byte().value),
-            host=None,
-            dataset_id=UUID(ebs_volume.tags[DATASET_ID_LABEL])
-        )
-
-    # def cloud_instance_id(self):
-    #     """
-    #     Look up the EC2 instance ID....somehow...maybe boto has an API for
-    #     this.
-    #     """
+        return get_instance_metadata()['instance-id'].decode("ascii")
 
     def create_volume(self, dataset_id, size):
         """
