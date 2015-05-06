002a016915c2275e445cc75880555f1c30858387
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-05-06 15:24:50 -0400

    Define some test-case-names.

--- flocker/node/agents/cinder.py
+++ flocker/node/agents/cinder.py
@@ -1,3 +1,4 @@
+# -*- test-case-name: flocker.node.agents.functional.test_cinder,flocker.node.agents.functional.test_cinder_behaviour -*- # noqa
 # Copyright Hybrid Logic Ltd.  See LICENSE file for details.
 
 """
