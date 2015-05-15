e2d8800a21d47eeef8a4b334916a9256326d976c
Adam Dangoor <adamdangoor@gmail.com>
2015-05-15 15:44:55 +0100

    Remove done TODOs

--- flocker/node/script.py
+++ flocker/node/script.py
@@ -267,10 +267,8 @@ def flocker_dataset_agent_main():
     # XXX This should use dynamic dispatch in the deployer_factory instead
     # of parsing the options here, FLOC-1791. One problem here is this needs
     # a valid dataset section of the config file.
-    # TODO submit debugging docs
     # TODO change tests which reference ZFSAgentOptions
     # TODO remove refences to flocker-zfs-agent
-    # TODO log time
     options = DatasetAgentOptions()
     if options['dataset']['backend'] == 'zfs':
         return FlockerScriptRunner(
