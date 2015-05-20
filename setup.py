704316db2c718a67312d63b1951b5435ff58706c
Itamar Turner-Trauring <itamar@itamarst.org>
2015-05-20 09:51:17 -0400

    Add full required library set for TLS, and update to newer pyOpenSSL.

--- setup.py
+++ setup.py
@@ -90,6 +90,10 @@ install_requirements = [
     "pytz",
     "characteristic >= 14.1.0",
     "Twisted == 15.1.0",
+    # TLS support libraries for Twisted:
+    "service_identity == 14.0.0",
+    "idna == 2.0",
+    "pyOpenSSL == 0.15.1",
 
     "PyYAML == 3.10",
 
@@ -103,7 +107,6 @@ install_requirements = [
     "klein == 0.2.3",
     "pyrsistent == 0.9.1",
     "pycrypto == 2.6.1",
-    "pyOpenSSL == 0.14",
     "effect==0.1a13",
     "bitmath==1.2.3-4",
     "boto==2.38.0",
