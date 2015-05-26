ee20f1245a1157f81a01686d6cb1d68503115d73
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-05-26 12:14:20 -0400

    Comments

--- vagrant/tutorial/bootstrap.sh
+++ vagrant/tutorial/bootstrap.sh
@@ -6,13 +6,21 @@
 set -e
 
 yum update -y
-# Install useful yum repos
+
+# Install a repository which has ZFS packages.
 yum install -y https://s3.amazonaws.com/archive.zfsonlinux.org/epel/zfs-release$(rpm -E %dist).noarch.rpm
+
+# Update the kernel and install some development tools necessary for building
+# the ZFS kernel module.
 yum install -y kernel-devel kernel dkms gcc make
+
+# Install a repository that provides epel packages/updates.
 yum install -y epel-release
 
-# Rebuild VirtualBox Additions
+# The kernel was just upgraded which means the existing VirtualBox Guest
+# Additions will no longer work.  Build them again against the new version of
+# the kernel.
 /etc/init.d/vboxadd setup
 
-# Create the 'docker' group
+# Create the 'docker' group (???)
 groupadd docker
