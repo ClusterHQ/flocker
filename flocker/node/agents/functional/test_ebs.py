2cd294d405431a2f0f7045e8a2c59ea7c8df8368
Richard Wall <richard.wall@clusterhq.com>
2015-05-26 19:36:06 +0100

    Just skip the tests that we *don't* expect to pass

--- flocker/node/agents/functional/test_ebs.py
+++ flocker/node/agents/functional/test_ebs.py
@@ -31,43 +31,6 @@ def ebsblockdeviceapi_for_test(test_case):
     return get_blockdeviceapi_with_cleanup(test_case, ProviderType.aws)
 
 
-# ``EBSBlockDeviceAPI`` only implements the ``create``, ``list``,
-# and ``destroy`` parts of ``IBlockDeviceAPI``.
-@skip_except(
-    supported_tests=[
-        'test_attach_attached_volume',
-        'test_interface',
-        'test_created_is_listed',
-        'test_created_volume_attributes',
-        'test_list_volume_empty',
-        'test_listed_volume_attributes',
-        'test_foreign_cluster_volume',
-        'test_foreign_volume',
-        'test_destroy_unknown_volume',
-        'test_destroy_volume',
-        'test_destroy_destroyed_volume',
-        'test_attach_attached_volume',
-        'test_attach_unknown_volume',
-        'test_attach_elsewhere_attached_volume',
-        'test_attach_unattached_volume',
-        'test_attach_destroyed_volume',
-        'test_attached_volume_listed',
-        'test_list_attached_and_unattached',
-        'test_detach_detached_volume',
-        'test_detach_unknown_volume',
-        'test_reattach_detached_volume',
-        'test_multiple_volumes_attached_to_host',
-        'test_compute_instance_id_nonempty',
-        'test_compute_instance_id_unicode',
-        'test_get_device_path_device',
-        'test_get_device_path_device_repeatable_results',
-        'test_get_device_path_unattached_volume',
-        'test_get_device_path_unknown_volume',
-        'test_detach_volume',
-        'test_attach_volume_validate_size',
-        'test_attached_volume_missing_device_tag',
-    ]
-)
 class EBSBlockDeviceAPIInterfaceTests(
         make_iblockdeviceapi_tests(
             blockdevice_api_factory=(
@@ -82,6 +45,16 @@ class EBSBlockDeviceAPIInterfaceTests(
     """
     Interface adherence Tests for ``EBSBlockDeviceAPI``.
     """
+    # We haven't implemented resize functionality yet.
+    def test_resize_destroyed_volume(self):
+        raise SkipTest("Resize not implemented on AWS - FLOC-1985")
+
+    def test_resize_unknown_volume(self):
+        raise SkipTest("Resize not implemented on AWS - FLOC-1985")
+
+    def test_resize_volume_listed(self):
+        raise SkipTest("Resize not implemented on AWS - FLOC-1985")
+
     def test_foreign_volume(self):
         """
         Test that ``list_volumes`` lists only those volumes
