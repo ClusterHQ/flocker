44e25b0b3cf0c8c39ef6ac26f8594798845d44a6
David Gebler <davidgebler@gmail.com>
2015-05-29 13:44:52 +0100

    renamed:    credentials/make.sh -> credentials/make_credentials.sh 	modified:   vagrant-acceptance-targets/centos-7/Vagrantfile

--- admin/credentials/make_credentials.sh
+++ admin/credentials/make_credentials.sh
@@ -4,6 +4,8 @@ read -t 10 -n 1 key
 if [[ $key = y ]]
 then
     rm *.crt *.key
+    rm -rf node1/
+    rm -rf node2/
 else
     echo ""
     echo "Exiting with no action."
@@ -13,8 +15,14 @@ echo ""
 read -p "Enter hostname / IP address of control service: " ip
 flocker-ca initialize tutorialcluster
 flocker-ca create-control-certificate $ip
-flocker-ca create-node-certificate
-flocker-ca create-node-certificate
+mkdir node1
+mkdir node2
+flocker-ca create-node-certificate -o ./node1/
+mv ./node1/*.crt ./node1/node1.crt
+mv ./node1/*.key ./node1/node1.key
+flocker-ca create-node-certificate -o ./node2/
+mv ./node2/*.crt ./node2/node2.crt
+mv ./node2/*.key ./node2/node2.key
 flocker-ca create-api-certificate user
 echo ""
 echo "Done."
