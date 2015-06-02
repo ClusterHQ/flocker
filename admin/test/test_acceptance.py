eb5284ec799884cb307716cd70a61e0a18c98355
Richard Wall <richard.wall@clusterhq.com>
2015-06-02 14:20:32 +0100

    A test for IClusterRunner interface

---
+++ admin/test/test_acceptance.py
@@ -0,0 +1,21 @@
+from zope.interface.verify import verifyObject
+from twisted.trial.unittest import SynchronousTestCase
+
+from ..acceptance import IClusterRunner, ManagedRunner
+
+
+class ManagedRunnerTests(SynchronousTestCase):
+    """
+    Tests for ``ManagedRunner``.
+    """
+    def test_interface(self):
+        """
+        ``ManagedRunner`` provides ``IClusterRunner``.
+        """
+        runner = ManagedRunner(
+            node_addresses=[b'192.0.2.1'],
+            distribution=b'centos-7'
+        )
+        self.assertTrue(
+            verifyObject(IClusterRunner, runner)
+        )
