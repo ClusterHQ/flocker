2352ca3441277114c8f0f0c72d6a9b552d5ee9b9
David Gebler <davidgebler@gmail.com>
2015-06-03 16:42:39 +0100

    modified:   vagrant/tutorial/make-certificates.py

--- vagrant/tutorial/make-certificates.py
+++ vagrant/tutorial/make-certificates.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 # Copyright ClusterHQ Inc.  See LICENSE file for details.
+# Generates a set of cluster, node and user certificates and keys for
+# use with the tutorial Vagrant box.
 
 from twisted.python.filepath import FilePath
 from flocker.provision._ca import Certificates
