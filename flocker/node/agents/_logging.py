679d201ad7569e760e64815860526ce5c530840e
Madhuri Yechuri <madhuri.yechuri@clusterhq.com>
2015-06-12 23:38:53 +0000

    Fix test failure.

--- flocker/node/agents/_logging.py
+++ flocker/node/agents/_logging.py
@@ -141,13 +141,16 @@ COMPUTE_INSTANCE_ID_NOT_FOUND = MessageType(
     u"Unable to determine the instance ID of this node.",
 )
 
+CINDER_LOG_HEADER = u'flocker:node:agents:blockdevice:openstack'
+
 # ActionType used by OpenStack storage driver.
 OPENSTACK_ACTION = ActionType(
-    u"flocker:node:agents:blockdevice:openstack",
+    CINDER_LOG_HEADER,
     [OPERATION],
     [],
     u"An IBlockDeviceAPI operation is executing using OpenStack"
     u"storage driver.")
 
-CINDER_LOG_HEADER = u'flocker:node:agents:blockdevice:cinder'
+CINDER_CREATE_VOLUME = u'flocker:node:agents:blockdevice:openstack:create_volume'
+
 # End: Helper datastructures used by OpenStack storage driver.
