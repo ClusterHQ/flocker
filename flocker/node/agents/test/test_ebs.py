9c09ef195d225cdd0380bafe2f8c4615bab8970b
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-06-18 17:06:58 -0400

    direct unit tests for name conversion

--- flocker/node/agents/test/test_ebs.py
+++ flocker/node/agents/test/test_ebs.py
@@ -7,7 +7,7 @@ Tests for ``flocker.node.agents.ebs``.
 from twisted.python.filepath import FilePath
 from twisted.trial.unittest import SynchronousTestCase
 
-from ..ebs import AttachedUnexpectedDevice
+from ..ebs import AttachedUnexpectedDevice, _expected_device
 
 
 class AttachedUnexpectedDeviceTests(SynchronousTestCase):
@@ -29,4 +29,36 @@ class AttachedUnexpectedDeviceTests(SynchronousTestCase):
         self.assertEqual(
             expected,
             repr(AttachedUnexpectedDevice(requested, discovered))
+        )
+
+
+class ExpectedDeviceTests(SynchronousTestCase):
+    """
+    Tests for ``_expected_device``.
+    """
+    def test_sdX_to_xvdX(self):
+        """
+        ``sdX``-style devices are rewritten to corresponding ``xvdX`` devices.
+        """
+        self.assertEqual(
+            (FilePath(b"/dev/xvdj"), FilePath(b"/dev/xvdo")),
+            (_expected_device(b"/dev/sdj"), _expected_device(b"/dev/sdo")),
+        )
+
+    def test_non_dev_rejected(self):
+        """
+        Devices not in ``/dev`` are rejected with ``ValueError``.
+        """
+        self.assertRaises(
+            ValueError,
+            _expected_device, b"/sys/block/sda",
+        )
+
+    def test_non_sdX_rejected(self):
+        """
+        Devices not in the ``sdX`` category are rejected with ``ValueError``.
+        """
+        self.assertRaises(
+            ValueError,
+            _expected_device, b"/dev/hda",
         )
