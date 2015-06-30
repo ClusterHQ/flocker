121caad3db2816f33e4529e5f76717a71ce3a42b
Itamar Turner-Trauring <itamar@itamarst.org>
2015-06-30 10:50:08 -0400

    Try to pacify the packaging system.

--- admin/packaging.py
+++ admin/packaging.py
@@ -587,6 +587,9 @@ IGNORED_WARNINGS = {
         'non-conffile-in-etc /etc/init/flocker-container-agent.conf',
         'non-conffile-in-etc /etc/init/flocker-control.conf',
 
+        # rsyslog files are not installed as conffiles.
+        'non-conffile-in-etc /etc/rsyslog.d/flocker.conf',
+
         # Cryptography hazmat bindings
         'package-installs-python-pycache-dir opt/flocker/lib/python2.7/site-packages/cryptography/hazmat/bindings/__pycache__/',  # noqa
 
@@ -653,6 +656,9 @@ IGNORED_WARNINGS = {
         'file-in-etc-not-marked-as-conffile etc/init/flocker-dataset-agent.conf',  # noqa
         'file-in-etc-not-marked-as-conffile etc/init/flocker-container-agent.conf',  # noqa
         'file-in-etc-not-marked-as-conffile etc/init/flocker-control.conf',
+
+        # rsyslog files are not installed as conffiles.
+        'file-in-etc-not-marked-as-conffile /etc/rsyslog.d/flocker.conf',
 
         # Cryptography hazmat bindings
         'package-installs-python-pycache-dir opt/flocker/lib/python2.7/site-packages/cryptography/hazmat/bindings/__pycache__/',  # noqa
