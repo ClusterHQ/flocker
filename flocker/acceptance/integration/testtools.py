a46c5f02377fbf9c1d9a592e912b0736c4cd72a5
Itamar Turner-Trauring <itamar@itamarst.org>
2015-07-23 09:47:22 -0400

    Restart acceptance test.

--- flocker/acceptance/integration/testtools.py
+++ flocker/acceptance/integration/testtools.py
@@ -34,7 +34,8 @@ def make_dataset_integration_testcase(image_name, volume_path, internal_port,
         Test that the given application can start and restart with Flocker
         datasets as volumes.
         """
-        def _start_container(self, name, dataset_id, external_port, cluster):
+        def _start_container(self, name, dataset_id, external_port, cluster,
+                             cleanup=True):
             """
             Start a container with a volume.
 
@@ -42,6 +43,7 @@ def make_dataset_integration_testcase(image_name, volume_path, internal_port,
             :param unicode dataset_id: The dataset ID.
             :param cluster: The ``Cluster``.
             :param int external_port: External port to expose on the container.
+            :param bool cleanup: If true, delete container when test is over.
 
             :return: ``Deferred`` that fires when the container has been
                 started.
@@ -57,8 +59,9 @@ def make_dataset_integration_testcase(image_name, volume_path, internal_port,
                               u"mountpoint": volume_path.path}],
             }
             created = cluster.create_container(app)
-            created.addCallback(lambda _: self.addCleanup(
-                cluster.remove_container, name))
+            if cleanup:
+                created.addCallback(lambda _: self.addCleanup(
+                    cluster.remove_container, name))
             return created
 
         @require_cluster(1)
@@ -66,6 +69,11 @@ def make_dataset_integration_testcase(image_name, volume_path, internal_port,
             """
             The specified application can be started with a Docker dataset
             configured as its volume.
+
+            This ensures a newly created dataset meets the requirements of
+            the application being tested. For example, some Docker
+            containers can require a completely empty volume, or one that
+            is writeable by non-root users, etc..
             """
             host = cluster.nodes[0].public_address
             port = 12345
@@ -78,6 +86,39 @@ def make_dataset_integration_testcase(image_name, volume_path, internal_port,
                 lambda _: insert_data(self, host, port))
             creating_dataset.addCallback(
                 lambda _: assert_inserted(self, host, port))
+            return creating_dataset
+
+        @require_cluster(1)
+        def test_restart(self, cluster):
+            """
+            The specified application can be started with a Docker dataset
+            configured as its volume that has already been used by the
+            same application previously.
+            """
+            host = cluster.nodes[0].public_address
+            port = 12345
+            another_port = 12366
+            first_container = random_name(self)
+            creating_dataset = create_dataset(self, cluster)
+
+            def created(dataset):
+                started = self._start_container(first_container,
+                                                dataset[u"dataset_id"],
+                                                port, cluster, cleanup=False)
+                started.addCallback(
+                    lambda _: insert_data(self, host, port))
+
+                restarting = started.addCallback(
+                    lambda _: cluster.remove_container(first_container))
+                restarting.addCallback(
+                    lambda _: self._start_container(random_name(self),
+                                                    dataset[u"dataset_id"],
+                                                    another_port, cluster))
+                return restarting
+            creating_dataset.addCallback(created)
+
+            creating_dataset.addCallback(
+                lambda _: assert_inserted(self, host, another_port))
             return creating_dataset
 
     return IntegrationTests
