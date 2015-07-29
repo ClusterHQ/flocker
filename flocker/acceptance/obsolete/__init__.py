a9ce728468fb09cd65cc21b3e7e3389e88bc744e
David Gebler <davidgebler@gmail.com>
2015-07-29 16:42:57 +0100

    split and move API acceptance tests

---
+++ flocker/acceptance/obsolete/__init__.py
@@ -0,0 +1,6 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Acceptance tests for ``flocker`` components that are either deprecated
+or planned for deprection in the near future.
+"""
