86f58a4be127fbf52e8371c7354dbc1b20aa4910
Richard Wall <richard.wall@clusterhq.com>
2015-08-10 13:55:17 +0100

    Finally got acceptance tests to run...not yet passing

--- flocker/acceptance/endtoend/test_diagnostics.py
+++ flocker/acceptance/endtoend/test_diagnostics.py
@@ -3,41 +3,40 @@
 """
 Tests for the flocker-diagnostics.
 """
+
+from subprocess import check_output
+import tarfile
+
 from twisted.trial.unittest import TestCase
 
-from ...testtools import loop_until
-
-from ..testtools import (
-    require_cluster, require_moving_backend, create_dataset,
-    REALISTIC_BLOCKDEVICE_SIZE,
-)
+from ..testtools import require_cluster
 
 
 class DiagnosticsTests(TestCase):
     """
     Tests for ``flocker-diagnostics``.
     """
-    # @require_cluster(1)
-    def test_export(self):
+    @require_cluster(1)
+    def test_export(self, cluster):
         """
         ``flocker-diagnostics`` creates an archive of all Flocker service logs
         and server diagnostics information.
         """
-        from twisted.internet import reactor
-        from effect.twisted import perform
-        from flocker.provision._ssh._conch import make_dispatcher
-        from flocker.provision._ssh._model import run_remotely, run_from_args
-        dispatcher = make_dispatcher(reactor)
-        remote_command = run_remotely(
-            username='root',
-            address='119.9.110.111',
-            commands=run_from_args(['date']),
-        )
-        running = perform(dispatcher, remote_command)
+        node_address = cluster.control_node.public_address
+        remote_archive_path = check_output(
+            ['ssh',
+             'root@{}'.format(node_address),
+             'flocker-diagnostics']
+        ).rstrip()
 
-        def verify(result):
-            import pdb; pdb.set_trace()
+        local_archive_path = self.mktemp()
 
+        check_output(
+            ['scp',
+             'root@{}:{}'.format(node_address, remote_archive_path),
+             local_archive_path]
+        ).rstrip()
 
-        running.addBoth(verify)
-        return running
+        with tarfile.open(local_archive_path) as f:
+            members = [m.name for m in f.getmembers()]
+            self.assertEqual([], members)
