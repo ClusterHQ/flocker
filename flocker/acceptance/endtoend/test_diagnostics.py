017c6849deffe700beab21e8131cb5dae40e35c1
Richard Wall <richard.wall@clusterhq.com>
2015-08-11 20:42:50 +0100

    SSH options for connecting Ubuntu known hosts and Ubuntu servers

--- flocker/acceptance/endtoend/test_diagnostics.py
+++ flocker/acceptance/endtoend/test_diagnostics.py
@@ -12,6 +12,22 @@ from twisted.trial.unittest import TestCase
 
 from ..testtools import require_cluster
 
+# XXX: Copied from ``flocker.admin.acceptance``.
+# Should probably refactor and re-use ``flocker.admin.runner`` too.
+SSH_OPTIONS = [
+    b"-q",  # suppress warnings
+    # We're ok with unknown hosts.
+    b"-o", b"StrictHostKeyChecking=no",
+    # The tests hang if ControlMaster is set, since OpenSSH won't
+    # ever close the connection to the test server.
+    b"-o", b"ControlMaster=no",
+    # Some systems (notably Ubuntu) enable GSSAPI authentication which
+    # involves a slow DNS operation before failing and moving on to a
+    # working mechanism.  The expectation is that key-based auth will
+    # be in use so just jump straight to that.
+    b"-o", b"PreferredAuthentications=publickey",
+]
+
 
 class DiagnosticsTests(TestCase):
     """
@@ -25,16 +41,16 @@ class DiagnosticsTests(TestCase):
         """
         node_address = cluster.control_node.public_address
         remote_archive_path = check_output(
-            ['ssh',
-             'root@{}'.format(node_address),
+            ['ssh'] + SSH_OPTIONS +
+            ['root@{}'.format(node_address),
              'flocker-diagnostics']
         ).rstrip()
 
         local_archive_path = self.mktemp()
 
         check_output(
-            ['scp',
-             'root@{}:{}'.format(node_address, remote_archive_path),
+            ['scp'] + SSH_OPTIONS +
+            ['root@{}:{}'.format(node_address, remote_archive_path),
              local_archive_path]
         ).rstrip()
 
