34772ccb6488717e88c82eac59e2510e201bd129
Itamar Turner-Trauring <itamar@itamarst.org>
2015-08-12 13:10:06 -0400

    A variety of fixes found by manual testing.

--- flocker/dockerplugin/_script.py
+++ flocker/dockerplugin/_script.py
@@ -4,8 +4,6 @@
 Command to start up the Docker plugin.
 """
 
-from uuid import UUID
-
 from twisted.python.usage import Options
 from twisted.internet.endpoints import serverFromString
 from twisted.application.internet import StreamServerEndpointService
@@ -18,8 +16,7 @@ from ._api import VolumePlugin
 from ..node.script import get_configuration
 from ..apiclient import FlockerClient
 
-
-DEFAULT_CERTIFICATE_PATH = b"/etc/flocker"
+PLUGIN_PATH = FilePath("/run/docker/plugins/flocker/flocker.sock")
 
 
 @flocker_standard_options
@@ -28,8 +25,6 @@ class DockerPluginOptions(Options):
     Command-line options for the Docker plugin.
     """
     optParameters = [
-        ["port", "p", "unix:path=/run/docker/plugins/flocker/flocker.sock",
-         "Port to listen for requests from Docker."],
         ["agent-config", "c", "/etc/flocker/agent.yml",
          "The configuration file for the local agent."],
     ]
@@ -46,18 +41,19 @@ class DockerPluginScript(object):
         # We can use /etc/flocker/agent.yml and /etc/flocker/node.crt to load
         # some information we need:
         agent_config = get_configuration(options)
-        control_host = agent_config['control-service']['host']
+        control_host = agent_config['control-service']['hostname']
         control_port = agent_config['control-service']['port']
-        node_id = UUID(
-            agent_config['node-credential'].credential.certificate.CN)
+        node_id = agent_config['node-credential'].uuid
 
-        certificates_path = options["agent-config"]
+        certificates_path = options["agent-config"].parent()
         flocker_client = FlockerClient(reactor, control_host, control_port,
                                        certificates_path.child(b"cluster.crt"),
                                        certificates_path.child(b"api.crt"),
                                        certificates_path.child(b"api.key"))
 
-        endpoint = serverFromString(reactor, options["port"])
+        PLUGIN_PATH.parent().makedirs()
+        endpoint = serverFromString(
+            reactor, "unix:{}:mode=600".format(PLUGIN_PATH.path))
         service = StreamServerEndpointService(endpoint, Site(
             VolumePlugin(reactor, flocker_client, node_id).app.resource()))
         return main_for_service(reactor, service)
