f7f2a532e4b8812bc76b6aa266180ec0351e2d8c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-08-25 14:32:52 -0400

    Require the exception that we get when we hit ``long_timeout``.  Specifically, do not accept the exception we get if we do not set ``long_timeout`` and instead run into the default connection setup timeout (~40 seconds).

--- flocker/node/functional/test_docker.py
+++ flocker/node/functional/test_docker.py
@@ -11,6 +11,7 @@ import socket
 from functools import partial
 
 from requests import Response
+from requests.exceptions import ReadTimeout
 from docker.errors import APIError
 from docker import Client
 # Docker-py uses 1.16 API by default, which isn't supported by docker, so force
@@ -637,14 +638,14 @@ class GenericDockerClientTests(TestCase):
             app_name = random_name(self)
             d = docker_client.add(app_name, registry_image.full_name)
 
-            # Assert that the timeout triggers requests has a TimeoutError, but
-            # timeout raises a ConnectionError.  Both are subclasses of
-            # IOError, so use that for now
+            # Assert that the timeout triggers.
+            #
+            # requests has a TimeoutError but timeout raises a ConnectionError.
             # https://github.com/kennethreitz/requests/issues/2620
             #
             # XXX DockerClient.add is our API.  We could make it fail with a
             # more coherent exception type if we wanted.
-            self.assertFailure(d, IOError)
+            self.assertFailure(d, ReadTimeout)
             d.addCallback(lambda ignored: (registry_image, registry))
             return d
         running.addCallback(setup_image)
