ca70fff8f0452e755519225814c8ba94489bbdf3
Itamar Turner-Trauring <itamar@itamarst.org>
2015-08-27 13:27:45 -0400

    add hash.

--- flocker/control/test/configurations/configuration_v3.py
+++ flocker/control/test/configurations/configuration_v3.py
@@ -1,7 +1,7 @@
 # Copyright ClusterHQ Inc.  See LICENSE file for details.
 
 # Generate a v3 configuration.
-# Commit Hash: XXX replace once this is actually committed.
+# Hash to recreate: 26ad636ace668dbbfd224fbcd19041af31db80da
 
 from flocker.control._model import Configuration
 from flocker.control._persistence import wire_encode
