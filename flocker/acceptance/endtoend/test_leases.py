2b729a05f8a791378e093494ba3ecc658ece3d1f
David Gebler <davidgebler@gmail.com>
2015-09-02 17:30:59 +0100

    updated test design sketch

--- flocker/acceptance/endtoend/test_leases.py
+++ flocker/acceptance/endtoend/test_leases.py
@@ -34,64 +34,51 @@ class LeaseAPITests(TestCase):
 
         def acquire_lease(dataset):
             # Call the API to acquire a lease with the dataset ID.
+            # Give the lease an expiry of 60 seconds.
             pass
 
-        def attempt_move_dataset(dataset):
-            pass
+        def start_http_container(dataset):
+            # In the 60 seconds before the lease expires, we can launch
+            # the acceptance tests' data HTTP container and make POST requests
+            # to it in a looping call every second.
+            # return looping call deferred
 
-        XXX what do we do here? Some options:
-        * Modify the dataset configuration API:
-          to return an error if the dataset has a lease?
+        def request_move_dataset(self):
+            # We can then request to move the dataset attached to the container.
+            return deferred
 
-        * Wait and timeout?
-          The dataset-agent won't move the dataset because it has a lease so
-          the state will not be updated.
-          But how do we distinguish this from a dataset move which has not
-          yet been performed (delayed for some reason)?
+        def wait_some_amount_of_time(self):
+            # Because the dataset is leased, we should be able to continue
+            # writing data via HTTP requests to the running container.
+            # We should be able to do this for some number of seconds.
+            # The looping call running in parallel to this is continuing to
+            # write data.
 
-        * Check the dataset-agent logs?
-          except that `calculate_changes` doesn't currently log anything if a
-          dataset is leased to nodeA but configured to be on nodeB.
+            # wait some time and return a deferred
 
-        * Don't attempt an acceptance test for this situation.
-          It's already tested in the blockdevice API tests...
-        """
-        self.fail("not implemented yet")
+        def stop_container(self):
+            # We stop the container, so that there is no constraint outside
+            # of leases to prevent the volume from being unmounted.
 
-    @require_moving_backend
-    @require_cluster(2)
-    def test_move_dataset_after_lease_release(self, cluster):
-        """
-        A dataset can be moved once a lease held on it by a
-        particular node is released.
+            def confirmed_stopped(self):
+                # When the container is confirmed as stopped,
+                # restart the container again, to prove the dataset
+                # hasn't moved.
+                # If the dataset had moved, the host path on the volume would
+                # have been unmounted. # XXX is this actually true, does
+                # Flocker remove mountpoint directories?
 
-        ...instead we could write only this test which issues the same API
-        calls that the Docker Plugin is likely to make eg
+        def stop_container_again(self):
+            # Stop the container again.
 
-        Kubernetes schedules Postgres on NodeA with -v PostgresData
-            Docker(NodeA) calls ``VolumePlugin.mount`` PostgresData
-                Flongle responds by:
-                    Create dataset for FooBar
-                    Acquire lease for FooBar on NodeA
-            Docker(NodeA) starts Postgres
+            def stopped_again(self):
+                # Ask for the lease to be released, causing the dataset to move.
+                # Wait a couple of seconds.
 
-        Kubernetes schedules Postgres on NodeB with -v PostgresData
-            Docker(NodaA) stops container
-            Docker(NodeA) calls ``VolumePlugin.umount`` PostgresData
-                Flongle releases lease for FooBar on NodeB
-
-            Docker(NodeB) calls ``VolumePlugin.mount`` PostgresData
-                Flongle
-                    Checks for PostgresData -- exists
-                    Checks for PostgresData leases -- none
-                    Move PostgresData to NodeB
-                    Acquire lease for FooBar on NodeB
-            Docker(NodeB) starts Postgres
-
-        @itamarst, is this what you mean by:
-        > so perhaps try thinking about leases from point of view of users,
-          e.g. docker plugin - how and when will they use it? what would they
-          care about?
+        def try_to_start_again(self):
+            # After a couple of seconds, we can try to recreate the container
+            # and that should fail, because the host mount path for the volume
+            # should no longer exist.
         """
         self.fail("not implemented yet")
 
