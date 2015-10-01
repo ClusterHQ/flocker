4d6fe58117bf94b61ac645db3b5961d76d6f0af6
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-10-01 11:11:34 -0400

    Generalize and apply the same assertion to the agent's log messages.

--- flocker/acceptance/integration/test_platform.py
+++ flocker/acceptance/integration/test_platform.py
@@ -14,12 +14,7 @@ class SyslogTests(TestCase):
     """
     Tests for Flocker's integration with syslog.
     """
-    @require_cluster(1)
-    def test_flocker_control_not_logged(self, cluster):
-        """
-        Log messages from ``flocker-control`` do not appear in syslog.
-        """
-        print 'GET FILE', cluster.control_node
+    def _assert_not_logged(self, fragment):
         getting = cluster.get_file(
             cluster.control_node,
             FilePath(b"/var/log/messages"),
@@ -28,7 +23,21 @@ class SyslogTests(TestCase):
         def got_messages(path):
             with path.open() as messages:
                 for line in messages:
-                    self.assertNotIn("flocker:controlservice", line)
+                    self.assertNotIn(fragment, line)
 
         getting.addCallback(got_messages)
         return getting
+
+    @require_cluster(1)
+    def test_flocker_control_not_logged(self, cluster):
+        """
+        Log messages from ``flocker-control`` do not appear in syslog.
+        """
+        return self._assert_not_logged(b"flocker:controlservice:")
+
+    @require_cluster(1)
+    def test_flocker_agent_not_logged(self, cluster):
+        """
+        Log messages from the Flocker agents do not appear in syslog.
+        """
+        return self._assert_not_logged(b"flocker:agent:")
