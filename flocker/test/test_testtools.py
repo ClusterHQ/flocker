b6c598f2ec1af4c8ac923b60ecc5be2eb44476b7
Jonathan Lange <jml@mumak.net>
2015-10-23 18:34:15 +0100

    Initial failing test for flaky stuff

--- flocker/test/test_testtools.py
+++ flocker/test/test_testtools.py
@@ -1,14 +1,27 @@
-# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+# Copyright ClusterHQ Ltd.  See LICENSE file for details.
 
 """
 Tests for ``flocker.testtools``.
 """
 
 from subprocess import CalledProcessError
+import unittest
+
+from eliot.testing import (
+    capture_logging,
+    LoggedAction, LoggedMessage,
+    assertContainsFields,
+)
+
+from hypothesis import given
+from hypothesis.strategies import integers
 
 from twisted.trial.unittest import SynchronousTestCase
 
-from flocker.testtools import run_process
+from flocker.testtools import (
+    flaky,
+    run_process,
+)
 
 
 class RunProcessTests(SynchronousTestCase):
@@ -68,3 +81,47 @@ class RunProcessTests(SynchronousTestCase):
         """
         exception = self._run_fail(self._failing_shell(b"echo hello"))
         self.assertIn("hello", str(exception))
+
+
+class FlakyTests(SynchronousTestCase):
+    """
+    Tests for ``@flaky`` decorator.
+    """
+
+    # XXX: Should I use this branch to introduce testtools as well?
+
+    @given(integers())
+    def test_decorated_function_executed(self, x):
+        """
+        ``@flaky`` decorates the given function sanely.
+        """
+        values = []
+
+        @flaky
+        def f(x):
+            values.append(x)
+            return x
+
+        y = f(x)
+        self.assertEqual(y, x)
+        self.assertEqual([x], values)
+
+    def test_successful_flaky_test(self):
+        """
+        A successful flaky test is considered successful.
+        """
+
+        # We use 'unittest' here to avoid accidentally depending on Twisted
+        # TestCase features, thus increasing complexity.
+        class SomeTest(unittest.TestCase):
+
+            @flaky
+            def test_something(self):
+                pass
+
+        test = SomeTest('test_something')
+        result = unittest.TestResult()
+
+        test.run(result)
+
+        self.assertEqual({}, result.__dict__)
--- flocker/testtools/__init__.py
+++ flocker/testtools/__init__.py
@@ -942,3 +942,24 @@ def run_process(command, *args, **kwargs):
                 returncode=status, cmd=command, output=output,
             )
     return result
+
+
+def flaky(test_method):
+    """
+    Mark a test as flaky.
+
+    A 'flaky' test is one that sometimes passes but sometimes fails. Marking a
+    test as flaky means both failures and successes are expected, and that
+    neither will fail the test run.
+
+    :param function test_method: The test method to mark as flaky.
+    :return: A decorated version of ``test_method``.
+    """
+    # TODO
+    # - allow specifying which exceptions are expected
+    # - retry on expected exceptions
+    #   - parametrize on retry options
+    #     - perhaps `loop_until` should take a generator of sleep intervals,
+    #       so we can use same routine for backoff
+    # - provide interesting & parseable logs for flaky tests
+    return test_method
