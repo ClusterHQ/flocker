ad07d32f2b4cfef763e9d232352075a7f8f47e84
Itamar Turner-Trauring <itamar@itamarst.org>
2015-10-26 11:34:34 -0400

    On theory that bytes aren't making it out fast enough, try to make really sure HTTP response is sent out before exiting.

--- flocker/acceptance/scripts/exitinghttp.py
+++ flocker/acceptance/scripts/exitinghttp.py
@@ -4,14 +4,19 @@ HTTP server that exits after responding to a GET request.
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import os
+import time
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(s):
         s.send_response(200)
+        s.send_header("content-length", "2")
         s.end_headers()
         s.wfile.write(b"hi")
+        s.wfile.flush()
         s.wfile.close()
+        # Try to ensure enough time for bytes to make it out before exiting:
+        time.sleep(0.1)
         os._exit(1)
 
 httpd = HTTPServer((b"0.0.0.0", 8080), Handler)
