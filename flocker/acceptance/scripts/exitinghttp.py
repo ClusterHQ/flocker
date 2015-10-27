94764ea79042405653a5072afffffa0feda325e3
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-10-27 13:06:13 -0400

    Give back a process-unique value to support client detection of restarts.

--- flocker/acceptance/scripts/exitinghttp.py
+++ flocker/acceptance/scripts/exitinghttp.py
@@ -2,18 +2,23 @@
 HTTP server that exits after responding to a GET request.
 """
 
+from os import urandom
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+PROCESS_UNIQUE_VALUE = urandom(32).encode("hex")
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(s):
         s.send_response(200)
-        s.send_header("content-length", "2")
+        s.send_header(
+            b"content-length",
+            u"{}".format(len(PROCESS_UNIQUE_VALUE)).encode("ascii")
+        )
         s.end_headers()
-        s.wfile.write(b"hi")
+        s.wfile.write(PROCESS_UNIQUE_VALUE)
         s.wfile.flush()
         s.wfile.close()
-        raise SystemExit()
 
 httpd = HTTPServer((b"0.0.0.0", 8080), Handler)
-httpd.serve_forever()
+httpd.handle_request()
