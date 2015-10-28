71fbb682c359735b0cb01336ed4e51681dc17897
Marcus Henry Ewert <user@marcushenryewert.com>
2015-10-28 15:18:14 -0700

    Re-enabling test just to try on buildbot...

--- flocker/acceptance/endtoend/test_dataset.py
+++ flocker/acceptance/endtoend/test_dataset.py
@@ -15,7 +15,7 @@ from ..testtools import (
     require_cluster, require_moving_backend, create_dataset, DatasetBackend
 )
 
-STORAGE_PROFILES_IMPLEMENTED = False
+STORAGE_PROFILES_IMPLEMENTED = True
 
 
 class DatasetAPITests(TestCase):
