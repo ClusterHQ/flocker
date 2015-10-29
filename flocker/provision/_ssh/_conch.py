1a501d3002b9b95602fda9ff314f38ffb73e057e
Jonathan Lange <jml@mumak.net>
2015-10-29 13:40:08 +0000

    Three dots

--- flocker/provision/_ssh/_conch.py
+++ flocker/provision/_ssh/_conch.py
@@ -31,7 +31,7 @@ from twisted.protocols.basic import LineOnlyReceiver
 from twisted.python.filepath import FilePath
 import os
 
-from ..common import loop_until
+from ...common import loop_until
 from ._model import (
     Run, Sudo, Put, Comment, RunRemotely, perform_comment, perform_put,
     perform_sudo)
