ee471549a79de93ddf565b4b9799b1e94a5be494
Marcus Henry Ewert <user@marcushenryewert.com>
2015-11-04 12:09:05 -0800

    test

--- vagrant/tutorial/post-reboot-bootstrap.py
+++ vagrant/tutorial/post-reboot-bootstrap.py
@@ -134,6 +134,9 @@ with file("/etc/NetworkManager/conf.d/auto-reload.conf", "a") as f:
 monitor-connection-files=true
 """)
 
+check_call(['ps', 'aux'])
+check_call(['whoami'])
+check_call(['docker --version'])
 # Install docker images.
 IMAGES = [
     'busybox',
