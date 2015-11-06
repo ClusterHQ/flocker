d78bc3e971ec2df3347508f1a378f615d6cdf75a
Itamar Turner-Trauring <itamar@itamarst.org>
2015-11-06 12:40:40 -0500

    Fix copyright.

--- flocker/control/_protocol.py
+++ flocker/control/_protocol.py
@@ -1,4 +1,4 @@
-# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+# Copyright ClusterHQ Inc.  See LICENSE file for details.
 
 """
 Communication protocol between control service and convergence agent.
