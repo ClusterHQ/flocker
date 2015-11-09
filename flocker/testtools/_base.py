d9bb9f8c3c17ceb4e2dc96bf6e667c88fe90e59e
Jonathan Lange <jml@mumak.net>
2015-11-09 17:46:01 +0000

    Use testtools for asynchronous base

--- flocker/testtools/_base.py
+++ flocker/testtools/_base.py
@@ -4,6 +4,9 @@
 Base classes for unit tests.
 """
 
+import testtools
+from testtools.deferredruntest import AsynchronousDeferredRunTest
+
 from twisted.trial import unittest
 
 
@@ -13,7 +16,23 @@ class TestCase(unittest.SynchronousTestCase):
     """
 
 
-class AsyncTestCase(unittest.TestCase):
+def async_runner(timeout):
+    """
+    Make a ``RunTest`` instance for asynchronous tests.
+
+    :param float timeout: The maximum length of time (in seconds) that a test
+        is allowed to take.
+    """
+    return AsynchronousDeferredRunTest.make_factory(timeout=timeout)
+
+
+# By default, asynchronous tests are timed out after 2 minutes.
+DEFAULT_ASYNC_TIMEOUT = 120
+
+
+class AsyncTestCase(testtools.TestCase):
     """
     Base class for asynchronous test cases.
     """
+
+    run_tests_with = async_runner(timeout=DEFAULT_ASYNC_TIMEOUT)
