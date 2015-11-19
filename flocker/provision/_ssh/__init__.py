300de6f28ae72702c7333332f2f07a5e5b797785
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-11-19 16:12:47 -0500

    Change company name while I am here.

--- flocker/provision/_ssh/__init__.py
+++ flocker/provision/_ssh/__init__.py
@@ -1,4 +1,4 @@
-# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+# Copyright ClusterHQ Inc.  See LICENSE file for details.
 
 from ._model import (
     run_network_interacting_from_args, sudo_network_interacting_from_args,
