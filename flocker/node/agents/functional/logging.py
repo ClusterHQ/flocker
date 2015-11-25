bbb12738af50cfff22d35f996dfd2251ad44b5dc
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-11-25 09:55:23 -0500

    ea

--- flocker/node/agents/functional/logging.py
+++ flocker/node/agents/functional/logging.py
@@ -5,7 +5,7 @@ from eliot import Field, MessageType
 CINDER_VOLUME = MessageType(
     u"flocker:functional:cinder:cinder_volume:created",
     [Field.for_types(
-        u"volume_id", [bytes, unicode],
+        u"id", [bytes, unicode],
         u"The Cinder-assigned unique identifier for the volume that was created.",
     )],
 )
