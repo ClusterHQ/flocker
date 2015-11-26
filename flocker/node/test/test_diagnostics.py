559e8cf20b452785060a56cc43a3db1ca08ee4c6
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-11-26 09:23:54 +0000

    Skip tests if lshw command not present, per code review

--- flocker/node/test/test_diagnostics.py
+++ flocker/node/test/test_diagnostics.py
@@ -1,24 +1,22 @@
 import json
-import platform
-from unittest import skipIf
+from unittest import skipUnless
 
+from twisted.python.procutils import which
 from twisted.trial.unittest import SynchronousTestCase
 
 from flocker.node.diagnostics import list_hardware
 
-on_linux = skipIf(platform.system() != 'Linux', 'Requires Linux')
-
 
 class ListHardwareTests(SynchronousTestCase):
 
-    @on_linux
+    @skipUnless(which('lshw'), 'Tests require the ``lshw`` command.')
     def test_list_hardware(self):
         """
         ``list_hardware`` returns valid JSON.
         """
         json.loads(list_hardware())
 
-    @on_linux
+    @skipUnless(which('lshw'), 'Tests require the ``lshw`` command.')
     def test_list_hardware_classes(self):
         """
         ``list_hardware`` with classes only provides named classes.
