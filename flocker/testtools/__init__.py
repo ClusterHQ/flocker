49467a49480480229c1975394fdf0ce1e1e9b15e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2015-12-02 14:56:51 -0500

    Change REALISTIC_BLOCKDEVICE_SIZE so that it is defined in terms of the size constant.

--- flocker/testtools/__init__.py
+++ flocker/testtools/__init__.py
@@ -79,13 +79,11 @@ from twisted.python.logfile import LogFile
 from ._base import AsyncTestCase, TestCase, async_runner
 from ._flaky import flaky
 from .. import __version__
+from ..common import RACKSPACE_MINIMUM_VOLUME_SIZE
 from ..common.script import (
     FlockerScriptRunner, ICommandLineScript)
 
-# This is currently set to the minimum size for a SATA based Rackspace Cloud
-# Block Storage volume. See:
-# * http://www.rackspace.com/knowledge_center/product-faq/cloud-block-storage
-REALISTIC_BLOCKDEVICE_SIZE = int(GiB(100).to_Byte().value)
+REALISTIC_BLOCKDEVICE_SIZE = RACKSPACE_MINIMUM_VOLUME_SIZE
 
 
 @implementer(IProcessTransport)
