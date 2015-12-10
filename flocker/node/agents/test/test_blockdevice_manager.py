c1e90382e901a192890ce45802cb0002736aea35
Tom Prince <tom.prince@clusterhq.com>
2015-12-10 01:06:47 -0700

    Use the proper type.

--- flocker/node/agents/test/test_blockdevice_manager.py
+++ flocker/node/agents/test/test_blockdevice_manager.py
@@ -34,7 +34,7 @@ class BlockDeviceManagerTests(AsyncTestCase):
         """
         Establish testing infrastructure for test cases.
         """
-        super(BlockDeviceManager, self)
+        super(BlockDeviceManagerTests, self)
         self.loopback_api = loopbackblockdeviceapi_for_test(self)
         self.manager_under_test = BlockDeviceManager()
         self.mountroot = mountroot_for_test(self)
