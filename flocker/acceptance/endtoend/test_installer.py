1fab0b5bed870388f91038859eebaac8c0862315
Ubuntu <ubuntu@ip-172-31-15-175.us-west-1.compute.internal>
2015-12-21 23:11:31 +0000

    Start MySQL and connect to it

--- flocker/acceptance/endtoend/test_installer.py
+++ flocker/acceptance/endtoend/test_installer.py
@@ -22,6 +22,8 @@ class DockerComposeTests(AsyncTestCase):
         """
         """
         client_ip = os.environ.get('CLIENT_IP')
+        docker_host = os.environ.get('DOCKER_HOST')
+        node0 = os.environ.get('CLUSTER_NODE0')
 
         def docker_compose_up():
             docker_compose_output = []
@@ -29,10 +31,22 @@ class DockerComposeTests(AsyncTestCase):
                 reactor,
                 'ubuntu',
                 client_ip,
-                ['docker-compose', '--help'],
+                ['DOCKER_HOST={}'.format(docker_host), 'docker-compose', '-f', '/home/ubuntu/mysql/docker-compose-node0.yml', 'up', '-d'],
                 handle_stdout=docker_compose_output.append
-            ).addCallback(
-                lambda ignored: self.assertEqual("", docker_compose_output)
             )
 
-        return docker_compose_up()
+        def mysql_insert(ignored):
+            mysql_output = []
+            return run_ssh(
+                reactor,
+                'ubuntu',
+                client_ip,
+                ['mysql', '--host', node0, '--port', '3306', '--user', 'root', '--password', 'secret', '--wait'],
+                handle_stdout=mysql_output.append
+            ).addCallback(
+                lambda ignored: self.assertEqual("", mysql_output)
+            )
+
+        d = docker_compose_up()
+        mysql_insert_node0 = d.addCallback(mysql_insert)
+        return mysql_insert_node0
