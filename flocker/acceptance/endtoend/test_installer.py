199c9a131a7a83da676173861c1f804297d025da
Richard Wall <richard.wall@clusterhq.com>
2015-12-21 15:45:19 -0800

    Create a remote database

--- flocker/acceptance/endtoend/test_installer.py
+++ flocker/acceptance/endtoend/test_installer.py
@@ -5,13 +5,15 @@ Tests for AWS CloudFormation installer.
 """
 
 import os
-import tarfile
 
 from twisted.internet import reactor
-from twisted.python.filepath import FilePath
 
-from ...common.runner import run_ssh, download_file
+from ...common.runner import run_ssh
 from ...testtools import AsyncTestCase
+
+CLIENT_IP = os.environ['CLIENT_IP']
+DOCKER_HOST = os.environ['DOCKER_HOST']
+NODE0 = os.environ['CLUSTER_NODE0']
 
 
 class DockerComposeTests(AsyncTestCase):
@@ -21,32 +23,40 @@ class DockerComposeTests(AsyncTestCase):
     def test_docker_compose_up_mysql(self):
         """
         """
-        client_ip = os.environ.get('CLIENT_IP')
-        docker_host = os.environ.get('DOCKER_HOST')
-        node0 = os.environ.get('CLUSTER_NODE0')
+        compose_node0 = '/home/ubuntu/mysql/docker-compose-node0.yml'
+        # MySQL doesn't allow dashes.
+        database_name = 'test_docker_compose_up_mysql'
+
+        docker_compose_output = []
 
         def docker_compose_up():
-            docker_compose_output = []
             return run_ssh(
                 reactor,
                 'ubuntu',
-                client_ip,
-                ['DOCKER_HOST={}'.format(docker_host), 'docker-compose', '-f', '/home/ubuntu/mysql/docker-compose-node0.yml', 'up', '-d'],
+                CLIENT_IP,
+                ['DOCKER_HOST={}'.format(DOCKER_HOST),
+                 'docker-compose', '-f', compose_node0, 'up', '-d'],
                 handle_stdout=docker_compose_output.append
             )
+        d = docker_compose_up()
+
+        mysql_output = []
 
         def mysql_insert(ignored):
-            mysql_output = []
             return run_ssh(
                 reactor,
                 'ubuntu',
-                client_ip,
-                ['mysql', '--host', node0, '--port', '3306', '--user', 'root', '--password', 'secret', '--wait'],
+                CLIENT_IP,
+                ['mysql', '--host=' + NODE0, '--port=3306',
+                 '--user=root', '--password=secret',
+                 '--wait',
+                 '--execute=create database {}'.format(database_name)],
                 handle_stdout=mysql_output.append
-            ).addCallback(
-                lambda ignored: self.assertEqual("", mysql_output)
             )
+        d.addCallback(mysql_insert)
 
-        d = docker_compose_up()
-        mysql_insert_node0 = d.addCallback(mysql_insert)
-        return mysql_insert_node0
+        d.addCallback(
+            lambda ignored: self.assertEqual("", mysql_output)
+        )
+
+        return d
