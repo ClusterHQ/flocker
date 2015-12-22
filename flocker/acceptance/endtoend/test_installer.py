6c7671d56685a397e30655acdea62122e3fc4dba
Richard Wall <richard.wall@clusterhq.com>
2015-12-21 16:48:46 -0800

    Cleanup containers before running tests

--- flocker/acceptance/endtoend/test_installer.py
+++ flocker/acceptance/endtoend/test_installer.py
@@ -7,8 +7,10 @@ Tests for AWS CloudFormation installer.
 import os
 
 from twisted.internet import reactor
+from twisted.internet.defer import maybeDeferred
 
 from ...common.runner import run_ssh
+from ...common import gather_deferreds
 from ...testtools import AsyncTestCase
 
 CLIENT_IP = os.environ['CLIENT_IP']
@@ -19,42 +21,57 @@ COMPOSE_NODE0 = '/home/ubuntu/mysql/docker-compose-node0.yml'
 COMPOSE_NODE1 = '/home/ubuntu/mysql/docker-compose-node1.yml'
 
 
+def remote_docker_compose(compose_file_path, *args):
+    docker_compose_output = []
+    d = run_ssh(
+        reactor,
+        'ubuntu',
+        CLIENT_IP,
+        ('DOCKER_HOST={}'.format(DOCKER_HOST),
+         'docker-compose', '--file', compose_file_path) + args,
+          handle_stdout=docker_compose_output.append
+        )
+    d.addCallback(lambda process_result: (process_result, docker_compose_output))
+    return d
+
+def remote_mysql(host, command):
+    mysql_output = []
+    d = run_ssh(
+        reactor,
+        'ubuntu',
+        CLIENT_IP,
+        ['mysql', '--host=' + host, '--port=3306',
+         '--user=root', '--password=secret',
+         '--wait',
+         '--execute={}'.format(command)],
+         handle_stdout=mysql_output.append
+    )
+    d.addCallback(lambda process_result: (process_result, docker_compose_output))
+    return d
+
 class DockerComposeTests(AsyncTestCase):
     """
     Tests for AWS CloudFormation installer.
     """
+
+    def setUp(self):
+        """
+        """
+        d = maybeDeferred(super(DockerComposeTests, self).setUp)
+        d_node1 = remote_docker_compose(COMPOSE_NODE0, 'stop')
+        d_node1.addCallback(lambda ignored: remote_docker_compose(COMPOSE_NODE0, 'rm', '-f'))
+        d_node2 = remote_docker_compose(COMPOSE_NODE1, 'stop')
+        d_node2.addCallback(lambda ignored: remote_docker_compose(COMPOSE_NODE1, 'rm', '-f'))
+        return gather_deferreds([d, d_node1, d_node2])
+	
     def test_docker_compose_up_mysql(self):
         """
         """
         # MySQL doesn't allow dashes.
         database_name = 'test_docker_compose_up_mysql'
 
-        docker_compose_output = []
 
-        def remote_docker_compose(compose_file_path, *args):
-            return run_ssh(
-                reactor,
-                'ubuntu',
-                CLIENT_IP,
-                ('DOCKER_HOST={}'.format(DOCKER_HOST),
-                 'docker-compose', '--file', compose_file_path) + args,
-                handle_stdout=docker_compose_output.append
-            )
         d = remote_docker_compose(COMPOSE_NODE0, 'up', '-d')
-
-        mysql_output = []
-
-        def remote_mysql(host, command):
-            return run_ssh(
-                reactor,
-                'ubuntu',
-                CLIENT_IP,
-                ['mysql', '--host=' + host, '--port=3306',
-                 '--user=root', '--password=secret',
-                 '--wait',
-                 '--execute={}'.format(command)],
-                handle_stdout=mysql_output.append
-            )
 
         d.addCallback(
             lambda ignored: remote_mysql(
@@ -81,7 +98,7 @@ class DockerComposeTests(AsyncTestCase):
         )
 
         d.addCallback(
-            lambda ignored: self.assertEqual("", mysql_output)
+            lambda (process_status, process_output): self.assertEqual("", process_output)
         )
 
         return d
