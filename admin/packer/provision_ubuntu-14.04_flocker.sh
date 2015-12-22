3d0f4e1b7ad8beb1ff0c7ff203572181cd0e9ff8
Richard Wall <richard.wall@clusterhq.com>
2015-12-21 20:39:30 -0800

    Build separate Ubuntu+Docker and Ubuntu+Flocker images

--- admin/packer/provision_ubuntu-14.04_flocker.sh
+++ admin/packer/provision_ubuntu-14.04_flocker.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Install Docker like Vagrant does
-# https://github.com/mitchellh/vagrant/blob/master/plugins/provisioners/docker/cap/debian/docker_install.rb#L13
-apt-get update -qq -y
-apt-get install -qq -y --force-yes curl
-apt-get purge -qq -y lxc-docker* || true
-curl -sSL https://get.docker.com/ | sh
-
 # Install Flocker
 apt-get install -qq -y apt-transport-https software-properties-common
 if test -z "${flocker_branch}"; then
