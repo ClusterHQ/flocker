65db42ad19dd7e0aa81704f309aa98e9e1a0dcc5
Madhuri Yechuri <madhuri.yechuri@clusterhq.com>
2015-12-22 10:54:07 -0800

    Replace mysql with postgres in installer client.

--- admin/installer/setup_client.sh
+++ admin/installer/setup_client.sh
@@ -5,13 +5,13 @@
 set -ex
 
 apt-get update
-apt-get -y install mysql-client
+sudo apt-get install -y postgresql-client
 
 curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-mkdir -p /home/ubuntu/mysql
+mkdir -p /home/ubuntu/postgres
 
-curl https://raw.githubusercontent.com/ClusterHQ/flocker/b371c64fc5f50801f2cce34d31ea3700f7cab024/admin/installer/mysql/docker-compose-node0.yml > /home/ubuntu/mysql/docker-compose-node0.yml
+curl https://raw.githubusercontent.com/ClusterHQ/flocker/b371c64fc5f50801f2cce34d31ea3700f7cab024/admin/installer/postgres/docker-compose-node0.yml > /home/ubuntu/postgres/docker-compose-node0.yml
 
-chown --recursive ubuntu:ubuntu /home/ubuntu/mysql
+chown --recursive ubuntu:ubuntu /home/ubuntu/postgres
