311fb63c4cbe7d0e0751137672ada593abf22112
Richard Wall <richard.wall@clusterhq.com>
2015-12-21 16:05:20 -0800

    Move the database and check for created database

--- flocker/acceptance/endtoend/test_installer.py
+++ flocker/acceptance/endtoend/test_installer.py
@@ -14,6 +14,9 @@ from ...testtools import AsyncTestCase
 CLIENT_IP = os.environ['CLIENT_IP']
 DOCKER_HOST = os.environ['DOCKER_HOST']
 NODE0 = os.environ['CLUSTER_NODE0']
+NODE1 = os.environ['CLUSTER_NODE1']
+COMPOSE_NODE0 = '/home/ubuntu/mysql/docker-compose-node0.yml'
+COMPOSE_NODE1 = '/home/ubuntu/mysql/docker-compose-node1.yml'
 
 
 class DockerComposeTests(AsyncTestCase):
@@ -23,37 +26,59 @@ class DockerComposeTests(AsyncTestCase):
     def test_docker_compose_up_mysql(self):
         """
         """
-        compose_node0 = '/home/ubuntu/mysql/docker-compose-node0.yml'
         # MySQL doesn't allow dashes.
         database_name = 'test_docker_compose_up_mysql'
 
         docker_compose_output = []
 
-        def docker_compose_up():
+        def remote_docker_compose(compose_file_path, *args):
             return run_ssh(
                 reactor,
                 'ubuntu',
                 CLIENT_IP,
-                ['DOCKER_HOST={}'.format(DOCKER_HOST),
-                 'docker-compose', '-f', compose_node0, 'up', '-d'],
+                ('DOCKER_HOST={}'.format(DOCKER_HOST),
+                 'docker-compose', '--file', compose_file_path) + args,
                 handle_stdout=docker_compose_output.append
             )
-        d = docker_compose_up()
+        d = remote_docker_compose(COMPOSE_NODE0, 'up', '-d')
 
         mysql_output = []
 
-        def mysql_insert(ignored):
+        def remote_mysql(host, command):
             return run_ssh(
                 reactor,
                 'ubuntu',
                 CLIENT_IP,
-                ['mysql', '--host=' + NODE0, '--port=3306',
+                ['mysql', '--host=' + host, '--port=3306',
                  '--user=root', '--password=secret',
                  '--wait',
-                 '--execute=create database {}'.format(database_name)],
+                 '--execute={}'.format(command)],
                 handle_stdout=mysql_output.append
             )
-        d.addCallback(mysql_insert)
+
+        d.addCallback(
+            lambda ignored: remote_mysql(
+                NODE0, 'create database {}'.format(database_name)
+            )
+        )
+
+        d.addCallback(
+            lambda ignored: remote_docker_compose(COMPOSE_NODE0, 'stop')
+        )
+
+        d.addCallback(
+            lambda ignored: remote_docker_compose(
+                COMPOSE_NODE0, 'rm', '--force'
+            )
+        )
+
+        d = remote_docker_compose(COMPOSE_NODE1, 'up', '-d')
+
+        d.addCallback(
+            lambda ignored: remote_mysql(
+                NODE1, 'show databases'
+            )
+        )
 
         d.addCallback(
             lambda ignored: self.assertEqual("", mysql_output)
