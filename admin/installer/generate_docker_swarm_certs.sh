9c412b98071d4facf109a989ea13b67995a97030
Madhuri Yechuri <madhuri.yechuri@clusterhq.com>
2016-01-11 14:59:27 -0800

    Docker + Swarm client cert generation.

--- admin/installer/generate_docker_swarm_certs.sh
+++ admin/installer/generate_docker_swarm_certs.sh
@@ -4,15 +4,15 @@ set -ex
 
 : ${s3_bucket:?}
 
-rm -rf /tmp/swarm-tls-config
-mkdir -p /tmp/swarm-tls-config
-cd /tmp/swarm-tls-config
+rm -rf /tmp/docker-swarm-tls-config
+mkdir -p /tmp/docker-swarm-tls-config
+cd /tmp/docker-swarm-tls-config
 
 # Get expect to autofill openssl inputs
 sudo apt-get install -y expect
 
 # Generate CA private and public keys
-echo > /tmp/swarm-tls-config/createca.exp << EOF
+echo > /tmp/docker-swarm-tls-config/createca1.exp << EOF
 #!/usr/bin/expect -f
 set timeout -1
 spawn openssl genrsa -passout stdin -aes256 -out ca-key.pem 4096
@@ -20,7 +20,89 @@ match_max 100000
 send -- "welcome\r"
 expect eof
 EOF
-/tmp/swarm-tls-config/createca.exp
+/tmp/docker-swarm-tls-config/createca1.exp
+
+echo > /tmp/docker-swarm-tls-config/createca2.exp << EOF
+#!/usr/bin/expect -f
+set timeout -1
+spawn openssl req -new -x509 -days 365 -key ca-key.pem -sha256 -out ca.pem
+match_max 100000
+expect -exact "Enter pass phrase for ca-key.pem:"
+send -- "welcome\r"
+expect -exact "\r
+You are about to be asked to enter information that will be incorporated\r
+into your certificate request.\r
+What you are about to enter is what is called a Distinguished Name or a DN.\r
+There are quite a few fields but you can leave some blank\r
+For some fields there will be a default value,\r
+If you enter '.', the field will be left blank.\r
+-----\r
+Country Name (2 letter code) \[AU\]:"
+send -- "US\r"
+expect -exact "US\r
+State or Province Name (full name) \[Some-State\]:"
+send -- "CA\r"
+expect -exact "CA\r
+Locality Name (eg, city) \[\]:"
+send -- "SF\r"
+expect -exact "SF\r
+Organization Name (eg, company) \[Internet Widgits Pty Ltd\]:"
+send -- "ClusterHQ\r"
+expect -exact "ClusterHQ\r
+Organizational Unit Name (eg, section) \[\]:"
+send -- "engineering\r"
+expect -exact "engineering\r
+Common Name (e.g. server FQDN or YOUR name) \[\]:"
+send -- "tech\r"
+expect -exact "tech\r
+Email Address \[\]:"
+send -- "\r"
+expect eof
+EOF
+/tmp/docker-swarm-tls-config/createca2.exp
 
 # Create server key and CSR.
 openssl genrsa -out server-key.pem 4096
+openssl req -subj "/CN=$(/usr/bin/ec2metadata --local-ipv4)" -sha256 -new -key server-key.pem -out server.csr
+
+# Sign server public key with CA.
+echo subjectAltName = IP:$(/usr/bin/ec2metadata --local-ipv4),IP:127.0.0.1 > /tmp/docker-swarm-tls-config/extfile.cnf
+echo > /tmp/docker-swarm-tls-config/createserver.exp << EOF
+#!/usr/bin/expect -f
+set timeout -1
+spawn openssl x509 -req -days 365 -sha256 -in server.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile extfile.cnf
+match_max 100000
+expect -exact "Signature ok\r
+subject=/CN=172.31.14.86\r
+Getting CA Private Key\r
+Enter pass phrase for ca-key.pem:"
+send -- "welcome\r"
+expect eof
+EOF
+/tmp/docker-swarm-tls-config/createserver.exp
+
+# Create client key.
+openssl genrsa -out key.pem 4096
+openssl req -subj '/CN=client' -new -key key.pem -out client.csr
+echo extendedKeyUsage = clientAuth > extfile.cnf
+echo > /tmp/docker-swarm-tls-config/createclient.exp << EOF
+#!/usr/bin/expect -f
+set timeout -1
+spawn openssl x509 -req -days 365 -sha256 -in client.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out cert.pem -extfile extfile.cnf
+match_max 100000
+expect -exact "Signature ok\r
+subject=/CN=client\r
+Getting CA Private Key\r
+Enter pass phrase for ca-key.pem:"
+send -- "welcome\r"
+expect eof
+EOF
+/tmp/docker-swarm-tls-config/createclient.exp
+
+# Set permissions.
+rm -v client.csr server.csr
+chmod -v 0400 ca-key.pem key.pem server-key.pem
+chmod -v 0444 ca.pem server-cert.pem cert.pem
+
+
+
