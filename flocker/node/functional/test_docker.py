840c2d8adaf70d55f0e4b803e327c7c4a680a28e
David Gebler <davidgebler@gmail.com>
2016-01-14 14:50:40 +0000

    address review comments

--- flocker/node/functional/test_docker.py
+++ flocker/node/functional/test_docker.py
@@ -260,7 +260,6 @@ class GenericDockerClientTests(AsyncTestCase):
         'which requires Docker-1.6.0 or newer. '
         'See https://docs.docker.com/registry/deploying/ for details.'
     )
-    @flaky(u"FLOC-3843")
     def test_private_registry_image(self):
         """
         ``DockerClient.add`` can start containers based on an image from a
@@ -627,7 +626,6 @@ class GenericDockerClientTests(AsyncTestCase):
         :return: A ``Registry`` describing the registry which was started.
         """
         registry_name = random_name(self)
-        external_port = find_free_port()[1]
         registry_starting = self.start_container(
             unit_name=registry_name,
             image_name='registry:2',
@@ -636,20 +634,32 @@ class GenericDockerClientTests(AsyncTestCase):
                     internal_port=5000,
                     # Doesn't matter what port we expose this on.  We'll
                     # discover what was assigned later.
-                    external_port=external_port,
+                    external_port=0,
                 ),
             ],
             retry_on_port_collision=True,
         )
 
-        def wait_for_listening(_, external_port):
+        def extract_listening_port(client):
+            listing = client.list()
+
+            def listed(apps):
+                [app] = [app for app in apps if app.name == registry_name]
+                return next(iter(app.ports)).external_port
+            listing.addCallback(listed)
+            return listing
+
+        registry_starting.addCallback(extract_listening_port)
+
+        def wait_for_listening(external_port):
             registry = Registry(
                 name=registry_name, port=external_port,
             )
             registry_listening = self.request_until_response(registry.port)
             registry_listening.addCallback(lambda ignored: registry)
             return registry_listening
-        registry_starting.addCallback(wait_for_listening, external_port)
+
+        registry_starting.addCallback(wait_for_listening)
 
         return registry_starting
 
