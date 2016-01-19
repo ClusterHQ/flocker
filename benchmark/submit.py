707d3133049b43e05c87936915117ad9d93f22b4
Andriy Gapon <andriy.gapon@clusterhq.com>
2016-01-19 17:07:38 +0200

    make the code nicer and more consistent
    Main changes:
     - submit() now reports its status programmatically and the reporting
       is moved to main()
    - treq is used instead of using twisted directly.

--- benchmark/submit.py
+++ benchmark/submit.py
@@ -8,78 +8,63 @@ from __future__ import print_function
 import json
 import sys
 
-from twisted.internet import endpoints
-from twisted.internet.defer import succeed
 from twisted.internet.task import react
 from twisted.python import usage
-from twisted.web import client, http
-from twisted.web.http_headers import Headers
-from twisted.web.iweb import IBodyProducer
+from twisted.web.http import CREATED
 
-from zope.interface import implementer
+from treq import json_content, post
 
 
-@implementer(IBodyProducer)
-class StringProducer(object):
-    def __init__(self, body):
-        self.body = body
-        self.length = len(body)
+class SubmitFailure(Exception):
+    def __init__(self, code, phrase, message):
+        super(SubmitFailure, self).__init__(message)
+        self.code = code
+        self.phrase = phrase
 
-    def startProducing(self, consumer):
-        consumer.write(self.body)
-        return succeed(None)
-
-    def pauseProducing(self):
-        pass
-
-    def stopProducing(self):
-        pass
+    def __str__(self):
+        return "[{} {}] {}".format(self.code, self.phrase, self.message)
 
 
-def submit(agent, result):
+def submit(server_url, result):
     """
-    Post the given result using the given agent.
+    Post the given result to the given server.
 
-    :param Agent agent: The HTTP client agent.
+    :param str server_url: The server's URL.
     :param dict result: The JSON-compatible result.
-    :return: Deferred that fires when a reponse from the server is received.
+    :return: Deferred that fires with an ID of the submitted result on success
+        or with SubmitFailure if the result is rejected by the server.
+
+    This function may also return any of the ``treq`` failures.
     """
-    body = StringProducer(json.dumps(result))
-    req = agent.request(
-        "POST",
-        "/v1/benchmark-results",
-        headers=Headers({'Content-Type': ['application/json']}),
-        bodyProducer=body
+    req = post(
+        server_url + "/v1/benchmark-results",
+        json.dumps(result),
+        headers=({'Content-Type': ['application/json']}),
     )
 
-    def get_response_body(response):
-        d = client.readBody(response)
-        d.addCallback(lambda body: (response, body))
+    def get_response_content(response):
+        d = json_content(response)
+        d.addCallback(lambda content: (response, content))
         return d
 
-    req.addCallback(get_response_body)
+    req.addCallback(get_response_content)
 
-    def process_response(response_and_body):
-        (response, body) = response_and_body
-        if response.code != http.CREATED:
-            print("Failed to submit the request", file=sys.stderr)
-            print("{} {}".format(response.code, response.phrase),
-                  file=sys.stderr)
+    def process_response(response_and_content):
+        (response, content) = response_and_content
+        if response.code != CREATED:
+            raise SubmitFailure(response.code, response.phrase,
+                                content['message'])
         else:
-            print("Successfully submitted the result")
-
-        json_body = json.loads(body)
-
-        if response.code != http.CREATED:
-            print("Message: {}".format(json_body['message']), file=sys.stderr)
-        else:
-            print("Assigned result ID: {}".format(json_body['id']))
+            return content['id']
 
     req.addCallback(process_response)
     return req
 
 
 class Options(usage.Options):
+    longdesc = ("Submit a benchmark result provided as a JSON document "
+                "on the standard input to the benchmark server.")
+
     optParameters = [
         ['address', None, None, "The address of the benchmark server."],
         ['port', None, 8888, "The port of the benchmark server.", int],
@@ -95,20 +80,13 @@ def main(reactor, args):
         options = Options()
         options.parseOptions(args)
     except usage.UsageError as e:
-        sys.stderr.write(e.args[0])
-        sys.stderr.write('\n\n')
-        sys.stderr.write(options.getSynopsis())
-        sys.stderr.write('\n')
-        sys.stderr.write(options.getUsage())
+        print(e.args[0], file=sys.stderr)
+        print('', file=sys.stderr)
+        print(str(options), file=sys.stderr)
         raise SystemExit(1)
 
-    agent = client.ProxyAgent(
-        endpoints.TCP4ClientEndpoint(
-            reactor,
-            options['address'],
-            options['port'],
-        ),
-        reactor,
+    server_url = b"http://{}:{}".format(
+        options['address'], options['port'],
     )
 
     try:
@@ -122,7 +100,17 @@ def main(reactor, args):
         )
         raise SystemExit(1)
 
-    return submit(agent, result)
+    d = submit(server_url, result)
+
+    def succeeded(id):
+        print("Assigned result ID: {}".format(id))
+
+    def failed(failure):
+        print("Failed to submit the result: {}".format(failure.value),
+              file=sys.stderr)
+
+    d.addCallbacks(succeeded, failed)
+    return d
 
 
 if __name__ == '__main__':
