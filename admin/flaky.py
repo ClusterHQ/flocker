53528a0fd45b26d9537c6c240bfda2798fbe3551
Jonathan Lange <jml@mumak.net>
2016-01-20 13:40:08 +0000

    Print all of the tests

--- admin/flaky.py
+++ admin/flaky.py
@@ -5,7 +5,11 @@ Find and report on tests marked as flaky.
 """
 
 import sys
+
+from testtools import iterate_tests
+from twisted.python.reflect import namedAny
 from twisted.python.usage import Options, UsageError
+from twisted.trial.runner import TestLoader
 
 
 class FindFlakyTestsOptions(Options):
@@ -21,11 +25,33 @@ class FindFlakyTestsOptions(Options):
         self['suites'] = suites
 
 
+def _load_tests(name):
+    """
+    Find all the tests under ``name``.
+
+    :param str name: The fully-qualified Python name of an object.
+    :return: A ``TestSuite`` or ``TestCase`` containing all the tests.
+    """
+    loader = TestLoader()
+    thing = namedAny(name)
+    return loader.loadAnything(thing, recurse=True)
+
+
+def _iter_tests(names):
+    """
+    Given a list of names, iterate through all the tests in them.
+    """
+    for name in names:
+        suite = _load_tests(name)
+        for test in iterate_tests(suite):
+            yield test
+
+
 def find_flaky_tests(suites):
     """
     Find all flaky tests in the given suites.
     """
-    return []
+    return _iter_tests(suites)
 
 
 def report_flaky_tests(output, flaky_tests):
