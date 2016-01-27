64a422e81ecece76a40063d02bca9110436623d8
Bridget McErlean <bridgetmcerlean@gmail.com>
2016-01-27 16:37:48 +0000

    Check that input arguments are files

--- benchmark/metrics_parser.py
+++ benchmark/metrics_parser.py
@@ -1,5 +1,6 @@
 # Copyright ClusterHQ Inc.  See LICENSE file for details.
 
+import argparse
 import json
 from collections import defaultdict
 
@@ -8,7 +9,6 @@ example = {"flocker_control":6,
 
 def apply_cpu_metric(result_blob):
     wall_time = float(result_blob.get(u'-- WALL --'))
-    #result_blob.remove(u'--WALL--')
     for k, v in result_blob.iteritems():
         if not k == u'-- WALL --':
             result_blob[k] = v/wall_time
@@ -27,16 +27,27 @@ def print_averages(results):
         )
         print s
 
-def main(args):
-    for json_file in args:
-        with open(json_file) as f:
-            results = json.load(f)
-            # XXX adding schema json validation
-            result_table = defaultdict(list)
-            for sample in results[u'samples']:
-                for result in sample[u'value'].itervalues():
-                    apply_cpu_metric(result)
-                    add_results_to_table(result, result_table)
 
-            print result_table
-            print_averages(result_table)
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description="Produce CSV from benchmarking results"
+    )
+    parser.add_argument("files", nargs="*", type=argparse.FileType(),
+                        help="Input JSON files to be processed")
+
+    return parser.parse_args(args).files
+
+def main(args):
+    files = parse_args(args)
+
+    for f in files:
+        results = json.load(f)
+        # XXX adding schema json validation
+        result_table = defaultdict(list)
+        for sample in results[u'samples']:
+            for result in sample[u'value'].itervalues():
+                apply_cpu_metric(result)
+                add_results_to_table(result, result_table)
+
+        print result_table
+        print_averages(result_table)
