096344b878bbd44cae4ac906f24c7d41d9fbe053
Madhuri Yechuri <madhuri.yechuri@clusterhq.com>
2016-01-27 17:18:03 -0800

    Draft of positive cluster limits testcase.

---
+++ admin/installer/__init__.py
@@ -0,0 +1,5 @@
+# Copyright ClusterHQ Inc.  See LICENSE file for details.
+
+"""
+Installer scripts and modules which should not be shipped with Flocker.
+"""
---
+++ admin/installer/test/test_cloudformation.py
@@ -0,0 +1,29 @@
+# Copyright ClusterHQ Inc.  See LICENSE file for details.
+
+"""
+Tests for ``flocker.admin.installer``.
+"""
+
+from subprocess import check_output
+
+from hypothesis import given
+from hypothesis.strategies import integers
+
+from flocker.testtools import TestCase
+
+from ..cloudformation import MIN_CLUSTER_SIZE, MAX_CLUSTER_SIZE
+
+# A Hypothesis strategy for generating supported cluster size.
+valid_cluster_size = integers(min_value=MIN_CLUSTER_SIZE,
+                              max_value=MAX_CLUSTER_SIZE)
+
+
+class ClusterSizeLimitsTests(TestCase):
+    """
+    """
+    @given(cluster_size=integers(min_value=MIN_CLUSTER_SIZE,
+           max_value=MAX_CLUSTER_SIZE))
+    def test_valid_cluster(self, cluster_size):
+        """
+        """
+        check_output([b"python", b"cloudformation.py", cluster_size])
