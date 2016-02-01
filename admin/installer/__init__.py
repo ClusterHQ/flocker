26b02796b5ebcfdb831982195554a9d33a7aac7d
Richard Wall <richard.wall@clusterhq.com>
2016-02-01 15:02:14 -0800

    generic package docstring

--- admin/installer/__init__.py
+++ admin/installer/__init__.py
@@ -1,5 +1,5 @@
 # Copyright ClusterHQ Inc.  See LICENSE file for details.
 
 """
-Helper utilities for CloudFormation Installer's Packer images.
+Helper utilities for the CloudFormation installer.
 """
