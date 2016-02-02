2b7a1b689570a20d905d2b753c7d2d018a4322b4
Pilar Gomez <pilar.gomez@gmail.com>
2016-02-02 09:56:49 +0000

    Making loop simpler

--- benchmark/scenarios/test/test_rate_measurer.py
+++ benchmark/scenarios/test/test_rate_measurer.py
@@ -21,9 +21,8 @@ class RateMeasurerTest(TestCase):
         :param num_samples: The number of samples to collect.
         """
         call_duration = 4.567
-        for i in range(num_samples):
-            for i in range(num_requests):
-                rate_measurer.response_received(call_duration)
+        for i in range(num_samples*num_requests):
+            rate_measurer.response_received(call_duration)
 
     def send_requests(self, rate_measurer, num_requests, num_samples):
         """
