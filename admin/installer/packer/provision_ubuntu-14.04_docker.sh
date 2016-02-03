c0dba35c0c94d12d849ef7d51475eb6b229736e2
Richard Wall <richard.wall@clusterhq.com>
2016-02-02 18:18:04 -0800

    Allow Docker and Swarm versions to be supplied as environment variables.

--- admin/installer/packer/provision_ubuntu-14.04_docker.sh
+++ admin/installer/packer/provision_ubuntu-14.04_docker.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 set -ex
 
-# Install Docker like Vagrant does
-# https://github.com/mitchellh/vagrant/blob/master/plugins/provisioners/docker/cap/debian/docker_install.rb#L13
+# Install Docker according to their distribution specific instructions:
+# https://docs.docker.com/engine/installation/ubuntulinux/
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+cat <<EOF > /etc/apt/sources.list.d/docker.list
+deb https://apt.dockerproject.org/repo ubuntu-trusty main
+EOF
+# apt-get update fails in an Ubuntu 14.04 docker container unless you install
+# this first.
+apt-get install -qq -y apt-transport-https
 apt-get update -qq -y
-apt-get install -qq -y --force-yes curl
 apt-get purge -qq -y lxc-docker* || true
-curl -sSL https://get.docker.com/ | sh
+apt-get install -qq -y "linux-image-extra-$(uname -r)" "docker-engine=${DOCKER_VERSION}*"
+service docker start || true
+if test -e /etc/default/ufw; then
+    sed --in-place 's/DEFAULT_FORWARD_POLICY=".*"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
+    ufw reload
+    ufw allow 2375/tcp
+fi
+
+# Prepull swarm
+docker pull "swarm:${SWARM_VERSION}"
