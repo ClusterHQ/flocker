0385e4f481bf19dbd1fa84c8f13518e1bd023c3c
Madhuri Yechuri <madhuriyechuri@clusterhq.com>
2016-02-05 20:01:36 +0000

    Find fully qualified pathname for cloudformation.py.

--- admin/installer/test/test_cloudformation.py
+++ admin/installer/test/test_cloudformation.py
@@ -4,6 +4,7 @@
 Tests for ``flocker.admin.installer``.
 """
 
+from os import walk
 from subprocess import check_output
 
 from hypothesis import given
@@ -23,22 +24,23 @@ valid_cluster_size = integers(min_value=MIN_CLUSTER_SIZE,
 def _get_cloudformation_full_path():
     """
     """
-    root_path = FilePath(b"/")
-    cloudformation_script = b'cloudformation.py'
-    for root, dirs, files in root_path.walk():
-        if b'cloudformation.py' in files:
-            return FilePath(root + cloudformation_script)
+    root_path = b'/'
+    cloudformation_path_suffix = b'flocker/admin/installer'
+    cloudformation_file = b'cloudformation.py'
+    for root, dirs, files in walk(root_path):
+        if root.endswith(cloudformation_path_suffix) \
+           and cloudformation_file in files:
+            return FilePath("/".join((root, cloudformation_file)))
 
 
 class ClusterSizeLimitsTests(TestCase):
     """
     """
-    @given(cluster_size=integers(min_value=MIN_CLUSTER_SIZE,
-           max_value=MAX_CLUSTER_SIZE))
+    @given(cluster_size=valid_cluster_size)
     def test_valid_cluster(self, cluster_size):
         """
         """
-        cloudformation_file = root_path.childSearchPreauth(b'cloudformation.py')
+        cloudformation_file = _get_cloudformation_full_path()
 
         check_output([b"python",
                       cloudformation_file.path,
