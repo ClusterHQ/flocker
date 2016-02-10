09d64dec352df53c64ce04ee0cff4d421c08817d
Pilar Gomez <pilar.gomez@gmail.com>
2016-02-10 14:49:58 +0000

    Increasing max sleep time

--- flocker/node/_loop.py
+++ flocker/node/_loop.py
@@ -277,7 +277,7 @@ _UNCONVERGED_DELAY = 0.1
 class UnconvergedDelay(object):
     def __init__(self,
                  initial_sleep=_UNCONVERGED_DELAY,
-                 max_sleep=240,
+                 max_sleep=400,
                  min_sleep=_UNCONVERGED_DELAY):
         self.delay = initial_sleep
         self.max_sleep = max_sleep
