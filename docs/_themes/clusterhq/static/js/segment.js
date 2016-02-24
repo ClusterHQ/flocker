b0e1bc476f27b0607536f0ee44a2a664a74b56e9
Marcus Hughes <hello@entirely.pro>
2016-02-24 14:09:59 +0000

    add initial segment support
    (cherry picked from commit ed493b1a312ddaf6ab9695750ac379e1df73a769)
    (cherry picked from commit c027ce39542509bb154201a268f41ba17dc880e1)

---
+++ docs/_themes/clusterhq/static/js/segment.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // For every anchor on the page, create an analytics track event onclick
+    $('a').each(function(index, val){ 
+        analytics.trackLink($(this), 'Clicked', {
+            object: 'link',
+            anchor_text:  $(this).text(),
+            destination_url: $(this).attr("href"),
+            page_name: window.location.pathname,
+        });
+    });
+});
+
+// On form submit event, send analytics track event
+$('form').submit(function( event ) {
+    analytics.track('Form Submit', {
+        form_id: $(this).attr("id"),
+    });
+});
