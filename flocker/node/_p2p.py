af7659186e622e0a33658752463d7d49b9af3db3
Richard Wall <richard.wall@clusterhq.com>
2016-03-10 11:38:36 -0800

    A unit test revealed that we were also attempting to re-delete the deleted datasets.

--- flocker/node/_p2p.py
+++ flocker/node/_p2p.py
@@ -354,8 +354,11 @@ def find_dataset_changes(uuid, current_state, desired_state):
                             # datasets; this is wrong. See FLOC-2060.
                             in (node.manifestations or {}).values())
                         for node in current_state.nodes}
-    local_desired_datasets = set(dataset for dataset in desired_datasets.get(uuid, set())
-                                 if dataset.deleted is False)
+
+    local_desired_datasets = set(
+        dataset for dataset in desired_datasets.get(uuid, set())
+        if dataset.deleted is False
+    )
     local_desired_dataset_ids = set(dataset.dataset_id for dataset in
                                     local_desired_datasets)
     local_current_dataset_ids = set(dataset.dataset_id for dataset in
@@ -408,6 +411,7 @@ def find_dataset_changes(uuid, current_state, desired_state):
                    if dataset.dataset_id in creating_dataset_ids)
 
     deleting = set(dataset for dataset in chain(*desired_datasets.values())
-                   if dataset.deleted)
+                   if dataset.deleted
+                   and dataset.dataset_id in local_current_dataset_ids)
     return DatasetChanges(going=going, deleting=deleting,
                           creating=creating, resizing=resizing)
