d0dcc299f504b6bda33463542454e141e4c6f642
Richard Wall <richard.wall@clusterhq.com>
2016-03-28 17:43:01 -0700

    Do not log configuration and deployment state during CONVERGE and update the tests accordingly.

--- flocker/node/_loop.py
+++ flocker/node/_loop.py
@@ -379,21 +379,13 @@ LOG_SEND_TO_CONTROL_SERVICE = ActionType(
     [_FIELD_CONNECTION, _FIELD_LOCAL_CHANGES], [],
     u"Send the local state to the control service.")
 
-_FIELD_CLUSTERSTATE = Field(
-    u"cluster_state", to_unserialized_json,
-    u"The state of the cluster, according to control service.")
-
-_FIELD_CONFIGURATION = Field(
-    u"desired_configuration", to_unserialized_json,
-    u"The configuration of the cluster according to the control service.")
-
 _FIELD_ACTIONS = Field(
     u"calculated_actions", repr,
     u"The actions we decided to take to converge with configuration.")
 
 LOG_CONVERGE = ActionType(
     u"flocker:agent:converge",
-    [_FIELD_CLUSTERSTATE, _FIELD_CONFIGURATION], [],
+    [], [],
     u"The convergence action within the loop.")
 
 LOG_DISCOVERY = ActionType(
@@ -524,8 +516,7 @@ class ConvergenceLoop(object):
             return succeed(None)
 
     def output_CONVERGE(self, context):
-        with LOG_CONVERGE(self.fsm.logger, cluster_state=self.cluster_state,
-                          desired_configuration=self.configuration).context():
+        with LOG_CONVERGE(self.fsm.logger).context():
             log_discovery = LOG_DISCOVERY(self.fsm.logger)
             with log_discovery.context():
                 discover = DeferredContext(maybeDeferred(
