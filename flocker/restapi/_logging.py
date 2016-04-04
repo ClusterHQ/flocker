83bb6c5b48c24656d2fcbeeb47afac258a67ac89
Marcus Henry Ewert <user@marcushenryewert.com>
2016-04-04 15:13:59 -0700

    Removed JSON_REQUEST tests

--- flocker/restapi/_logging.py
+++ flocker/restapi/_logging.py
@@ -7,7 +7,6 @@ This module defines the Eliot log events emitted by the API implementation.
 from eliot import Field, ActionType
 
 __all__ = [
-    "JSON_REQUEST",
     "REQUEST",
     ]
 
@@ -26,20 +25,9 @@ RESPONSE_CODE = Field.forTypes(
     u"The response code for the request.")
 
 
-# It would be nice if RESPONSE_CODE was in REQUEST instead of
-# JSON_REQUEST; see FLOC-1586.
+# It would be nice if RESPONSE_CODE was in REQUEST; see FLOC-1586.
 REQUEST = ActionType(
     LOG_SYSTEM + u":request",
     [REQUEST_PATH, METHOD],
     [],
     u"A request was received on the public HTTP interface.")
-
-# NB we deliberately do not log the entire JSON response body because the
-# volume of log that it generates for state and configuration requests in a
-# cluster with large numbers of datasets and nodes causes high CPU usage in the
-# control service and in the logging daemon. (FLOC-4327)
-JSON_REQUEST = ActionType(
-    LOG_SYSTEM + u":json_request",
-    [JSON],
-    [RESPONSE_CODE],
-    u"A request containing JSON request body and HTTP response code.")
