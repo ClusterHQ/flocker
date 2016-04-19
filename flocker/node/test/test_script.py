5d32794cae4644aa2c7ded8dc77a60f9132b66e4
Richard Wall <richard.wall@clusterhq.com>
2016-04-19 12:00:56 -0700

    Remove mimic stuff

--- flocker/node/test/test_script.py
+++ flocker/node/test/test_script.py
@@ -5,12 +5,8 @@ Tests for :module:`flocker.node.script`.
 """
 
 from functools import wraps
-from itertools import repeat
 import logging
-import os
-import re
 import socket
-from subprocess import Popen
 from unittest import skipUnless
 from uuid import uuid4
 
@@ -33,7 +29,7 @@ from twisted.python.usage import UsageError
 
 from ...common.script import ICommandLineScript
 from ...common.plugin import PluginNotFound
-from ...common import get_all_ips, poll_until
+from ...common import get_all_ips
 from ...common._era import get_era
 
 from ..script import (
@@ -303,49 +299,6 @@ class AgentServiceFromConfigurationTests(TestCase):
         logger = logging.getLogger(log_name)
         logger.info(log_message)
         self.assertIn(log_message, logfile.getContent())
-
-
-def mimic_for_test(test_case):
-    """
-    Start a mimic server in the background on an ephemeral port and return the
-    port number.
-
-    This is used in synchronous test cases so I can't launch the mimic service
-    in process.
-
-    Parsing the logs for the chosen port number is ugly, but ``find_free_port``
-    kept returning ports that were in use when mimic attempted to bind to them.
-    """
-    log = test_case.make_temporary_path()
-    stdout = test_case.make_temporary_path()
-    stderr = test_case.make_temporary_path()
-    with stdout.open('w') as stdout, stderr.open('w') as stderr:
-        p = Popen(['twistd', '--nodaemon', '--logfile', log.path,
-                   'mimic', '--listen', '0', '--realtime'],
-                  stdin=open(os.devnull), stdout=stdout, stderr=stderr,
-                  close_fds=True)
-
-    def cleanup():
-        p.terminate()
-        p.wait()
-    test_case.addCleanup(cleanup)
-
-    poll_until(
-        predicate=log.exists,
-        steps=repeat(1, 5)
-    )
-
-    def port_from_log():
-        for line in log.open():
-            match = re.search(r"Site starting on (\d+)$", line)
-            if match:
-                port = match.group(1)
-                return int(port)
-
-    return poll_until(
-        predicate=port_from_log,
-        steps=repeat(1, 5)
-    )
 
 
 class AgentServiceGetAPITests(TestCase):
