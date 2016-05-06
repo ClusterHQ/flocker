a85029a8e7e10fbcd0c4246bd69f62a0ef8e1a34
Richard Wall <richard.wall@clusterhq.com>
2016-05-06 14:31:33 +0100

    Pin the swarm version to 1.2.1

--- admin/installer/setup_swarm_manager.sh
+++ admin/installer/setup_swarm_manager.sh
@@ -11,10 +11,10 @@ DOCKER_CERT_HOME='/root/.docker'
 # Create Swarm cluster, and publish Swarm cluster ID to S3 bucket.
 rm -rf /tmp/swarm-config
 mkdir -p /tmp/swarm-config
-docker pull swarm:1.0.1
-docker run --rm -v ${DOCKER_CERT_HOME}:${DOCKER_CERT_HOME} swarm create > /tmp/swarm-config/swarm_cluster_id
+docker pull swarm:1.2.1
+docker run --rm -v ${DOCKER_CERT_HOME}:${DOCKER_CERT_HOME} swarm:1.2.1 create > /tmp/swarm-config/swarm_cluster_id
 /usr/bin/s3cmd put --config=/root/.s3cfg --recursive /tmp/swarm-config/ s3://${s3_bucket}/swarm-config/
 
 # Start Swarm Manager.
 swarm_cluster_id=$(cat /tmp/swarm-config/swarm_cluster_id)
-docker run -d -v ${DOCKER_CERT_HOME}:${DOCKER_CERT_HOME} -p 2376:2375 swarm manage --tlsverify --tlscacert=${DOCKER_CERT_HOME}/ca.pem --tlskey=${DOCKER_CERT_HOME}/key.pem --tlscert=${DOCKER_CERT_HOME}/cert.pem token://$swarm_cluster_id
+docker run -d -v ${DOCKER_CERT_HOME}:${DOCKER_CERT_HOME} -p 2376:2375 swarm:1.2.1 manage --tlsverify --tlscacert=${DOCKER_CERT_HOME}/ca.pem --tlskey=${DOCKER_CERT_HOME}/key.pem --tlscert=${DOCKER_CERT_HOME}/cert.pem token://$swarm_cluster_id
