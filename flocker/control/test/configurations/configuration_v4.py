68a960598dc65345822be6ae1ab918e9d94952c1
Marcus Henry Ewert <user@marcushenryewert.com>
2016-05-19 14:56:36 -0700

    WIP changed the format of flocker.control.test.configurations

--- flocker/control/test/configurations/configuration_v4.py
+++ flocker/control/test/configurations/configuration_v4.py
@@ -1,11 +1,22 @@
 # Copyright ClusterHQ Inc.  See LICENSE file for details.
 
 # Generate a v4 configuration.
-# Hash to recreate: 993d92c1b6a62199cf38e45afa7d2e584d48ead1
+# Hash to recreate: TODO(mewert): Fill this in after you push.
+
+from twisted.python.filepath import FilePath
 
 from flocker.control._model import Configuration
 from flocker.control._persistence import wire_encode
-from flocker.control.test.test_persistence import TEST_DEPLOYMENT
+from flocker.control.test.test_persistence import TEST_DEPLOYMENTS
+
+_VERSION = 4
 
 if __name__ == "__main__":
-    print wire_encode(Configuration(version=4, deployment=TEST_DEPLOYMENT))
+    myfile = FilePath(__file__)
+    for i, deployment in enumerate(TEST_DEPLOYMENTS, start=1):
+        encoding = wire_encode(
+            Configuration(version=_VERSION, deployment=deployment)
+        )
+        myfile.sibling(
+            b"configuration_%d_v%d.json" % (i, _VERSION)
+        ).setContent(encoding)
