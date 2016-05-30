10322e3d6b7b00d40c28d367c8caa7c25e0ff1db
Marcus Henry Ewert <user@marcushenryewert.com>
2016-05-30 16:37:52 -0700

    Add template files

---
+++ flocker/control/_generations.py
@@ -0,0 +1,2 @@
+# Copyright ClusterHQ Inc.  See LICENSE file for details.
+# -*- test-case-name: flocker.control.test.test_generations -*-
---
+++ flocker/control/test/test_generations.py
@@ -0,0 +1,12 @@
+# Copyright ClusterHQ Inc.  See LICENSE file for details.
+
+"""
+Tests for ``flocker.node._generations``.
+"""
+
+
+from ...testtools import TestCase
+
+
+class GenerationTracker(TestCase):
+    pass
