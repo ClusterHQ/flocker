7177c1891ed815cd0c52c667e5af46836d556420
Marcus Henry Ewert <user@marcushenryewert.com>
2016-05-30 18:22:02 -0700

    convinced that composition is working

--- flocker/control/_generations.py
+++ flocker/control/_generations.py
@@ -1,2 +1,48 @@
 # Copyright ClusterHQ Inc.  See LICENSE file for details.
 # -*- test-case-name: flocker.control.test.test_generations -*-
+
+from collections import deque
+from pyrsistent import PClass, field
+
+from ._model import GenerationHash
+from ._persistence import make_generation_hash
+from ._diffing import Diff, create_diff
+
+
+class _GenerationRecord(PClass):
+    generation_hash = field(type=GenerationHash, mandatory=True)
+
+    # ``None`` indicates that this is the most recent record
+    diff_to_next = field(type=(Diff, type(None)), mandatory=True, initial=None)
+
+
+class GenerationTracker(object):
+
+    def __init__(self, cache_size):
+        self._queue = deque(maxlen=cache_size)
+        self._latest_object = None
+        self._latest_hash = None
+
+    def _append_hash(self, obj_hash):
+        self._latest_hash = obj_hash
+
+    def get_diff_from_hash_to_latest(self, generation_hash, latest):
+        latest_hash = make_generation_hash(latest)
+        result = None
+
+        if self._latest_latest is not None:
+            new_diff = create_diff(self._latest_object, latest)
+            if self._latest_hash is not None:
+                self._queue.append(
+                    _GenerationRecord(
+                        generation_hash=self._latest_hash,
+                        diff_to_next=new_diff
+                    )
+                )
+            self._latest_object = latest
+            result = new_diff
+
+        self._append_hash(latest_hash)
+        self._latest_object = latest
+
+        return result
--- flocker/control/test/test_generations.py
+++ flocker/control/test/test_generations.py
@@ -4,9 +4,29 @@
 Tests for ``flocker.node._generations``.
 """
 
+from testtools.matchers import Equals
 
 from ...testtools import TestCase
 
+from ..testtools import deployment_strategy
+from .._generations import GenerationTracker
+from .._persistence import make_generation_hash
 
-class GenerationTracker(TestCase):
-    pass
+
+class GenerationTrackerTests(TestCase):
+
+    def test_baic_use_works(self):
+        deployments = list(deployment_strategy() for _ in xrange(5))
+        tracker_under_test = GenerationTracker(10)
+        for d in tracker_under_test:
+            self.get_diff_from_hash_to_latest(None, d)
+
+        last_deployment = deployments[-1]
+
+        for d in tracker_under_test:
+            diff = self.get_diff_from_hash_to_latest(
+                make_generation_hash(d), last_deployment)
+            self.assertThat(
+                diff.apply(d),
+                Equals(last_deployment)
+            )
