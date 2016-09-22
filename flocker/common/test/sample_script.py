605f4d7e2172c3ce1b6f614c8ccc3ce070816007
Richard Wall <richard.wall@clusterhq.com>
2016-09-22 13:57:28 +0100

    Refactor and document the tests

--- flocker/common/test/sample_script.py
+++ flocker/common/test/sample_script.py
@@ -1,6 +1,17 @@
 # Copyright ClusterHQ Inc.  See LICENSE file for details.
+
+"""
+A script which is called by ``run_process`` in the ``test_process` module.
+The returncode, stdout and stderr can all be supplied as command line arguments
+so that we can test how ``run_process`` captures process output and how it
+behaves when sub-processes exit with different return codes.
+If the supplied returncode is < 0 the script will send that integer as a signal
+to its own PID.
+"""
+
 import os
 import sys
+import time
 
 from twisted.python.usage import Options
 
@@ -23,8 +34,8 @@ def main():
     returncode = options["returncode"]
     if returncode < 0:
         os.kill(os.getpid(), abs(returncode))
-    else:
-        return returncode
+        time.sleep(10)
+    return returncode
 
 if __name__ == "__main__":
     raise SystemExit(main())
--- flocker/common/test/test_process.py
+++ flocker/common/test/test_process.py
@@ -5,39 +5,78 @@ Tests for ``flocker.common.runner``.
 from subprocess import CalledProcessError
 import sys
 
+from pyrsistent import PClass, field
 from twisted.python.filepath import FilePath
 
-from flocker.testtools import TestCase, random_name
-
+from ...testtools import TestCase, random_name
 from ..process import run_process, _ProcessResult
+
+SAMPLE_SCRIPT_FILE = FilePath(__file__).sibling('sample_script.py')
+
+
+class SampleScript(PClass):
+    """
+    The parameters which will be supplied when calling ``SAMPLE_SCRIPT_FILE``
+    in tests.
+    """
+    returncode = field(type=int)
+    stdout = field(type=bytes)
+    stderr = field(type=bytes)
+
+    def commandline(self):
+        """
+        :returns: A ``list`` suitable for passing to ``run_process`` in tests.
+        """
+        return [
+            sys.executable,
+            SAMPLE_SCRIPT_FILE.path,
+            "--returncode", bytes(self.returncode),
+            "--stdout", self.stdout,
+            "--stderr", self.stderr,
+        ]
 
 
 class RunProcessTests(TestCase):
+    """
+    Tests for ``run_process``.
+    """
+    def command_for_test(self, returncode):
+        return SampleScript(
+            returncode=returncode,
+            stdout=random_name(self).encode("utf8"),
+            stderr=random_name(self).encode("utf8"),
+        )
+
     def test_success(self):
         """
         ``run_process`` returns a `__ProcessResult`` object with the status,
         command and combined stdout and stderr if the exit status is 0.
         """
-        expected_returncode = 0
-        expected_stdout = random_name(self).encode("utf8")
-        expected_stderr = random_name(self).encode("utf8")
-        sample_script = FilePath(__file__).sibling('sample_script.py')
-        command = [
-            sys.executable,
-            sample_script.path,
-            "--returncode", bytes(expected_returncode),
-            "--stdout", expected_stdout,
-            "--stderr", expected_stderr,
-        ]
-        result = run_process(command)
+        command = self.command_for_test(returncode=0)
+        result = run_process(command.commandline())
         self.assertEqual(
             _ProcessResult(
-                command=command,
-                status=expected_returncode,
-                output=expected_stdout + expected_stderr,
+                command=command.commandline(),
+                status=command.returncode,
+                output=command.stdout + command.stderr,
             ),
             result
         )
+
+    def check_run_process_error(self, expected_returncode):
+        command = self.command_for_test(returncode=expected_returncode)
+        e = self.assertRaises(
+            CalledProcessError,
+            run_process,
+            command.commandline()
+        )
+        self.assertEqual(
+            (command.commandline(),
+             command.returncode,
+             command.stdout + command.stderr),
+            (e.cmd, e.returncode, e.output)
+        )
+        self.assertIn(command.stdout + command.stderr, unicode(e))
 
     def test_error(self):
         """
@@ -45,47 +84,11 @@ class RunProcessTests(TestCase):
         The string representation of the raised error includes the combined
         stdout and stderr.
         """
-        expected_returncode = 1
-        expected_stdout = random_name(self).encode("utf8")
-        expected_stderr = random_name(self).encode("utf8")
-        sample_script = FilePath(__file__).sibling('sample_script.py')
-        command = [
-            sys.executable,
-            sample_script.path,
-            "--returncode", bytes(expected_returncode),
-            "--stdout", expected_stdout,
-            "--stderr", expected_stderr,
-        ]
+        self.check_run_process_error(expected_returncode=1)
 
-        e = self.assertRaises(CalledProcessError, run_process, command)
-
-        self.assertEqual(
-            (command, expected_returncode, expected_stdout + expected_stderr),
-            (e.cmd, e.returncode, e.output)
-        )
-        self.assertIn(expected_stdout + expected_stderr, unicode(e))
-
-    def test_signal(self):
+    def test_error_signal(self):
         """
         ``run_process`` raises CalledProcessError when it exits due to a signal
         and the signal is included in the exception.
         """
-        expected_returncode = -1
-        expected_stdout = random_name(self).encode("utf8")
-        expected_stderr = random_name(self).encode("utf8")
-        sample_script = FilePath(__file__).sibling('sample_script.py')
-        command = [
-            sys.executable,
-            sample_script.path,
-            "--returncode", bytes(expected_returncode),
-            "--stdout", expected_stdout,
-            "--stderr", expected_stderr,
-        ]
-
-        e = self.assertRaises(CalledProcessError, run_process, command)
-
-        self.assertEqual(
-            (command, expected_returncode, expected_stdout + expected_stderr),
-            (e.cmd, e.returncode, e.output)
-        )
-        self.assertIn(expected_stdout + expected_stderr, unicode(e))
+        self.check_run_process_error(expected_returncode=-1)
