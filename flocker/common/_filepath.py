5a17ae3a7b61fe3486209703b90b4ae4a85b2c1c
Richard Wall <richard.wall@clusterhq.com>
2016-09-23 11:00:16 +0100

    A temporary_directory function and tests

--- flocker/common/_filepath.py
+++ flocker/common/_filepath.py
@@ -3,6 +3,10 @@
 """
 Helpers for :py:class:`~twisted.python.filepath.FilePath`.
 """
+from tempfile import mkdtemp
+
+from twisted.python.filepath import FilePath, IFilePath
+from twisted.python.components import proxyForInterface
 
 
 def make_file(path, content='', permissions=None):
@@ -38,3 +42,40 @@ def make_directory(path):
     if not path.isdir():
         path.makedirs()
     return path
+
+
+class _TemporaryPath(proxyForInterface(IFilePath, "_path")):
+    """
+    An ``IFilePath`` which when used as a context manager will remove itself.
+    """
+    def __init__(self, path):
+        self._path = path
+
+    @property
+    def path(self):
+        return self._path.path
+
+    def remove(self):
+        return self._path.remove()
+
+    def __eq__(self, other):
+        return self._path == other
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.remove()
+
+
+def temporary_directory(parent_path=None):
+    """
+    :returns: A temporary directory (a ``_TemporaryPath``).
+    """
+    mkdtemp_args = {}
+    if parent_path is not None:
+        mkdtemp_args["dir"] = parent_path.path
+
+    return _TemporaryPath(
+        path=FilePath(mkdtemp(**mkdtemp_args))
+    )
