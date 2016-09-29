dc9f9f9015255bdbb5962e0e1a284efe12fcaee7
Richard Wall <richard.wall@clusterhq.com>
2016-09-29 15:29:42 +0100

    Remove instructions for enabling and starting the container agent from the docs

--- flocker/provision/test/test_install.py
+++ flocker/provision/test/test_install.py
@@ -105,8 +105,6 @@ class EnableFlockerAgentTests(TestCase):
         expected_sequence = sequence([
             run(command="systemctl enable flocker-dataset-agent"),
             run(command="systemctl start flocker-dataset-agent"),
-            run(command="systemctl enable flocker-container-agent"),
-            run(command="systemctl start flocker-container-agent"),
         ])
         self.assertEqual(commands, expected_sequence)
 
@@ -124,8 +122,6 @@ class EnableFlockerAgentTests(TestCase):
         expected_sequence = sequence([
             run(command="systemctl enable flocker-dataset-agent"),
             run(command="systemctl restart flocker-dataset-agent"),
-            run(command="systemctl enable flocker-container-agent"),
-            run(command="systemctl restart flocker-container-agent"),
         ])
         self.assertEqual(commands, expected_sequence)
 
@@ -140,7 +136,6 @@ class EnableFlockerAgentTests(TestCase):
         )
         expected_sequence = sequence([
             run(command="service flocker-dataset-agent start"),
-            run(command="service flocker-container-agent start"),
         ])
         self.assertEqual(commands, expected_sequence)
 
@@ -157,7 +152,6 @@ class EnableFlockerAgentTests(TestCase):
         )
         expected_sequence = sequence([
             run(command="service flocker-dataset-agent restart"),
-            run(command="service flocker-container-agent restart"),
         ])
         self.assertEqual(commands, expected_sequence)
 
