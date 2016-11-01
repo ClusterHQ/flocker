3990ce6f42274cb02d9fda00ab266804a39ea512
Richard Wall <richard.wall@clusterhq.com>
2016-11-01 14:16:53 +0000

    remove trailing commas

--- docs/_themes/clusterhq/static/js/segment.js
+++ docs/_themes/clusterhq/static/js/segment.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     // For every anchor on the page, create an analytics track event onclick
-    $('a').each(function(index, val){ 
+    $('a').each(function(index, val){
         analytics.trackLink($(this), 'Clicked', {
             object: 'link',
             anchor_text:  $(this).text(),
             destination_url: $(this).attr("href"),
-            page_name: window.location.pathname,
+            page_name: window.location.pathname
         });
     });
 });
@@ -13,6 +13,6 @@ $(document).ready(function() {
 // On form submit event, send analytics track event
 $('form').submit(function( event ) {
     analytics.track('Form Submit', {
-        form_id: $(this).attr("id"),
+        form_id: $(this).attr("id")
     });
 });
