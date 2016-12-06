e488a516b680b1e603bb87495f3abb1f1975aa26
Richard Wall <richard.wall@clusterhq.com>
2016-12-06 18:13:21 +0000

    An sqlalchemy based implementation with tests.

--- flocker/control/configuration_store/sql.py
+++ flocker/control/configuration_store/sql.py
@@ -5,18 +5,71 @@ Persistence of cluster configuration to a SQL database.
 """
 from .interface import IConfigurationStore
 
-from twisted.internet.defer import succeed
+from alchimia import TWISTED_STRATEGY
+
+from pyrsistent import PClass, field
+
+from sqlalchemy import (
+    create_engine, MetaData, Table, Column, Integer, String, LargeBinary
+)
+from sqlalchemy.schema import CreateTable
+
+from twisted.internet.defer import inlineCallbacks, succeed, returnValue
 
 from zope.interface import implementer
 
+METADATA = MetaData()
+CONFIGURATION_TABLE = Table(
+    "configuration",
+    METADATA,
+    Column("content", LargeBinary),
+)
+
 
 @implementer(IConfigurationStore)
-class SQLConfigurationStore(object):
+class SQLConfigurationStore(PClass):
+
+    connection_string = field(
+        type=(unicode,),
+        mandatory=True
+    )
+    reactor = field(mandatory=True)
+
+    def _engine(self):
+        return create_engine(
+            self.connection_string,
+            reactor=self.reactor,
+            strategy=TWISTED_STRATEGY,
+        )
+
+    @inlineCallbacks
     def initialize(self):
-        return succeed(None)
+        engine = self._engine()
+        table_names = yield engine.table_names()
+        if not set(METADATA.tables.keys()).issubset(table_names):
+            yield engine.execute(
+                CreateTable(CONFIGURATION_TABLE)
+            )
+            yield engine.execute(
+                CONFIGURATION_TABLE.insert().values(content=b"")
+            )
+        table_names = yield engine.table_names()
+        returnValue(None)
 
+    @inlineCallbacks
     def get_content(self):
-        return succeed(None)
+        engine = self._engine()
+        result = yield engine.execute(
+            CONFIGURATION_TABLE.select()
+        )
+        [content] = yield result.fetchall()
+        [content] = content
+        returnValue(content)
 
+    @inlineCallbacks
     def set_content(self, content):
-        return succeed(None)
+        engine = self._engine()
+        yield engine.execute(
+            CONFIGURATION_TABLE.update().values(content=content)
+        )
+        returnValue(None)
--- flocker/control/configuration_store/test/test_sql.py
+++ flocker/control/configuration_store/test/test_sql.py
@@ -7,6 +7,7 @@ from ....testtools import AsyncTestCase
 from ..sql import SQLConfigurationStore
 from ..testtools import IConfigurationStoreTestsMixin
 
+from twisted.internet import reactor
 
 class SQLConfigurationStoreInterfaceTests(IConfigurationStoreTestsMixin,
                                           AsyncTestCase):
@@ -15,4 +16,7 @@ class SQLConfigurationStoreInterfaceTests(IConfigurationStoreTestsMixin,
     """
     def setUp(self):
         super(SQLConfigurationStoreInterfaceTests, self).setUp()
-        self.store = SQLConfigurationStore()
+        self.store = SQLConfigurationStore(
+            connection_string=u"sqlite:///{}".format(self.make_temporary_file().path),
+            reactor=reactor,
+        )
